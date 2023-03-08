e20188bd7dad1b8fa741afdfea9529f0af6d4c76
Janne Suominen <janne.suominen@siili.com>
2023-03-08 17:05:44 +0200

    AU-40: fix: PHPCS

--- public/modules/custom/grants_handler/src/Controller/CompletionController.php
+++ public/modules/custom/grants_handler/src/Controller/CompletionController.php
@@ -3,12 +3,8 @@
 namespace Drupal\grants_handler\Controller;
 
 use Drupal\Core\Controller\ControllerBase;
-use Drupal\Core\Entity\EntityStorageException;
 use Drupal\Core\StringTranslation\TranslatableMarkup;
 use Drupal\grants_handler\ApplicationHandler;
-use Drupal\grants_handler\Plugin\WebformElement\CompensationsComposite;
-use Drupal\node\Entity\Node;
-use Drupal\taxonomy\Entity\Term;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 /**
@@ -21,25 +17,18 @@ class CompletionController extends ControllerBase {
    */
   public function build($submission_id): array {
 
-
-
-
-
-
-
     $build = [
       '#theme' => 'grants_handler_completion',
       '#submissionId' => $submission_id,
     ];
 
-//    try {
-//      $submissionObject = ApplicationHandler::submissionObjectFromApplicationNumber($submission_id);
-//      $build['#submissionObject'] = $submissionObject;
-//
-//    }
-//    catch (\Throwable $e) {
-//      throw new NotFoundHttpException('Submission not found');
-//    }
+    try {
+      $submissionObject = ApplicationHandler::submissionObjectFromApplicationNumber($submission_id);
+      $build['#submissionObject'] = $submissionObject;
+
+    } catch (\Throwable $e) {
+      throw new NotFoundHttpException('Submission not found');
+    }
 
     return $build;
   }
