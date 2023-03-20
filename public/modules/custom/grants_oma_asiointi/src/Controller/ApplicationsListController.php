99c57cd417f10b6382946153a321fd29b9eec88b
Suvi Kankare <suvi.kankare@vincit.fi>
2023-03-20 13:17:33 +0200

    AU-744: remove unnecessary stuff

--- public/modules/custom/grants_oma_asiointi/src/Controller/ApplicationsListController.php
+++ public/modules/custom/grants_oma_asiointi/src/Controller/ApplicationsListController.php
@@ -176,12 +176,9 @@ class ApplicationsListController extends ControllerBase {
       throw new NotFoundHttpException('No documents found');
     }
 
-    $hasUnreadMessages = [];
-
     $build = [
       '#theme' => 'application_list',
       '#items' => $applications,
-      '#unreadItems' => $hasUnreadMessages,
       '#type' => 'all',
       '#header' => $this->t('My applications'),
       '#id' => 'applications__list',
