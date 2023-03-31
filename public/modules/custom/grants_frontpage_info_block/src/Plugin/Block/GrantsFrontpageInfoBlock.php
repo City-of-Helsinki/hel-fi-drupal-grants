1da6204025eb9e4b9eaded8a86e34a30cc4c5cce
Petri Leinonen <petri.leinonen@siili.com>
2023-03-31 16:37:19 +0300

    Update GrantsFrontpageInfoBlock.php

--- public/modules/custom/grants_frontpage_info_block/src/Plugin/Block/GrantsFrontpageInfoBlock.php
+++ public/modules/custom/grants_frontpage_info_block/src/Plugin/Block/GrantsFrontpageInfoBlock.php
@@ -27,24 +27,4 @@ class GrantsFrontpageInfoBlock extends BlockBase {
     ];
   }
 
-  /**
-   * @return int
-   */
-  public function getCacheMaxAge() {
-    return 0;
-  }
-
-  /**
-   * Private function for getting random quote.
-   */
-  private function getRandQuote() {
-    $quotes = [
-      '<i>Whoever is happy will make others happy too.</i> Anne Frank',
-      '<i>The secret of getting ahead is getting started.</i> Mark Twain',
-      '<i>You can\'t blame gravity for falling in love.</i> Albert Einstein',
-      '<i>The weak can never forgive. Forgiveness is the attribute of the strong.</i> Mahatma Gandhi',
-    ];
-    return $quotes[array_rand($quotes)];
-  }
-
 }
