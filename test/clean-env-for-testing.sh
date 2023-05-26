b52c219198cef566834e8b81601cebc25dc04c1b
Janne Suominen <janne.suominen@siili.com>
2023-05-26 16:00:01 +0300

    AU-1007: Add script to delete documents via user id or business id.

--- test/clean-env-for-testing.sh
+++ test/clean-env-for-testing.sh
@@ -1,78 +1,78 @@
 #!/usr/bin/env bash
 
-stringContain() { [ -z "$1" ] || { [ -z "${2##*$1*}" ] && [ -n "$2" ]; }; }
-
 . /app/test/.test_env
 
-echo $ATV_BASE_URL
-echo $USER_IDT
+# first loop user uuids
+for uuid in "${USER_IDT[@]}"; do
+  # create url
+  user_url="$ATV_BASE_URL/$ATV_VERSION/documents/?lookfor=appenv%3A$APP_ENV&service_name=$ATV_SERVICE&user_id=$uuid"
+  echo "$user_url"
+  # loop while there's next result
+  while [ "$user_url" != "null" ]; do
+    # Fetch the next page of results
+    RESPONSE=$(curl --location "$user_url" \
+      --header 'Accept-Encoding: utf8' \
+      --header "X-Api-Key: $ATV_API_KEY")
 
-curl --location 'https://atv-api-hki-kanslia-atv-test.agw.arodevtest.hel.fi/v1/statistics/?services=AvustushakemusIntegraatio&types=KUVAPROJ' \
-  --header 'Accept-Encoding: utf8' \
-  --header 'X-Api-Key: ' |
-  jq -r '.results[] | "\(.id) \(.transaction_id) \(.business_id)"' |
-  while read -r line; do
-    transaction_id=$(echo "$line" | awk '{print $2}')
-    if [[ "$transaction_id" == *"$APP_ENV"* ]]; then
-      echo "$line"
-    fi
+    new_results=()
+
+    # Extract the results from the response and append them to the array
+    while IFS= read -r result; do
+      new_results+=("$result")
+    done < <(echo "$RESPONSE" | jq -r --arg filter "$APP_ENV" '.results[] | "\(.id) \(.transaction_id) \(.type) \(.business_id)"')
+
+    # loop parsed results
+    for result in "${new_results[@]}"; do
+      # Split the result into individual variables
+      read -r id transaction_id type business_id <<<"$result"
+      # create delete url
+      delete_by_user_url="$ATV_BASE_URL/$ATV_VERSION/documents/$id"
+      echo "$delete_by_user_url"
+
+      DELETERESPONSE=$(curl --location "$delete_by_user_url" --request DELETE \
+                             --header 'Accept-Encoding: utf8' \
+                             --header "X-Api-Key: $ATV_API_KEY")
+    done
+
+    # Get the URL of the next page of results, or set it to "null" if there are no more pages
+    user_url=$(echo "$RESPONSE" | jq -r '.next')
   done
 
-url='https://atv-api-hki-kanslia-atv-test.agw.arodevtest.hel.fi/v1/statistics/?services=AvustushakemusIntegraatio&types=KUVAPROJ'
-all_results=()
-
-while [ "$url" != "null" ]; do
-  # Fetch the next page of results
-  RESPONSE=$(curl --location "$url" \
-               --header 'Accept-Encoding: utf8' \
-               --header 'X-Api-Key: ')
-
-  # Extract the results from the response and append them to the array
-  new_results=$(echo "$RESPONSE" | jq -r '.results[] | "\(.id) \(.transaction_id) \(.business_id)"')
-  all_results+=( $new_results )
-
-  # Get the URL of the next page of results, or set it to "null" if there are no more pages
-  url=$(echo "$RESPONSE" | jq -r '.next')
 done
 
-echo "RESULTS: $all_results"
+for business_id in "${Y_TUNNUKSET[@]}"; do
+  business_url="$ATV_BASE_URL/$ATV_VERSION/documents/?lookfor=appenv%3A$APP_ENV&service_name=$ATV_SERVICE&business_id=$business_id"
+  echo "$business_url"
 
-#for result in "${results[@]}"; do
-#    id=$(echo "$result" | cut -d ' ' -f 1)
-#    transaction_id=$(echo "$result" | cut -d ' ' -f 2)
-#    business_id=$(echo "$result" | cut -d ' ' -f 3)
-#
-#    echo "$id $transaction_id, $business_id"
-#done
+  while [ "$business_url" != "null" ]; do
+    # Fetch the next page of results
+    RESPONSE=$(curl --location "$business_url" \
+      --header 'Accept-Encoding: utf8' \
+      --header "X-Api-Key: $ATV_API_KEY")
 
-#    trimmed_transaction_id=$(echo "$transaction_id" | sed -e 's/^[[:space:]]*//' -e 's/[[:space:]]*$//')
-#    if [[ "$trimmed_transaction_id" == *"$APP_ENV"* ]]; then
-#      echo "It's there!"
-#    fi
+    new_results=()
 
-#    trimmed_transaction_id=$(echo "$transaction_id" | tr -dc '[:print:]')
-#    if [[ "$trimmed_transaction_id" == *"$APP_ENV"* ]]; then
-#      echo "It's there!"
-#    fi
-#while read -r id transaction_id business_id; do
-#    echo "Do whatever with ${id} ${transaction_id} ${business_id}"
-#done< <($output | jq '.results | "\(.id) \(.transaction_id) \(.business_id)"')
-#
-#
-#
-#curl -s 'localhost:14002/api/sno' |
-#jq -r '.satellites[].id' |
-#while IFS= read -r id; do
-#        curl -s 'localhost:14002/api/sno/satellite/'"$id"
-#done |
-#jq -r \
-#        --argjson auditScore 1 \
-#        --argjson suspensionScore 1 \
-#        --argjson onlineScore 0.9 \
-#        '.audits as $a | $a.satelliteName as $name |
-#        reduce ($ARGS.named|keys[]) as $key (
-#                [];
-#                if $a[$key] < $ARGS.named[$key] then (
-#                        . + ["\($key) below threshold: \($a[$key]) for \($name)"]
-#                ) else . end
-#        ) | .[]'
+    # Extract the results from the response and append them to the array
+    while IFS= read -r result; do
+      new_results+=("$result")
+    done < <(echo "$RESPONSE" | jq -r --arg filter "$APP_ENV" '.results[] | "\(.id) \(.transaction_id) \(.type) \(.business_id)"')
+
+    for result in "${new_results[@]}"; do
+      # Split the result into individual variables
+      read -r id transaction_id type business_id <<<"$result"
+
+      delete_by_user_url="$ATV_BASE_URL/$ATV_VERSION/documents/$id"
+      echo "$delete_by_user_url"
+
+
+      DELETERESPONSE=$(curl --location "$delete_by_user_url" --request DELETE \
+                             --header 'Accept-Encoding: utf8' \
+                             --header "X-Api-Key: $ATV_API_KEY")
+      echo "$DELETERESPONSE"
+    done
+
+    # Get the URL of the next page of results, or set it to "null" if there are no more pages
+    business_url=$(echo "$RESPONSE" | jq -r '.next')
+  done
+
+done
