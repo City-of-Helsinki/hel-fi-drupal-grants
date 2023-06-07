c6f224f09e44907a4f94c4b9122f58c59504d477
Petri Leinonen <petri.leinonen@siili.com>
2023-06-07 12:41:51 +0300

    AU-1054: initial commit

---
+++ public/modules/custom/grants_profile/js/profile_dialog.js
@@ -0,0 +1,110 @@
+// eslint-disable-next-line no-unused-vars
+(($, Drupal, drupalSettings) => {
+  var unsaved = false;
+  /**
+   * Unsaved changes.
+   *
+   * @type {Drupal~behavior}
+   *
+   * @prop {Drupal~behaviorAttach} attach
+   *   Attaches the behavior for unsaved changes.
+   */
+  Drupal.behaviors.profileFormUnsaved = {
+    clear: function () {
+      // Allow Ajax refresh/redirect to clear unsaved flag.
+      // @see Drupal.AjaxCommands.prototype.webformRefresh
+      unsaved = false;
+    },
+    get: function () {
+      // Get the current unsaved flag state.
+      return unsaved;
+    },
+    set: function (value) {
+      // Set the current unsaved flag state.
+      unsaved = value;
+    },
+    attach: function (context) {
+      // Look for the 'data-webform-unsaved' attribute which indicates that
+      // a multi-step webform has unsaved data.
+      // @see \Drupal\webform\WebformSubmissionForm::buildForm
+
+      if ($('.js-webform-unsaved[data-webform-unsaved]').once('data-webform-unsaved').length) {
+        unsaved = true;
+      }
+      else {
+        $('.js-webform-unsaved :input:not(:button, :submit, :reset, [type="hidden"])').once('webform-unsaved').on('change keypress', function (event, param1) {
+          // Ignore events triggered when #states API is changed,
+          // which passes 'webform.states' as param1.
+          // @see webform.states.js ::triggerEventHandlers().
+          if (param1 !== 'webform.states') {
+            unsaved = true;
+          }
+        });
+      }
+
+      $('.js-webform-unsaved button, .js-webform-unsaved input[type="submit"]', context)
+        .once('webform-unsaved')
+        .not('[data-webform-unsaved-ignore]')
+        .on('click', function (event) {
+          // For reset button we must confirm unsaved changes before the
+          // before unload event handler.
+          if ($(this).hasClass('webform-button--reset') && unsaved) {
+            if (!window.confirm(Drupal.t('Changes you made may not be saved.') + '\n\n' + Drupal.t('Press OK to leave this page or Cancel to stay.'))) {
+              return false;
+            }
+          }
+
+          unsaved = false;
+        });
+
+      // Add submit handler to form.beforeSend.
+      // Update Drupal.Ajax.prototype.beforeSend only once.
+      if (typeof Drupal.Ajax !== 'undefined' && typeof Drupal.Ajax.prototype.beforeSubmitWebformUnsavedOriginal === 'undefined') {
+        Drupal.Ajax.prototype.beforeSubmitWebformUnsavedOriginal = Drupal.Ajax.prototype.beforeSubmit;
+        Drupal.Ajax.prototype.beforeSubmit = function (form_values, element_settings, options) {
+          unsaved = false;
+          return this.beforeSubmitWebformUnsavedOriginal.apply(this, arguments);
+        };
+      }
+
+      // Track all CKEditor change events.
+      // @see https://ckeditor.com/old/forums/Support/CKEditor-jQuery-change-event
+      if (window.CKEDITOR && !CKEDITOR.webformUnsaved) {
+        CKEDITOR.webformUnsaved = true;
+        CKEDITOR.on('instanceCreated', function (event) {
+          event.editor.on('change', function (evt) {
+            unsaved = true;
+          });
+        });
+      }
+    }
+  };
+  $('a').on('click', function (event) {
+    var unset_name = true
+    if ($('#edit-companynamewrapper-companyname').val() == '') {
+      unset_name = true
+    }
+    console.log(unsaved)
+    let containingElement = document.querySelector('form');
+    if ((unsaved || unset_name) && !containingElement.contains( event.target )) {
+      event.preventDefault();
+      const $previewDialog = $(
+        `<div></div>`,
+      ).appendTo('body');
+      Drupal.dialog($previewDialog, {
+        title: Drupal.t('You have unsaved changes in your profile. Please save or cancel them'),
+        width: '33%',
+        buttons: [
+          {
+            text: Drupal.t('Back to profile'),
+            buttonType: 'primary',
+            click() {
+              $(this).dialog('close');
+            },
+          },
+        ],
+      }).showModal();
+    }
+    // eslint-disable-next-line no-undef
+  })
+})(jQuery, Drupal, drupalSettings);
