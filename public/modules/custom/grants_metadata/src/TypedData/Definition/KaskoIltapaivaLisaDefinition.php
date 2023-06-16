62136ba9802afc4c77abafa1cff0470e404b3e1c
Janne Suominen <janne.suominen@siili.com>
2023-06-16 13:50:28 +0300

    AU-978: Add config + datatype

---
+++ public/modules/custom/grants_metadata/src/TypedData/Definition/KaskoIltapaivaLisaDefinition.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace Drupal\grants_metadata\TypedData\Definition;
+
+use Drupal\Core\TypedData\ComplexDataDefinitionBase;
+use Drupal\Core\TypedData\DataDefinition;
+use Drupal\Core\TypedData\ListDataDefinition;
+
+/**
+ * Define KaskoYleisavustusDefinition.php data.
+ */
+class KaskoIltapaivaLisaDefinition extends ComplexDataDefinitionBase {
+
+  use ApplicationDefinitionTrait;
+
+  /**
+   * Base data definitions for all.
+   *
+   * @return array
+   *   Property definitions.
+   */
+  public function getPropertyDefinitions(): array {
+    if (!isset($this->propertyDefinitions)) {
+
+      $info = &$this->propertyDefinitions;
+
+      foreach ($this->getBaseProperties() as $key => $property) {
+        $info[$key] = $property;
+      }
+
+
+    }
+    return $this->propertyDefinitions;
+  }
+
+  /**
+   * Override property definition.
+   *
+   * @param string $name
+   *   Property name.
+   *
+   * @return \Drupal\Core\TypedData\DataDefinitionInterface|void|null
+   *   Property definition.
+   */
+  public function getPropertyDefinition($name) {
+    $retval = parent::getPropertyDefinition($name);
+    return $retval;
+  }
+
+}
