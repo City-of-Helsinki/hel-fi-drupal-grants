5af4341efde0a92237d36423452da58f4e4596a0
Suvi Kankare <suvi.kankare@vincit.fi>
2023-08-08 14:23:36 +0300

    AU-1414: add preview link module

---
+++ public/modules/custom/grants_preview_link/src/Plugin/Block/GrantsPreviewLinkBlock.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Drupal\grants_preview_link\Plugin\Block;
+
+use Drupal\Core\Block\BlockBase;
+use Drupal\Core\Url;
+
+/**
+ * Provides a grants Preview Link block.
+ *
+ * @Block(
+ *   id = "grants_preview_link",
+ *   admin_label = @Translation("Grants Preview Link"),
+ *   category = @Translation("Oma Asiointi")
+ * )
+ */
+class GrantsPreviewLinkBlock extends BlockBase {
+
+  /**
+   * {@inheritdoc}
+   *
+   * @throws \GuzzleHttp\Exception\GuzzleException
+   */
+  public function build() {
+
+    $node = \Drupal::routeMatch()->getParameter('node');
+    $webformArray = $node->get('field_webform')->getValue();
+    $webformName = $webformArray[0]['target_id'];
+
+    $link = Url::fromRoute('grants_webform_print.print_webform',
+    [
+      'webform' => $webformName,
+    ], ['absolute' => TRUE]);
+
+    $build = [
+      '#theme' => 'grants_preview_link',
+      '#webformLink' => $link,
+    ];
+    return $build;
+  }
+
+}
