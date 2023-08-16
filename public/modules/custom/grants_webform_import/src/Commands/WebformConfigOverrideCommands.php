def60238684e9bbc4666f1dbabfcdc732f09dcd0
Alexander Tallqvist <alexander.tallqvist@siili.com>
2023-08-16 11:45:41 +0300

    AU-1408: Finished the webform config override feature.

--- public/modules/custom/grants_webform_import/src/Commands/WebformConfigOverrideCommands.php
+++ public/modules/custom/grants_webform_import/src/Commands/WebformConfigOverrideCommands.php
@@ -2,152 +2,34 @@
 
 namespace Drupal\grants_webform_import\Commands;
 
-use Drupal\Component\Utility\NestedArray;
-use Drupal\config\StorageReplaceDataWrapper;
 use Drupal\Core\Config\ConfigFactoryInterface;
-use Drupal\Core\Config\ConfigImporter;
-use Drupal\Core\Config\ConfigImporterException;
-use Drupal\Core\Config\ConfigManagerInterface;
-use Drupal\Core\Config\Importer\ConfigImporterBatch;
-use Drupal\Core\Config\StorageComparer;
-use Drupal\Core\Config\StorageInterface;
-use Drupal\Core\Config\TypedConfigManagerInterface;
-use Drupal\Core\Extension\ModuleExtensionList;
-use Drupal\Core\Extension\ModuleHandlerInterface;
-use Drupal\Core\Extension\ModuleInstallerInterface;
-use Drupal\Core\Extension\ThemeHandlerInterface;
-use Drupal\Core\Lock\LockBackendInterface;
 use Drupal\Core\Site\Settings;
-use Drupal\Core\StringTranslation\TranslationInterface;
 use Drush\Commands\DrushCommands;
 use Symfony\Component\Yaml\Parser;
-use Symfony\Contracts\EventDispatcher\EventDispatcherInterface;
 use Webmozart\PathUtil\Path;
 
 /**
  * Class to import overridden Webform configurations.
- *
- * This class is based on config_import_single module in drupal.org.
  *
  * @package Drupal\grants_webform_import\Commands
  */
 class WebformConfigOverrideCommands extends DrushCommands {
 
   /**
-   * CachedStorage.
-   *
-   * @var \Drupal\Core\Config\StorageInterface
-   */
-  private $storage;
-
-  /**
-   * Event dispatcher.
-   *
-   * @var \Symfony\Contracts\EventDispatcher\EventDispatcherInterface
-   */
-  private $eventDispatcher;
-
-  /**
-   * Config manager.
-   *
-   * @var \Drupal\Core\Config\ConfigManagerInterface
-   */
-  private $configManager;
-
-  /**
-   * Lock.
-   *
-   * @var \Drupal\Core\Lock\LockBackendInterface
-   */
-  private $lock;
-
-  /**
-   * Config typed.
-   *
-   * @var \Drupal\Core\Config\TypedConfigManagerInterface
-   */
-  private $configTyped;
-
-  /**
-   * ModuleHandler.
-   *
-   * @var \Drupal\Core\Extension\ModuleHandlerInterface
-   */
-  private $moduleHandler;
-
-  /**
-   * Module installer.
-   *
-   * @var \Drupal\Core\Extension\ModuleInstallerInterface
-   */
-  private $moduleInstaller;
-
-  /**
-   * Theme handler.
-   *
-   * @var \Drupal\Core\Extension\ThemeHandlerInterface
-   */
-  private $themeHandler;
-
-  /**
-   * String translation.
-   *
-   * @var \Drupal\Core\StringTranslation\TranslationInterface
-   */
-  private $stringTranslation;
-
-  /**
-   * Extension list module.
-   *
-   * @var \Drupal\Core\Extension\ModuleExtensionList
-   */
-  private $extensionListModule;
-
-  /**
-   * Config factory.
+   * The ConfigFactoryInterface.
    *
    * @var \Drupal\Core\Config\ConfigFactoryInterface
    */
-  private $configFactory;
+  private ConfigFactoryInterface $configFactory;
 
   /**
-   * ConfigImportSingleCommands constructor.
+   * Class constructor.
    *
-   * @param \Drupal\Core\Config\StorageInterface $storage
-   *   Storage.
-   * @param \Symfony\Contracts\EventDispatcher\EventDispatcherInterface $eventDispatcher
-   *   Event Dispatcher.
-   * @param \Drupal\Core\Config\ConfigManagerInterface $configManager
-   *   Config Manager.
-   * @param \Drupal\Core\Lock\LockBackendInterface $lock
-   *   Lock.
-   * @param \Drupal\Core\Config\TypedConfigManagerInterface $configTyped
-   *   Config typed.
-   * @param \Drupal\Core\Extension\ModuleHandlerInterface $moduleHandler
-   *   Module handler.
-   * @param \Drupal\Core\Extension\ModuleInstallerInterface $moduleInstaller
-   *   Module Installer.
-   * @param \Drupal\Core\Extension\ThemeHandlerInterface $themeHandler
-   *   Theme handler.
-   * @param \Drupal\Core\StringTranslation\TranslationInterface $stringTranslation
-   *   String Translation.
-   * @param \Drupal\Core\Extension\ModuleExtensionList $extensionListModule
-   *   Extension list module.
    * @param \Drupal\Core\Config\ConfigFactoryInterface $configFactory
-   *   Config factory.
+   *   The ConfigFactoryInterface.
    */
-  public function __construct(StorageInterface $storage, EventDispatcherInterface $eventDispatcher, ConfigManagerInterface $configManager, LockBackendInterface $lock, TypedConfigManagerInterface $configTyped, ModuleHandlerInterface $moduleHandler, ModuleInstallerInterface $moduleInstaller, ThemeHandlerInterface $themeHandler, TranslationInterface $stringTranslation, ModuleExtensionList $extensionListModule, ConfigFactoryInterface $configFactory) {
+  public function __construct(ConfigFactoryInterface $configFactory) {
     parent::__construct();
-    $this->storage = $storage;
-    $this->eventDispatcher = $eventDispatcher;
-    $this->configManager = $configManager;
-    $this->lock = $lock;
-    $this->configTyped = $configTyped;
-    $this->moduleHandler = $moduleHandler;
-    $this->moduleInstaller = $moduleInstaller;
-    $this->themeHandler = $themeHandler;
-    $this->stringTranslation = $stringTranslation;
-    $this->extensionListModule = $extensionListModule;
     $this->configFactory = $configFactory;
   }
 
@@ -163,39 +45,97 @@ class WebformConfigOverrideCommands extends DrushCommands {
    * @throws \Exception
    */
   public function webformConfigOverride() {
-    $overrides = $this->loadOverrides();
+    $overrides = $this->getOverrides();
+    $mapping = $this->getApplicationTypeIdMapping();
 
     if (!$overrides) {
-      $this->output()->writeln("No overrides were found. Aborting.");
+      $this->output()
+        ->writeln("No overrides were found. Aborting.");
       return;
     }
 
-    $mapping = $this->getApplicationTypeIdMapping();
-    $config_factory = \Drupal::configFactory();
+    if (!$mapping) {
+      $this->output()
+        ->writeln("Application type ID -> Machine name mapping could not be established. Aborting.");
+      return;
+    }
 
+    if (getenv('APP_ENV') == 'production') {
+      $this->output()
+        ->writeln("Command not allowed in production. Aborting.");
+      return;
+    }
+
+    $this->override($overrides, $mapping);
+  }
+
+  /**
+   * The override method.
+   *
+   * This method performs the overriding of Webform
+   * configurations. The overrides are only
+   * performed on the information in the DB, and not on
+   * the actual configuration files.
+   *
+   * @param array $overrides
+   *   The overrides we are implementing.
+   * @param array $mapping
+   *   An "Application type id" -> "Machine name" map.
+   */
+  private function override(array $overrides, array $mapping): void {
     foreach ($overrides as $override) {
       $applicationTypeId = key($override);
-      $newGrantsMetadata = reset($override[$applicationTypeId]);
-      $config = $config_factory->getEditable($mapping[$applicationTypeId]);
-      $originalGrantsMetadata = $config->get('third_party_settings.grants_metadata');
+      $configurationOverrides = $override[$applicationTypeId]['grants_metadata'];
+      $configurationName = $mapping[$applicationTypeId];
 
-      if ($newGrantsMetadata && $originalGrantsMetadata) {
-        $overriddenConfiguration = array_merge($originalGrantsMetadata, $newGrantsMetadata);
+      $config = $this->configFactory->getEditable($configurationName);
+      $originalConfiguration = $config->get('third_party_settings.grants_metadata');
+
+      if ($configurationOverrides && $originalConfiguration) {
+        $overriddenConfiguration = array_merge($originalConfiguration, $configurationOverrides);
         $config->set('third_party_settings.grants_metadata', $overriddenConfiguration);
         $config->save();
-        $this->output()->writeln("Imported overrides for $mapping[$applicationTypeId] ($applicationTypeId).");
+
+        $this->output()
+          ->writeln("Imported overrides for $mapping[$applicationTypeId] ($applicationTypeId).\n");
+
+        # Debug printing.
+        # dump($configurationOverrides);
+        # dump($originalConfiguration);
+        # dump($overriddenConfiguration);
       }
     }
   }
 
   /**
-   * The loadOverrides method.
+   * The getOverrides method.
+   *
+   * This method gets the Webform configuration overrides from
+   * the "grants_metadata.settings.yml" file. The overrides should
+   * be located under the "overridden_configuration" key.
+   * The overrides presented in the file should have a structure that matches
+   * the structure in the forms own configuration file.
+   *
+   * Example:
+   *
+   * overridden_configuration:
+   *  - 53:
+   *      grants_metadata:
+   *        applicationOpen: '2024-06-16T08:30:52'
+   *        applicationClose: '2025-08-19T08:36:07'
+   *        disableCopying: 1
+   *  - 47:
+   *      grants_metadata:
+   *        applicationOpen: '2024-06-16T08:30:52'
+   *        applicationClose: '2025-08-19T08:36:07'
+   *        disableCopying: 1
    *
    * @return false|mixed
+   *   Returns either the configuration overrides or FALSE.
    */
-  private function loadOverrides(): mixed {
-    $directory = Settings::get('config_sync_directory');
+  private function getOverrides(): mixed {
     $parser = new Parser();
+    $directory = Settings::get('config_sync_directory');
 
     $configurationYamlFile = $directory . '/grants_metadata.settings.yml';
     $configurationSettings = $parser->parse(file_get_contents($configurationYamlFile));
@@ -204,20 +144,32 @@ class WebformConfigOverrideCommands extends DrushCommands {
     if (!$configurationSettings || !isset($configurationSettings['overridden_configuration'])) {
       return FALSE;
     }
-
     return $configurationSettings['overridden_configuration'];
   }
 
   /**
    * The getApplicationTypeIdMapping method.
    *
+   * This method builds a map between Webform application
+   * type IDs and their corresponding machine names. The map is
+   * structured like this:
+   *
+   * [
+   *  53 => "webform.webform.kasko_ip_lisa",
+   *  51 => "webform.webform.kasvatus_ja_koulutus_yleisavustu",
+   *  48 => "webform.webform.kuva_projekti",
+   *  47 => "webform.webform.kuva_toiminta"
+   * ]
+   *
    * @return array
+   *   An array containing an "Application type id" -> "Machine name"
+   *   map.
    */
   private function getApplicationTypeIdMapping(): array {
     $parser = new Parser();
-    $mapping = [];
     $configurationDirectory = Settings::get('config_sync_directory');
     $webformConfigurationFiles = glob($configurationDirectory . '/webform.webform.*');
+    $mapping = [];
 
     foreach ($webformConfigurationFiles as $file) {
       $name = Path::getFilenameWithoutExtension($file);
@@ -227,11 +179,12 @@ class WebformConfigOverrideCommands extends DrushCommands {
         continue;
       }
 
-      // Here we can implement logic regarding form versions.
+      // TODO: Implement logic regarding form versions.
       $applicationTypeID = $formConfiguration['third_party_settings']['grants_metadata']['applicationTypeID'];
-      $mapping[$applicationTypeID] = $name;
+      if ($name && $applicationTypeID) {
+        $mapping[$applicationTypeID] = $name;
+      }
     }
-
     return $mapping;
   }
 
