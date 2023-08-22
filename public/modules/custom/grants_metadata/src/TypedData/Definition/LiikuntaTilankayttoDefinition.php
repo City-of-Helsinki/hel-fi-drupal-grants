c6c93f864bd5454b99fc3627769718edeece7cd2
Oskari Nevalainen <oskari@mysteerio.fi>
2023-08-22 14:10:41 +0300

    AU-1293: AU-1296: Data mappings for Liikunta tilankautto

--- public/modules/custom/grants_metadata/src/TypedData/Definition/LiikuntaTilankayttoDefinition.php
+++ public/modules/custom/grants_metadata/src/TypedData/Definition/LiikuntaTilankayttoDefinition.php
@@ -28,6 +28,7 @@ class LiikuntaTilankayttoDefinition extends ComplexDataDefinitionBase {
         $info[$key] = $property;
       }
 
+      // Section 2: Avustustiedot.
       $info['subventions'] = ListDataDefinition::create('grants_metadata_compensation_type')
         ->setLabel('compensationArray')
         ->setSetting('jsonPath', [
@@ -45,12 +46,84 @@ class LiikuntaTilankayttoDefinition extends ComplexDataDefinitionBase {
           'purpose',
         ]);
 
+      $info['compensation_explanation'] = DataDefinition::create('string')
+        ->setLabel('compensationInfo=>explanation')
+        ->setSetting('defaultValue', "")
+        ->setSetting('jsonPath', [
+          'compensation',
+          'compensationInfo',
+          'generalInfoArray',
+          'explanation',
+        ])
+        ->setSetting('webformDataExtracter', [
+          'service' => 'grants_metadata.atv_schema',
+          'method' => 'returnRelations',
+          'mergeResults' => TRUE,
+          'arguments' => [
+            'relations' => [
+              'master' => 'compensation_explanation',
+              'slave' => 'compensation_boolean',
+              'type' => 'boolean',
+            ],
+          ],
+        ]);
+
       $info['seuran_yhdistyksen_saamat_vuokrat_edellisen_kalenterivuoden_ajal'] = ListDataDefinition::create('grants_rent_income')
         ->setSetting('jsonPath', [
           'compensation',
           'compensationInfo',
           'rentIncomesArray',
         ]);
+
+      // Section 3: Yhteisön toiminta.
+      $mappings = [
+        'miehet_20_63_vuotiaat_' => 'menGlobal',
+        'joista_helsinkilaisia_miehet_20_63' => 'menLocal',
+        'naiset_20_63_vuotiaat_' => 'womenGlobal',
+        'joista_helsinkilaisia_naiset_20_63' => 'womenLocal',
+        'muut_20_63_vuotiaat_' => 'adultOthersGlobal',
+        'joista_helsinkilaisia_muut_20_63' => 'adultOthersLocal',
+        'miehet_64' => '',
+        'joista_helsinkilaisia_miehet_64' => '',
+        'naiset_64' => '',
+        'joista_helsinkilaisia_naiset_64' => '',
+        'muut_64' => '',
+        'joista_helsinkilaisia_muut_64' => '',
+        'pojat_20' => 'boysGlobal',
+        'joista_helsinkilaisia_pojat_20' => 'boysLocal',
+        'tytot_20' => 'girlsGlobal',
+        'joista_helsinkilaisia_tytot_20' => 'girlsLocal',
+        'muut_20' => 'juniorOthersGlobal',
+        'joista_helsinkilaisia_muut_20' => 'juniorOthersLocal',
+        'miehet_20_63_vuotiaat_aktiiviharrastajat' => 'activeFanciersMenGlobal',
+        'joista_helsinkilaisia_miehet_20_63_aktiiviharrastajat' => 'activeFanciersMenLocal',
+        'naiset_20_63_vuotiaat_aktiiviharrastajat' => 'activeFanciersWomenGlobal',
+        'joista_helsinkilaisia_naiset_20_63_aktiiviharrastajat' => 'activeFanciersWomenLocal',
+        'muut_20_63_vuotiaat_aktiiviharrastajat' => 'activeFanciersAdultOthersGlobal',
+        'joista_helsinkilaisia_muut_20_63_aktiiviharrastajat' => 'activeFanciersAdultOthersLocal',
+        'miehet_64_aktiiviharrastajat' => '',
+        'joista_helsinkilaisia_miehet_64_aktiiviharrastajat' => '',
+        'naiset_64_aktiiviharrastajat' => '',
+        'joista_helsinkilaisia_naiset_64_aktiiviharrastajat' => '',
+        'muut_64_aktiiviharrastajat' => '',
+        'joista_helsinkilaisia_muut_64_aktiiviharrastajat' => '',
+        'pojat_20_aktiiviharrastajat' => 'activeFanciersBoysGlobal',
+        'joista_helsinkilaisia_pojat_20_aktiiviharrastajat' => 'activeFanciersBoysLocal',
+        'tytot_20_aktiiviharrastajat' => 'activeFanciersGirlsLocal',
+        'joista_helsinkilaisia_tytot_20_aktiiviharrastajat' => 'activeFanciersGirlsLocal',
+        'muut_20_aktiiviharrastajat' => 'activeFanciersJuniorOthersGlobal',
+        'joista_helsinkilaisia_muut_20_aktiiviharrastajat' => 'activeFanciersJuniorOthersLocal',
+        'valmentajien_ohjaajien_maara_edellisena_vuonna_yhteensa' => 'allCoaches',
+        'joista_valmentaja_ja_ohjaajakoulutuksen_vok_1_5_tason_koulutukse' => 'level1to5Coaches',
+      ];
+
+      foreach ($mappings as $key => $value) {
+        if (empty($value)) {
+          // Not yet implemented in avus2.
+          continue;
+        }
+        $this->createRepeatedMembershipDefinitions($key, $value, $info);
+      }
 
       $info['club_section'] = ListDataDefinition::create('grants_club_section')
         ->setSetting('jsonPath', [
@@ -61,6 +134,32 @@ class LiikuntaTilankayttoDefinition extends ComplexDataDefinitionBase {
 
     }
     return $this->propertyDefinitions;
+  }
+
+  /**
+   * Helper function generate repetitive definition.
+   *
+   * @param string $key
+   *   Webform element key.
+   * @param string $jsonPath
+   *   Last part of the JSON path.
+   * @param array $info
+   *   Data definitions.
+   */
+  private function createRepeatedMembershipDefinitions($key, $jsonPath, &$info) {
+    $info[$key] = DataDefinition::create('integer')
+      ->setSetting('jsonPath', [
+        'membersInfo',
+        'membersInfoArray',
+        $jsonPath,
+      ])->setSetting('valueCallback', [
+        '\Drupal\grants_handler\Plugin\WebformHandler\GrantsHandler',
+        'convertToInt',
+      ])
+      ->setSetting('typeOverride', [
+        'dataType' => 'string',
+        'jsonType' => 'int',
+      ]);
   }
 
   /**
