dbe2d386bfa6539772bf6eae922cacef538ecf05
Suvi Kankare <suvi.kankare@vincit.fi>
2023-08-25 15:48:50 +0300

    AU-1285: start

---
+++ public/modules/custom/grants_budget_components/src/Element/GrantsBudgetCostTotal.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace Drupal\grants_budget_components\Element;
+
+use Drupal\Core\Form\FormStateInterface;
+use Drupal\grants_handler\Processor\NumberProcessor;
+use Drupal\webform\Element\WebformCompositeBase;
+
+/**
+ * Provides a 'grants_budget_cost_total'.
+ *
+ * Webform composites contain a group of sub-elements.
+ *
+ *
+ * IMPORTANT:
+ * Webform composite can not contain multiple value elements (i.e. checkboxes)
+ * or composites (i.e. webform_address)
+ *
+ * @FormElement("grants_budget_cost_total")
+ *
+ * @see \Drupal\webform\Element\WebformCompositeBase
+ * @see \Drupal\webform_example_composite\Element\WebformExampleComposite
+ */
+class GrantsBudgetCostTotal extends WebformCompositeBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getInfo() {
+    return parent::getInfo() + ['#theme' => 'webform_grants_budget'];
+  }
+
+  // @codingStandardsIgnoreStart
+
+  /**
+   * Process default values and values from submitted data.
+   *
+   * @param array $element
+   *   Element that is being processed.
+   * @param \Drupal\Core\Form\FormStateInterface $form_state
+   *   Form state.
+   * @param array $complete_form
+   *   Full form.
+   *
+   * @return array[]
+   *   Form API element for webform element.
+   */
+  public static function processWebformComposite(&$element, FormStateInterface $form_state, &$complete_form): array {
+
+    $element['#tree'] = TRUE;
+    $element = parent::processWebformComposite($element, $form_state, $complete_form);
+    $dataForElement = $element['#value'];
+
+    $jtyt = $element;
+
+    if (isset($dataForElement['costTotal'])) {
+      $element['costTotal']['#value'] = $dataForElement['costTotal'];
+    }
+
+    if (empty($element['costTotal']['#value']) && isset($element['#incomeGroup'])) {
+      $element['costTotal']['#value'] = $element['#incomeGroup'];
+    }
+
+    return $element;
+  }
+
+  // @codingStandardsIgnoreEnd
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function getCompositeElements(array $element) {
+    $elements = [];
+
+    $elements['costTotal'] = [
+      '#type' => 'text',
+      '#title' => t('Total costs'),
+      // Add .js-form-wrapper to wrapper (ie td) to prevent #states API from
+      // disabling the entire table row when this element is disabled.
+      '#wrapper_attributes' => ['class' => 'js-form-wrapper'],
+    ];
+    return $elements;
+  }
+
+}
