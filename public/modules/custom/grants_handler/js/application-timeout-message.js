96e72ec50197f6a04ce9bc08265e674a841b1eb0
Alexander Tallqvist <alexander.tallqvist@siili.com>
2023-09-08 13:48:49 +0300

    AU-210: Continued on the timeout message implementation.

--- public/modules/custom/grants_handler/js/application-timeout-message.js
+++ public/modules/custom/grants_handler/js/application-timeout-message.js
@@ -3,26 +3,63 @@
   'use strict';
 
   Drupal.behaviors.applicationTimeoutMessage = {
+
+    /**
+     * Attach the behavior.
+     *
+     * @param context
+     *   The context.
+     * @param settings
+     *   Drupal settings.
+     */
     attach: function(context, settings) {
-
-      const formTimestamp = settings.grants_handler.settings.formTimestamp;
-      let intervalId;
-
-      if (typeof formTimestamp !== 'undefined') {
-        checkTimeout();
-        intervalId = setInterval(checkTimeout, 10000);
+      const applicationCloseTimestamp = settings.grants_handler.settings.applicationCloseTimestamp;
+      if (typeof applicationCloseTimestamp !== 'undefined') {
+        this.checkTimeout(applicationCloseTimestamp);
+        this.closeButton();
       }
+    },
 
-      function checkTimeout() {
+    /**
+     * The checkTimeout function.
+     *
+     * This function loops the hasTimeoutPassed() interval
+     * in order to check if the current time has passed an
+     * applications closing time. If the time is passed,
+     * a warning message is triggered.
+     *
+     * @param applicationCloseTimestamp
+     *   The applications closing time timestamp.
+     */
+    checkTimeout: function (applicationCloseTimestamp) {
+      let intervalId;
+      intervalId = setInterval(hasTimeoutPassed, 10000);
+
+      function hasTimeoutPassed() {
         const currentTime = new Date().toLocaleString('en-US', {timeZone: 'Europe/Helsinki'});
         const currentTimestamp = Math.floor(new Date(currentTime).getTime() / 1000);
 
-        if (currentTimestamp > formTimestamp) {
+        if (currentTimestamp > applicationCloseTimestamp) {
           const element = document.querySelector('.application-timeout-message');
           element.classList.add('slide-in');
           clearInterval(intervalId);
         }
       }
+    },
+
+    /**
+     * The closeButton function.
+     *
+     * This function adds an event listener to the close
+     * button of the timeout message. When the button is
+     * clicked, the message is hidden.
+     */
+    closeButton: function () {
+      const closeButton = document.querySelector('.close-application-timeout-message');
+      closeButton.addEventListener('click', function () {
+        const messageContainer = this.closest('.application-timeout-message');
+        messageContainer.style.display = 'none';
+      });
     }
   };
 
--- public/modules/custom/grants_handler/src/Plugin/Block/ApplicationTimeoutMessageBlock.php
+++ public/modules/custom/grants_handler/src/Plugin/Block/ApplicationTimeoutMessageBlock.php
@@ -8,6 +8,10 @@ use Drupal\Core\Cache\Cache;
 /**
  * Provides an ApplicationTimeoutMessageBlock block.
  *
+ * This block displays a dynamic message on a Webform
+ * if the application period of a form is passed when a
+ * user is filling out the form.
+ *
  * @Block(
  *   id = "application_timeout_message",
  *   admin_label = @Translation("Application Timeout Message"),
@@ -37,12 +41,19 @@ class ApplicationTimeoutMessageBlock extends BlockBase {
       return [];
     }
 
-    $formTimestamp = $webform->getThirdPartySetting('grants_metadata', 'applicationClose');
-    $formTimestamp = strtotime($formTimestamp);
+    $applicationCloseTime = $webform->getThirdPartySetting('grants_metadata', 'applicationClose');
+    $applicationCloseTimestamp = strtotime($applicationCloseTime);
+    $currentTimestamp = strtotime('now');
+
+    // Do not render this message if the form is already closed.
+    if (!$applicationCloseTimestamp || $currentTimestamp > $applicationCloseTimestamp) {
+      return [];
+    }
 
     return [
       '#theme' => 'application_timeout_message',
-      '#message' => $this->t('The application period is closed, no further editing is allowed.'),
+      '#message_heading' => $this->t('The application period for this grant has closed.', [], ['context' => 'grants_handler']),
+      '#message_body' => $this->t('You can no longer submit an application because application period for this grant has closed.', [], ['context' => 'grants_handler']),
       '#attached' => [
         'library' => [
           'grants_handler/application-timeout-message',
@@ -50,7 +61,7 @@ class ApplicationTimeoutMessageBlock extends BlockBase {
         'drupalSettings' => [
           'grants_handler' => [
             'settings' => [
-              'formTimestamp' => $formTimestamp,
+              'applicationCloseTimestamp' => $applicationCloseTimestamp,
             ],
           ],
         ],
