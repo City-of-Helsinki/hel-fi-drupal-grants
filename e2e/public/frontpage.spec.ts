4b2aec229cacc0108f0eaff6b4bd1aead8939036
Roope Jansson <roope.jansson@vincit.fi>
2023-09-21 12:37:09 +0300

    Replace RobotFramework with Playwright

---
+++ e2e/public/frontpage.spec.ts
@@ -0,0 +1,40 @@
+import { expect, test } from '@playwright/test';
+import { BASE_URL } from '../test_data';
+
+
+test.beforeEach(async ({ page }) => {
+    await page.goto(BASE_URL);
+});
+
+
+test('has title', async ({ page }) => {
+    await expect(page).toHaveTitle(/.*Avustusasiointi/);
+});
+
+
+test('has a login button', async ({ page }) => {
+    const loginLink = page.getByRole('link', { name: 'Kirjaudu' })
+    await expect(loginLink).toBeVisible()
+});
+
+
+
+
+test('contains news', async ({ page }) => {
+    const newsBlockHeader = page.getByRole('heading', { name: 'Ajankohtaista avustuksista' })
+    await expect(newsBlockHeader).toBeVisible()
+
+    // A news article is visible
+    expect(page.locator('.news-listing__item')).toBeTruthy()
+
+    const linkToNewsPage = page.getByRole('link', { name: 'Katso kaikki ajankohtaiset' })
+    await expect(linkToNewsPage).toBeVisible()
+})
+
+
+test('has sinua kiinnostaa section', async ({ page }) => {
+    await expect(page.getByRole('heading', { name: 'Sinua voisi kiinnostaa' })).toBeVisible()
+    await expect(page.getByRole('link', { name: 'Yleiset avustusehdot' })).toBeVisible()
+    await expect(page.getByRole('link', { name: 'Tilavaraukset' })).toBeVisible()
+    await expect(page.getByRole('link', { name: 'Päätökset-palvelu (Linkki johtaa ulkoiseen palveluun)' })).toBeVisible()
+});
---
+++ e2e/public/search_page.spec.ts
@@ -0,0 +1,66 @@
+import { expect, test } from '@playwright/test';
+import { BASE_URL } from '../test_data';
+
+
+test.beforeEach(async ({ page }) => {
+    await page.goto('https://hel-fi-drupal-grant-applications.docker.so/fi/etsi-avustusta');
+});
+
+
+test('has title', async ({ page }) => {
+    const pageTitle = await page.title()
+    expect(pageTitle).toContain('Etsi avustusta')
+});
+
+test('contains header', async ({ page }) => {
+    await expect(page.getByRole('heading', { name: 'Etsi avustusta' })).toBeVisible()
+});
+
+test('search filter fields are visible', async ({ page }) => {
+    await expect(page.getByRole('heading', { name: 'Rajaa hakua' })).toBeVisible()
+    await expect(page.getByText('Kohderyhmä')).toBeVisible()
+    await expect(page.getByText('Avustuslaji')).toBeVisible()
+    await expect(page.getByText('Avustuksen hakija')).toBeVisible()
+    await expect(page.getByText('Hakusana')).toBeVisible()
+    await expect(page.getByLabel('Näytä vain haettavissa olevat avustukset')).toBeVisible();
+});
+
+test('search results are initially visible', async ({ page }) => {
+    const resultCount = await page.locator(".application_search--link").count()
+    expect(resultCount).toBeTruthy()
+});
+
+test('can search for grants', async ({ page }) => {
+    const inputField = page.getByPlaceholder('Etsi nimellä tai hakusanalla, esim toiminta-avustus')
+    await inputField.fill('avustus');
+
+    await page.getByRole('button', { name: 'Etsi' }).click();
+
+    // results are visible
+    const resultCount = await page.locator(".application_search--link").count()
+    expect(resultCount).toBeGreaterThan(0)
+});
+
+test('invalid search returns no results', async ({ page }) => {
+    const inputField = page.getByPlaceholder('Etsi nimellä tai hakusanalla, esim toiminta-avustus')
+    await inputField.fill('xxxxxx');
+
+
+    await page.getByRole('button', { name: 'Etsi' }).click();
+
+    // no grants should be found
+    const resultCount = await page.locator(".application_search--link").count()
+    expect(resultCount).toBeFalsy()
+});
+
+test('search result link can be opened', async ({ page }) => {
+    const searchResultLinks = page.locator(".application_search--link")
+    const firstLink = searchResultLinks.first()
+
+    const firstLinkTitle = await firstLink.locator("h3").textContent()
+    console.log(firstLinkTitle);
+
+    await firstLink.click()
+    const pageContent = await page.locator("main").textContent()
+    expect(pageContent).toContain("Myöntämisperusteet")
+});
