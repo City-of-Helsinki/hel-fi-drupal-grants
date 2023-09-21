9a67f85f8fffcc0d5ba23116d3c1ee754a2c1ecd
Roope Jansson <roope.jansson@vincit.fi>
2023-09-21 15:18:18 +0300

    Add faker as dependency

--- e2e/public/ohjeita_hakijalle.spec.ts
+++ e2e/public/ohjeita_hakijalle.spec.ts
@@ -11,15 +11,15 @@ test.beforeEach(async ({ page }) => {
 test('instructions page accordion', async ({ page }) => {
     await page.getByRole('link', { name: 'Ohjeita hakijalle' }).first().click();
 
-    await expect(page.getByText('Osaa avustuksista voi hakea vain kerran vuodessa, mutta joillekin avustuksille')).not.toBeVisible();
+    await expect(page.getByText('Osaa avustuksista voi hakea vain kerran vuodessa, mutta joillekin avustuksille')).toBeHidden();
     await page.getByRole('button', { name: 'Avustusten hakuajat' }).first().click();
     await expect(page.getByText('Osaa avustuksista voi hakea vain kerran vuodessa, mutta joillekin avustuksille')).toBeVisible();
 
-    await expect(page.getByText('Helsingin kaupungin avustusta voidaan myöntää oikeushenkilöille, kuten rekisterö')).not.toBeVisible();
+    await expect(page.getByText('Helsingin kaupungin avustusta voidaan myöntää oikeushenkilöille, kuten rekisterö')).toBeHidden();
     await page.getByRole('button', { name: 'Kuka voi saada avustusta?' }).first().click();
     await expect(page.getByText('Helsingin kaupungin avustusta voidaan myöntää oikeushenkilöille, kuten rekisterö')).toBeVisible();
 
-    await expect(page.getByText('Avustettavan toiminnan tulee kohdistua pääosin helsinkiläisiin. Lisäksi avustuks')).not.toBeVisible();
+    await expect(page.getByText('Avustettavan toiminnan tulee kohdistua pääosin helsinkiläisiin. Lisäksi avustuks')).toBeHidden();
     await page.getByRole('button', { name: 'Mihin voi saada avustusta?' }).first().click();
     await expect(page.getByText('Avustettavan toiminnan tulee kohdistua pääosin helsinkiläisiin. Lisäksi avustuks')).toBeVisible();
 });
--- e2e/test_data.ts
+++ e2e/test_data.ts
@@ -1,5 +1,4 @@
 const TEST_SSN = "090797-999P"
-const TEST_EMAIL = "test@example.org"
 
 
-export { TEST_EMAIL, TEST_SSN }
+export { TEST_SSN }
