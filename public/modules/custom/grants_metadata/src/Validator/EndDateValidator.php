69c8fff3d3fffe670e4044a569122a6e2af86a4c
Alexander Tallqvist <alexander.tallqvist@siili.com>
2023-09-28 12:41:20 +0300

    AU-1612: fix: AU-1612: Final adjustments to the end date validation logic.

--- public/modules/custom/grants_metadata/src/Validator/EndDateValidator.php
+++ public/modules/custom/grants_metadata/src/Validator/EndDateValidator.php
@@ -11,6 +11,10 @@ class EndDateValidator {
 
   /*
    * A Webform ID <=> Date fields map.
+   *
+   * This constant maps a Webform ID to
+   * the machine names of a start and end
+   * date field on said form.
    */
   const WEBFORM_ID_DATE_FIELD_MAP = [
     'liikunta_tapahtuma' => [
@@ -38,6 +42,10 @@ class EndDateValidator {
   /**
    * Validate an end date.
    *
+   * The validation is done by comparing a pair of date fields.
+   * If the end date is before the start date, then an error
+   * is added to the form.
+   *
    * @param array $element
    *   The form element to process.
    * @param \Drupal\Core\Form\FormStateInterface $formState
@@ -46,16 +54,19 @@ class EndDateValidator {
    *   The complete form structure.
    */
   public static function validate(array &$element, FormStateInterface $formState, array &$form): void {
+    $tOpts = ['context' => 'grants_metadata'];
     $webformId = $form['#webform_id'];
-    $dateFields = self::WEBFORM_ID_DATE_FIELD_MAP[$webformId];
 
+    if (!$webformId) {
+      return;
+    }
+
+    $dateFields = self::WEBFORM_ID_DATE_FIELD_MAP[$webformId];
     $startDateValue = $formState->getValue($dateFields['start_date_field']);
     $endDateValue = $formState->getValue($dateFields['end_date_field']);
 
     $startDate = strtotime($startDateValue);
     $endDate = strtotime($endDateValue);
-
-    $tOpts = ['context' => 'grants_metadata'];
 
     // Skip this particular validation if we don't have either value.
     if (!$endDate || !$startDate) {
--- public/themes/custom/hdbt_subtheme/src/js/restrictedDatepicker.js
+++ public/themes/custom/hdbt_subtheme/src/js/restrictedDatepicker.js
@@ -5,29 +5,64 @@
   /**
    * The restrictedDatepicker behavior.
    *
-   * This behavior dynamically changes the "minDate" value
+   * This behavior dynamically changes the "min" and "value" values
    * on an "end date" field based on the selected date value
    * on a "start date" field.
    */
   Drupal.behaviors.restrictedDatepicker = {
     attach: function(context, settings) {
 
-      const dateFieldPairs = [
-        ["edit-alkaa", "edit-paattyy"],
-        ["edit-projekti-alkaa", "edit-projekti-loppuu"],
-        ["edit-alkaen", "edit-paattyy"],
-        ["edit-hanke-alkaa", "edit-hanke-loppuu"],
-      ];
-
-      for (let pair of dateFieldPairs) {
-        let startDateInput = document.getElementById(pair[0]);
-        let endDateInput = document.getElementById(pair[1]);
-
-        if (startDateInput && endDateInput) {
-          startDateInput.addEventListener("input", function () {
-            endDateInput.min = startDateInput.value;
-          });
+      /*
+       * A Webform ID <=> Date fields map.
+       *
+       * This constant maps a Webform ID to
+       * the machine names of a start and end
+       * date field on said form.
+       */
+      const webformIdDateFieldMap = {
+        'liikunta_tapahtuma': { // OK
+          'start_date_field': 'edit-alkaa',
+          'end_date_field': 'edit-paattyy',
+        },
+        'nuorisotoiminta_projektiavustush': { // OK
+          'start_date_field': 'edit-projekti-alkaa',
+          'end_date_field': 'edit-projekti-loppuu',
+        },
+        'kasko_ip_lisa': { // OK
+          'start_date_field': 'edit-alkaen',
+          'end_date_field': 'edit-paattyy',
+        },
+        'kuva_projekti': { // OK
+          'start_date_field': 'edit-hanke-alkaa',
+          'end_date_field': 'edit-hanke-loppuu',
+        },
+        'taide_ja_kulttuuri_kehittamisavu': {
+          'start_date_field': 'edit-hanke-alkaa',
+          'end_date_field': 'edit-hanke-loppuu',
         }
+      };
+
+
+      if (!settings.restricted_datepicker || !settings.restricted_datepicker.webform_id) {
+        return;
+      }
+
+      // Find the date field for the active Webform.
+      let webformId = settings.restricted_datepicker.webform_id;
+      let startDateInput = document.getElementById(webformIdDateFieldMap[webformId]['start_date_field']);
+      let endDateInput = document.getElementById(webformIdDateFieldMap[webformId]['end_date_field']);
+
+      if (startDateInput && endDateInput) {
+        startDateInput.addEventListener("change", function () {
+
+          // Set the minimum value for the end date.
+          endDateInput.min = startDateInput.value;
+
+          // Change the value of the end date if it is before the start date.
+          if (endDateInput.value && startDateInput.value > endDateInput.value) {
+            endDateInput.value = startDateInput.value;
+          }
+        });
       }
 
     }
