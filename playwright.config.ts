361762145d5a07ceb544fcbe71749d1f89f1f8a7
Roope Jansson <roope.jansson@vincit.fi>
2023-10-02 11:08:57 +0300

    AU-819: Update playwright config

--- playwright.config.ts
+++ playwright.config.ts
@@ -9,13 +9,13 @@ export default defineConfig({
   testDir: './e2e',
   timeout: 120 * 1000,
   /* Run tests in files in parallel */
-  fullyParallel: false,
+  fullyParallel: true,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
   /* Retry on CI only */
   retries: process.env.CI ? 2 : 0,
   /* Opt out of parallel tests on CI. */
-  workers: 1,
+  workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: 'html',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
