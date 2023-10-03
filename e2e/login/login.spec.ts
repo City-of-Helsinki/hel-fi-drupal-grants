9d16de716ea47adcbc30f9d7f75df3f2abeb98c4
Roope Jansson <roope.jansson@vincit.fi>
2023-10-03 11:49:11 +0300

    AU-819: Update tests for public sites

--- e2e/login/login.spec.ts
+++ e2e/login/login.spec.ts
@@ -1,5 +1,4 @@
 import { test } from '@playwright/test';
-import { TEST_SSN } from '../utils/test_data';
 import { login } from '../utils/helpers';
 
 test('can login through Tunnistamo', async ({ page }) => {
