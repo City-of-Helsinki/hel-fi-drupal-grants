082f52accb61a7c5671404a862dad6eb88567cad
Roope Jansson <roope.jansson@vincit.fi>
2023-10-03 11:49:11 +0300

    AU-819: Update tests for public sites

--- e2e/public/frontpage.spec.ts
+++ e2e/public/frontpage.spec.ts
@@ -6,18 +6,70 @@ test.beforeEach(async ({ page }) => {
 });
 
 
-test('has title', async ({ page }) => {
+test('verify title', async ({ page }) => {
     await expect(page).toHaveTitle(/.*Avustusasiointi/);
 });
 
+test('verify hero', async ({ page }) => {
+    await expect(page.getByRole('heading', { name: 'Avustukset' })).toBeVisible()
+    await expect(page.getByText('Helsingin kaupungin avustukset helsinkiläisille järjestöille, yhteisöille, asukasryhmille ja yksityisille henkilöille')).toBeVisible()
+    await expect(page.locator('.hero').getByRole('link', { name: 'Tietoa avustuksista' })).toBeVisible()
+});
+
+test('verify info block', async ({ page }) => {
+    await expect(page.getByRole('heading', { name: 'Uuteen avustusasiointiin siirtyminen tapahtuu vaiheittain vuoden 2023 aikana' })).toBeVisible();
+
+    const textArray = [
+        'Avustuslomakkeita työstetään uuteen asiointiin kovaa vauhtia!',
+        'Mikäli avustushakemuksesi löytyy listalta: jatka asiointia tällä sivustolla',
+        'Kasvatus ja koulutus: Yleisavustushakemus',
+        'Kasvatus ja koulutus: Iltapäivätoiminnan harkinnanvarainen lisäavustushakemus',
+        'Kulttuuri: Taide- ja kulttuuriavustusten projektiavustukset',
+        'Kulttuuri: Taiteen perusopetuksen avustukset',
+        'Liikunta: Yleisavustushakemus',
+        'HUOM! Mikäli et löydä hakemustasi listalta: Siirry vanhaan asiointiin.'
+      ];
+    
+      for (const text of textArray) {
+        await expect(page.getByText(text)).toBeVisible();
+      }
+
+    await expect(page.getByRole('link', { name: 'Siirry avustuksen vanhoille sivuille (Linkki avautuu uuteen välilehteen.) (Linkki johtaa ulkoiseen palveluun)' })).toBeVisible();
+});
+
+test('verify Näillä Sivuilla section', async ({ page }) => {
+    await expect(page.locator(".component--list-of-links").getByRole('link', { name: 'Tietoa avustuksista' })).toBeVisible();
+    await expect(page.getByText("Lue lisää kaupungin avustuksista ja niiden myöntämisperusteista.")).toBeVisible()
+
+    await expect(page.locator(".component--list-of-links").getByRole('link', { name: 'Etsi avustusta' })).toBeVisible();
+    await expect(page.getByText("Etsi sopivaa avustusta, lue lisätietoja ja siirry jättämään avustushakemus")).toBeVisible()
+    
+    await expect(page.locator(".component--list-of-links").getByRole('link', { name: 'Ohjeita hakijalle' })).toBeVisible();
+    await expect(page.getByText("Apua hakemuksen tekemiseen ja vastauksia usein kysyttyihin kysymyksiin")).toBeVisible()
+});
+
+test('verify banner', async ({ page }) => {
+    await expect(page.getByRole('heading', { name: 'Pääset täyttämään hakemusta kirjautumalla omaan asiointiin ja luomalla hakijaprofiilin' })).toBeVisible();
+    await expect(page.getByText("Palvelussa pääset hakemaan avustusta Helsingin kaupungilta sekä päivittämään avustushakemiseen liittyviä tietoja")).toBeVisible()
+    await expect(page.getByRole('button', { name: 'Kirjaudu sisään' })).toBeVisible()
+});
+
+test('verify news section', async ({ page }) => {
+    await expect(page.getByRole('heading', { name: 'Ajankohtaista avustuksista' })).toBeVisible();
+    await expect(page.getByRole('link', { name: 'Katso kaikki ajankohtaiset' })).toBeVisible()
+});
+
+test('verify help section', async ({ page }) => {
+    await expect(page.locator(".liftup-with-image").getByRole('heading', { name: 'Tarvitsetko apua hakemuksen tekemiseen?' })).toBeVisible();
+    await expect(page.locator(".liftup-with-image").getByText("Hakemuksen tekeminen voi olla haastavaa, joten kokosimme avuksesi kattavan infopaketin")).toBeVisible()
+    await expect(page.locator(".liftup-with-image").getByRole('link', { name: 'Ohjeita hakijalle' })).toBeVisible()
+    await expect(page.locator(".liftup-with-image").getByRole('link', { name: 'Tietoa avustuksista' })).toBeVisible()
+});
 
 test('has a login button', async ({ page }) => {
     const loginLink = page.getByRole('link', { name: 'Kirjaudu' })
     await expect(loginLink).toBeVisible()
 });
-
-
-
 
 test('contains news', async ({ page }) => {
     const newsBlockHeader = page.getByRole('heading', { name: 'Ajankohtaista avustuksista' })
@@ -30,10 +82,15 @@ test('contains news', async ({ page }) => {
     await expect(linkToNewsPage).toBeVisible()
 })
 
-
-test('has sinua kiinnostaa section', async ({ page }) => {
+test('verify Sinua Voisi Kiinnostaa section', async ({ page }) => {
     await expect(page.getByRole('heading', { name: 'Sinua voisi kiinnostaa' })).toBeVisible()
+    
     await expect(page.getByRole('link', { name: 'Yleiset avustusehdot' })).toBeVisible()
+    await expect(page.getByText("Helsingin kaupungin myöntämissä avustuksissa noudatettavat yleiset periaatteet ja menettelytavat.")).toBeVisible()
+
     await expect(page.getByRole('link', { name: 'Tilavaraukset' })).toBeVisible()
+    await expect(page.getByText("Voit varata kaupungin tiloja ja laitteita helposti käyttöösi.")).toBeVisible()
+    
     await expect(page.getByRole('link', { name: 'Päätökset-palvelu (Linkki johtaa ulkoiseen palveluun)' })).toBeVisible()
+    await expect(page.getByText("Päätökset-palvelusta löydät kaupungin päätöksentekoon liittyvät tiedot yhdestä paikasta.")).toBeVisible()
 });
---
+++ e2e/public/info_about_grants.spec.ts
@@ -0,0 +1,51 @@
+import { expect, test } from '@playwright/test';
+
+test.beforeEach(async ({ page }) => {
+    await page.goto('/fi/tietoa-avustuksista');
+});
+
+
+test('verify title', async ({ page }) => {
+    await expect(page).toHaveTitle(/.*Tietoa avustuksista/);
+});
+
+
+test('verify hero', async ({ page }) => {
+    await expect(page.getByRole('heading', { name: 'Tietoa avustuksista' })).toBeVisible();
+    await expect(page.getByText('Tältä sivulta löydät tietoa erilaisista avustuksista.')).toBeVisible();
+});
+
+test('navigation links are enabled', async ({ page }) => {
+    await expect(page.getByRole('link', { name: 'Kasvatus ja koulutus' })).toBeEnabled();
+    await expect(page.getByRole('link', { name: 'Kulttuuri ja vapaa-aika' })).toBeEnabled();    
+    await expect(page.getByRole('link', { name: 'Muut avustukset' })).toBeEnabled();
+});
+
+test('verify section links', async ({ page }) => {
+    // Checking visibility of elements under "Kasvatus ja koulutus"
+    await expect(page.locator('#kasvatus-ja-koulutus-2')).toBeVisible();
+    await expect(page.getByRole('link', { name: 'Iltapäivätoiminnan harkinnanvarainen lisäavustushakemus' })).toBeVisible();
+    await expect(page.getByRole('link', { name: 'Toiminta-avustushakemus koululaisten iltapäivätoiminnan järjestäjille' })).toBeVisible();
+    await expect(page.getByRole('link', { name: 'Yleisavustushakemus', exact: true })).toBeVisible();
+
+    // Checking visibility of elements under "Kulttuuri ja vapaa-aika"
+    await expect(page.locator('#kulttuuri-ja-vapaa-aika-2')).toBeVisible();
+    await expect(page.getByRole('link', { name: 'Kulttuurin avustukset' })).toBeVisible();
+    await expect(page.getByRole('link', { name: 'Liikunnan avustukset' })).toBeVisible();
+    await expect(page.getByRole('link', { name: 'Nuorisotoiminnan avustukset' })).toBeVisible();
+
+    // Checking visibility of elements under "Asukasosallisuuden avustukset"
+    await expect(page.locator('#asukasosallisuuden-avustukset')).toBeVisible();
+    await expect(page.getByRole('link', { name: 'Asukasosallisuuden avustukset' })).toBeVisible();
+
+    // Checking visibility of elements under "Muut Avustukset"
+    await expect(page.locator('#muut-avustukset-2')).toBeVisible();
+    await expect(page.getByRole('link', { name: 'Hyvinvoinnin ja terveyden edistämisen sekä sosiaali-, terveys- ja pelastustoimen avustukset' })).toBeVisible();
+    await expect(page.getByRole('link', { name: 'Kaupunginhallituksen yleisavustus (Linkki johtaa ulkoiseen palveluun)' })).toBeVisible();
+    await expect(page.getByRole('link', { name: 'Työllisyysavustushakemus (Linkki johtaa ulkoiseen palveluun)' })).toBeVisible();
+    await expect(page.getByRole('link', { name: 'Ympäristötoimen yleisavustushakemus (Linkki johtaa ulkoiseen palveluun)' })).toBeVisible();
+});
+
+test('news section is visible', async ({ page }) => {
+    await expect(page.getByRole('heading', { name: 'Ajankohtaista avustuksista' })).toBeVisible();
+});
