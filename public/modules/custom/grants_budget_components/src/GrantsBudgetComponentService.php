508feb811eb109c81d90fb51236ea822811fb28c
Oskari Nevalainen <oskari@mysteerio.fi>
2023-10-04 15:56:53 +0300

    AU-1681: Fix 0's not saving correctly for budgetComponents

--- public/modules/custom/grants_budget_components/src/GrantsBudgetComponentService.php
+++ public/modules/custom/grants_budget_components/src/GrantsBudgetComponentService.php
@@ -40,7 +40,7 @@ class GrantsBudgetComponentService {
 
         $value = $item->getValue();
 
-        if (empty($value) || $value === "") {
+        if (is_null($value) || $value === "") {
           continue;
         }
 
@@ -76,7 +76,7 @@ class GrantsBudgetComponentService {
 
       $value = $values['value'];
 
-      if (empty($value) || $value === "") {
+      if (is_null($value) || $value === "") {
         continue;
       }
 
