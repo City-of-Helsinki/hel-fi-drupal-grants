a9c91270a3f9bc9be90d0afb5e579ea296a10cee
Suvi Kankare <suvi.kankare@vincit.fi>
2023-10-05 14:40:43 +0300

    AU-1593: build

--- public/themes/custom/hdbt_subtheme/src/js/common.js
+++ public/themes/custom/hdbt_subtheme/src/js/common.js
@@ -1,9 +1,40 @@
 // eslint-disable-next-line no-unused-vars
-((Drupal, drupalSettings) => {
+(($, Drupal, drupalSettings) => {
   Drupal.behaviors.themeCommon = {
     attach: function attach() {
-      // Code here.
+
+      $(document).ready(function(){
+        const queryString = window.location.search;
+        const subString = 'items_per_page=';
+
+        const substringIndex = queryString.indexOf(subString);
+
+        const selectElement = document.getElementById('search-result-amount');
+
+        // Loop through the <option> elements in the <select>
+        for (let i = 0; i < selectElement.options.length; i++) {
+          const characterAfterSubstring = queryString.substring(substringIndex + subString.length);
+          const option = selectElement.options[i];
+
+          // Check if the option's value matches the value you want to select
+          if (option.label === characterAfterSubstring) {
+            // Set the option as selected
+            option.selected = true;
+
+            // Optionally, break the loop if you only want to select one option
+            break;
+          }
+        }
+
+        $('a.reset-search').on( 'click', function() {
+          console.log('sfsdf')
+          const datafield = $(this).attr('data-field');
+          $('#'+datafield).val('All');
+          $( '#views-exposed-form-application-search-search-api-search-page' ).submit();
+        });
+      });
+
     },
   };
   // eslint-disable-next-line no-undef
-})(Drupal, drupalSettings);
+})(jQuery, Drupal, drupalSettings);
