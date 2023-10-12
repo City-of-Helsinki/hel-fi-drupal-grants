ad8a1d4d244a4904ee1ed594ec4368863dd873dc
Roope Jansson <roope.jansson@vincit.fi>
2023-10-12 22:14:09 +0300

    AU-1734: Add tests to Oma Asiointi page

--- e2e/tests/my_services.spec.ts
+++ e2e/tests/my_services.spec.ts
@@ -1,5 +1,5 @@
 import { faker } from '@faker-js/faker';
-import { expect, test } from '@playwright/test';
+import { Page, expect, test } from '@playwright/test';
 import { selectRole, setupUnregisteredCommunity } from '../utils/helpers';
 
 test.describe('oma asiointi', () => {
@@ -9,16 +9,62 @@ test.describe('oma asiointi', () => {
     })
 
     test('check headings', async ({ page }) => {
-        // headings and texts
         await expect(page.getByRole('heading', { name: 'Tietoa avustuksista ja ohjeita hakijalle' })).toBeVisible()
         await expect(page.getByRole('heading', { name: 'Löydä avustuksesi' })).toBeVisible()
         await expect(page.getByRole('heading', { name: 'Tutustu yleisiin ohjeisiin' })).toBeVisible()
         await expect(page.getByRole('heading', { name: 'Keskeneräiset hakemukset' })).toBeVisible()
         await expect(page.getByRole('heading', { name: 'Lähetetyt hakemukset' })).toBeVisible()
+    });
 
-        // search controls
-        await expect(page.getByLabel('Näytä vain käsittelyssä olevat hakemukset')).toBeVisible()
+    test('controls for searching applications', async ({ page }) => {
+        await expect(page.getByLabel('Etsi hakemusta')).toBeVisible()
         await expect(page.getByRole('button', { name: 'Etsi hakemusta' })).toBeEnabled()
+        await expect(page.getByLabel('Näytä vain käsittelyssä olevat hakemukset')).toBeVisible()
+        await expect(page.getByLabel('Järjestä')).toBeVisible()
+    });
+
+    test('application drafts are visible', async ({ page }) => {
+        const applicationDraftCount = await page.locator('[id*="GRANTS"].draft').count()
+        expect(applicationDraftCount).toBeTruthy()
+    });
+
+    test('sent applications are visible', async ({ page }) => {
+        const receivedApplications = await page.locator('[id*="GRANTS"].received').count()
+        expect(receivedApplications).toBeTruthy()
+    });
+
+    test('applications can be sorted by date', async ({ page }) => {
+        let dates = await getDateValues(page)
+
+        expect(isDescending(dates)).toBeTruthy()
+
+        await page.getByLabel('Järjestä').selectOption('asc application-list__item--submitted');
+
+        dates = await getDateValues(page)
+        expect(isAscending(dates)).toBeTruthy()
+    });
+
+
+    test('search functionality', async ({ page }) => {
+        const INPUT_DELAY = 50;
+
+        let receivedApplicationCount = await getReceivedApplicationCount(page);
+
+        if (receivedApplicationCount < 2) {
+            test.skip()
+        }
+
+        const firstApplicationId = await page.locator('[id*="GRANTS"].received').first().getAttribute("id");
+
+        if (!firstApplicationId) {
+            test.fail()
+            return;
+        }
+
+        await page.getByLabel('Etsi hakemusta').pressSequentially(firstApplicationId, { delay: INPUT_DELAY });
+
+        receivedApplicationCount = await getReceivedApplicationCount(page);
+        expect(receivedApplicationCount).toBe(1)
     });
 })
 
@@ -292,3 +338,21 @@ test.describe('hakuprofiili', () => {
         });
     });
 })
+
+const isAscending = (dates: Date[]) => dates.every((x, i) => i === 0 || x >= dates[i - 1]);
+const isDescending = (dates: Date[]) => dates.every((x, i) => i === 0 || x <= dates[i - 1]);
+
+const getDateValues = async (page: Page) => {
+    const receivedApplications = await page.locator('[id*="GRANTS"].received').locator(".application-list__item--submitted").all()
+
+    const datePromises = receivedApplications.map(async a => {
+        const innerText = await a.innerText()
+        const trimmedText = innerText.trim()
+        return new Date(trimmedText)
+    })
+
+    const dates = await Promise.all(datePromises)
+    return dates;
+}
+
+const getReceivedApplicationCount = async (page: Page) => await page.locator('[id*="GRANTS"].received').count();
