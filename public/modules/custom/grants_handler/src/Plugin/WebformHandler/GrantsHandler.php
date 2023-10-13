45ed6b7b47193b8f96dc8306d6930fe3afafed19
Petri Leinonen <petri.leinonen@siili.com>
2023-10-13 16:09:21 +0300

    AU-1719: phpcs

--- public/modules/custom/grants_handler/src/Plugin/WebformHandler/GrantsHandler.php
+++ public/modules/custom/grants_handler/src/Plugin/WebformHandler/GrantsHandler.php
@@ -601,7 +601,7 @@ class GrantsHandler extends WebformHandlerBase {
     }
     if (isset($form["elements"]['palkkaussumma']) && $form["elements"]['palkkaussumma']) {
       $palkkausTotalAmount = 0;
-      // @todo: If we are going to keep using webform not react as frontend, this needs to go.
+      // @todo: Remove this hardcoding.
       if (isset($submissionData["subventions"]) && is_array($submissionData["subventions"])) {
         foreach ($submissionData["subventions"] as $sub) {
           if ($sub['subventionType'] == 2) {
