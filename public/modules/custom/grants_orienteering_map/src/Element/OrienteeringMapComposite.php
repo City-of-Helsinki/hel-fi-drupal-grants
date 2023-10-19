2b653cb68d1b9ceddd9323f82aa6b26108ed6264
Oskari Nevalainen <oskari@mysteerio.fi>
2023-10-19 08:43:05 +0300

    AU-1736: Add validation to orienteering map - other compensation field

--- public/modules/custom/grants_orienteering_map/src/Element/OrienteeringMapComposite.php
+++ public/modules/custom/grants_orienteering_map/src/Element/OrienteeringMapComposite.php
@@ -2,6 +2,8 @@
 
 namespace Drupal\grants_orienteering_map\Element;
 
+use Drupal\Core\Form\FormStateInterface;
+use Drupal\grants_handler\Plugin\WebformHandler\GrantsHandler;
 use Drupal\grants_handler\Processor\NumberProcessor;
 use Drupal\webform\Element\WebformCompositeBase;
 
@@ -26,6 +28,15 @@ class OrienteeringMapComposite extends WebformCompositeBase {
    */
   public function getInfo(): array {
     return parent::getInfo() + ['#theme' => 'orienteering_map_composite'];
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function processWebformComposite(&$element, FormStateInterface $form_state, &$complete_form): array {
+    $element = parent::processWebformComposite($element, $form_state, $complete_form);
+    _grants_handler_process_multivalue_errors($element, $form_state);
+    return $element;
   }
 
   /**
@@ -76,9 +87,56 @@ class OrienteeringMapComposite extends WebformCompositeBase {
       '#process' => [
         [NumberProcessor::class, 'process'],
       ],
+      '#element_validate' => [
+        [self::class, 'validateOtherCompensation'],
+      ],
     ];
 
     return $elements;
+  }
+
+  /**
+   * Validate orienteering map other compensation value.
+   *
+   * The field cannot be higher than the sum of voluntaryHours + cost fields.
+   *
+   * @param array $element
+   *   Element tobe validated.
+   * @param \Drupal\Core\Form\FormStateInterface $formState
+   *   Form state.
+   * @param array $form
+   *   The form.
+   */
+  public static function validateOtherCompensation(array &$element, FormStateInterface $formState, array &$form) {
+    $tOpts = ['context' => 'grants_orienteering_map'];
+
+    $arrayPath = $element['#parents'];
+    array_pop($arrayPath);
+
+    $elementValues = $formState->getValue($arrayPath);
+
+    // Get current item values.
+    $voluntaryHours = $elementValues['voluntaryHours'] ?? 0;
+    $cost = $elementValues['cost'] ?? 0;
+    $otherCompensations = $elementValues['otherCompensations'] ?? 0;
+
+    // Float conversion, just to be sure.
+    $voluntaryHours = GrantsHandler::convertToFloat($voluntaryHours);
+    $cost = GrantsHandler::convertToFloat($cost);
+    $otherCompensation = GrantsHandler::convertToFloat($otherCompensations);
+
+    // Hours + Cost cannot be lower than otherCompensation.
+    $hoursAndCostSum = $voluntaryHours + $cost;
+    if ($otherCompensation > $hoursAndCostSum) {
+      $formState->setError(
+        $element,
+        t(
+          'This value cannot be higher than the sum of voluntary hours and cost fields',
+          [],
+          $tOpts
+        )
+      );
+    }
   }
 
 }
