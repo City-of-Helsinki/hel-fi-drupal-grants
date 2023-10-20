42f3eec9ed4f2de7401487c87ca48e5c76df420d
Paavo Kivistö <paavo.kivisto@brightlyworks.com>
2023-10-20 16:45:35 +0300

    AU-1699: Handle dates in print view

--- public/modules/custom/grants_handler/src/Controller/ApplicationController.php
+++ public/modules/custom/grants_handler/src/Controller/ApplicationController.php
@@ -31,6 +31,8 @@ use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
  */
 class ApplicationController extends ControllerBase {
 
+  const ISO8601 = "/^(?:[1-9]\d{3}-(?:(?:0[1-9]|1[0-2])-(?:0[1-9]|1\d|2[0-8])|(?:0[13-9]|1[0-2])-(?:29|30)|(?:0[13578]|1[02])-31)|(?:[1-9]\d(?:0[48]|[2468][048]|[13579][26])|(?:[2468][048]|[13579][26])00)-02-29)T(?:[01]\d|2[0-3]):[0-5]\d:[0-5]\d(?:\.\d{1,9})?(?:Z|[+-][01]\d:[0-5]\d)$/";
+
 
   use StringTranslationTrait;
 
@@ -374,8 +376,8 @@ class ApplicationController extends ControllerBase {
         }
         // @todo other types when needed.
       }
-      // Handle application type field.
-      if ($field['ID'] === 'registrationDate') {
+      // Handle dates
+      if (preg_match(self::ISO8601, $field['value'])) {
         $field['value'] = date_format(date_create($field['value']), 'd.m.Y');
       }
 
