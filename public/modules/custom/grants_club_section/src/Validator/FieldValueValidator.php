1db45eb17fdf785e6fd39b39468fa057019872f8
Oskari Nevalainen <oskari@mysteerio.fi>
2023-10-30 12:28:15 +0200

    AU-1760: Fix simple sonarlint errors

--- public/modules/custom/grants_club_section/src/Validator/FieldValueValidator.php
+++ public/modules/custom/grants_club_section/src/Validator/FieldValueValidator.php
@@ -16,7 +16,7 @@ class FieldValueValidator {
    */
   public static function validate(&$element, FormStateInterface $form_state) {
     $parents = $element['#parents'];
-    $field = array_pop($parents);
+    array_pop($parents);
     $parent = $form_state->getValue($parents);
     $tOpts = ['context' => 'grants_club_section'];
 
@@ -48,7 +48,7 @@ class FieldValueValidator {
    */
   public static function validateAdultHours(&$element, FormStateInterface $form_state) {
     $parents = $element['#parents'];
-    $field = array_pop($parents);
+    array_pop($parents);
     $parent = $form_state->getValue($parents);
     $tOpts = ['context' => 'grants_club_section'];
 
@@ -72,11 +72,15 @@ class FieldValueValidator {
    */
   public static function validateSeniorHours(&$element, FormStateInterface $form_state) {
     $parents = $element['#parents'];
-    $field = array_pop($parents);
+    array_pop($parents);
     $parent = $form_state->getValue($parents);
     $tOpts = ['context' => 'grants_club_section'];
 
-    if ((!empty($parent['seniorMen']) || !empty($parent['seniorWomen']) || !empty($parent['seniorOthers'])) && empty($parent['seniorHours'])) {
+    if (
+        (!empty($parent['seniorMen']) || !empty($parent['seniorWomen']) ||
+        !empty($parent['seniorOthers'])) &&
+        empty($parent['seniorHours'])
+      ) {
       $form_state->setError(
         $element,
         t(
@@ -96,7 +100,7 @@ class FieldValueValidator {
    */
   public static function validateJuniorHours(&$element, FormStateInterface $form_state) {
     $parents = $element['#parents'];
-    $field = array_pop($parents);
+    array_pop($parents);
     $parent = $form_state->getValue($parents);
     $tOpts = ['context' => 'grants_club_section'];
 
