16f4a6c189168052695628cfe2e34ed8646f7363
Alexander Tallqvist <alexander.tallqvist@siili.com>
2023-11-01 15:14:54 +0200

    AU-676: feature: AU-676: Adjustments to operation hooks.

--- public/modules/custom/grants_industries/src/Services/NodeAccessCheckService.php
+++ public/modules/custom/grants_industries/src/Services/NodeAccessCheckService.php
@@ -14,6 +14,19 @@ use Drupal\user\Entity\User;
 class NodeAccessCheckService {
 
   /**
+   * Content admin access roles.
+   *
+   * An array of roles that are considered
+   * content admin roles (Can edit any service page,
+   * standard page and landing page nodes).
+   *
+   * @var array
+   */
+  protected array $contentAdminAccessRoles = [
+    'content_producer',
+  ];
+
+  /**
    * Restricted access roles.
    *
    * An array of roles that are considered
@@ -25,6 +38,27 @@ class NodeAccessCheckService {
   protected array $restrictedAccessRoles = [
     'content_producer_industry',
   ];
+
+  /**
+   * The hasRestrictedRole method.
+   *
+   * This method check if a users has a content admin role,
+   * meaning a role in the contentAdminAccessRoles property.
+   *
+   * @param \Drupal\user\Entity\User $user
+   *   The user we are checking.
+   *
+   * @return bool
+   *   True if the user has a content admin role, false otherwise.
+   */
+  public function hasContentAdminAccessRole(User $user): bool {
+    foreach ($this->contentAdminAccessRoles as $role) {
+      if ($user->hasRole($role)) {
+        return TRUE;
+      }
+    }
+    return FALSE;
+  }
 
   /**
    * The hasRestrictedRole method.
