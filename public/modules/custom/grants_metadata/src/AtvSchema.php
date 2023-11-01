7243c6e990f191920f8e8a334ea67f070c947c22
Paavo Kivistö <Puavo@users.noreply.github.com>
2023-11-01 07:47:17 +0200

    test: AU-801: Add more forms under tests (#883)
    * AU-801 Remove unused code
    * AU-801 Add two forms under tests
    * AU-801 Test conditional fields
    * AU-801 Add tests for two new forms
    * AU-801 Rename function to make tests more readable
    * AU-801 Improve ATVSchema coed
    * AU-801: AU-1584 Remove unnecessary assert
    * AU-801 Add test data for new forms
    * AU-801 Improve code style for sonarlint
    * Revert "AU-801 Remove unused code"
    
    This reverts commit 48b3336b4951243e8ecf469267f24ac45d22f26a.
    
    * AU-801 Add comments
    
    ---------
    
    Co-authored-by: Paavo Kivistö <paavo.kivisto@brightlyworks.com>

--- public/modules/custom/grants_metadata/src/AtvSchema.php
+++ public/modules/custom/grants_metadata/src/AtvSchema.php
@@ -608,7 +608,8 @@ class AtvSchema {
         $metaData = self::getMetaData($page, $section, $element);
       }
       else {
-
+        // This section is possibly used by grants budget module.
+        // Investigate if this can be removed eventually.
         if ($propertyStructureCallback) {
 
           $addWebformToCallback = $propertyStructureCallback['webform'] ?? FALSE;
@@ -655,17 +656,10 @@ class AtvSchema {
         if ($itemValue === '0' && $defaultValue === NULL && $skipZeroValue) {
           continue;
         }
-
-        // Skip empty values.
-        if ($itemValue === '' && $defaultValue === NULL) {
-          continue;
-        }
       }
-      else {
-        // Also remove other empty valued fields.
-        if ($itemValue === '' && $defaultValue === NULL) {
-          continue;
-        }
+      // Skip empty values.
+      if ($itemValue === '' && $defaultValue === NULL) {
+        continue;
       }
       // Value translation for select fields.
       if (isset($webformLabelElement['#options'][$itemValue])) {
@@ -808,7 +802,6 @@ class AtvSchema {
                     if ($itemName == 'issuerName') {
                       $webformName = 'issuer_name';
                     }
-                    $label = $itemValueDefinition->getLabel();
                     if (isset($webformMainElement['#webform_composite_elements'][$webformName]['#title'])) {
                       $titleElement = $webformMainElement['#webform_composite_elements'][$webformName]['#title'];
                       if (is_string($titleElement)) {
@@ -991,7 +984,7 @@ class AtvSchema {
   public static function isFieldHidden($property) {
     $definition = $property->getDataDefinition();
     $propertyName = $property->getName();
-    $hide = $definition->getSetting('hidden') || FALSE;
+    $hide = $definition->getSetting('hidden') ?? FALSE;
     if ($hide) {
       return TRUE;
     }
@@ -1000,7 +993,7 @@ class AtvSchema {
       return FALSE;
     }
     $parentDefinition = $parent->getDataDefinition();
-    $hiddenFields = $definition->getSetting('hiddenFields');
+    $hiddenFields = $parentDefinition->getSetting('hiddenFields');
     if (is_array($hiddenFields) && in_array($propertyName, $hiddenFields)) {
       return TRUE;
     }
@@ -1488,12 +1481,8 @@ class AtvSchema {
               }
             }
           }
-          else {
-            if (is_numeric($key2)) {
-              if (in_array($item2['ID'], $keys) && !in_array($item2['ID'], $values)) {
-                $values[$item2['ID']] = $item2['value'];
-              }
-            }
+          elseif (is_numeric($key2) && in_array($item2['ID'], $keys) && !in_array($item2['ID'], $values)) {
+            $values[$item2['ID']] = $item2['value'];
           }
         }
       }
