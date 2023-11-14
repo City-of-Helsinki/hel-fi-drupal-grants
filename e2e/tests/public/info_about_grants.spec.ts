723dcd1b3c40bcde755588d87d1c84a28453b9cb
Roope Jansson <roope.jansson@vincit.fi>
2023-11-14 11:24:52 +0200

    AU-1873: Combine tests cases for for similar pages

--- e2e/tests/public/info_about_grants.spec.ts
+++ e2e/tests/public/info_about_grants.spec.ts
@@ -1,24 +1,35 @@
-import { expect, test } from '@playwright/test';
+import { Page, expect, test } from '@playwright/test';
 
-test.beforeEach(async ({ page }) => {
-    await page.goto('/fi/tietoa-avustuksista');
-});
+test.describe("Info page", () => {
+    let page: Page;
 
+    test.beforeAll(async ({ browser }) => {
+        page = await browser.newPage()
+    });
 
-test('page title', async ({ page }) => {
-    await expect(page).toHaveTitle(/.*Tietoa avustuksista/);
-});
-
-
-test('verify hero', async ({ page }) => {
-    await expect(page.getByRole('heading', { name: 'Tietoa avustuksista' })).toBeVisible();
-});
-
-
-test('section headers', async ({ page }) => {
-    await expect(page.getByRole('heading', { name: 'Kasvatus ja koulutus' })).toBeVisible();
-    await expect(page.getByRole('heading', { name: 'Kulttuuri ja vapaa-aika' })).toBeVisible();
-    await expect(page.getByRole('heading', { name: 'Asukasosallisuuden avustukset', exact: true })).toBeVisible();
-    await expect(page.getByRole('heading', { name: 'Muut Avustukset' })).toBeVisible();
-    await expect(page.getByRole('heading', { name: 'Ajankohtaista avustuksista' })).toBeVisible();
-});
+    test.beforeEach(async () => {
+        await page.goto('/fi/tietoa-avustuksista');
+    });
+        
+    test('page title', async () => {
+        await expect(page).toHaveTitle(/.*Tietoa avustuksista/);
+    });
+    
+    
+    test('verify hero', async () => {
+        await expect(page.getByRole('heading', { name: 'Tietoa avustuksista' })).toBeVisible();
+    });
+    
+    
+    test('section headers', async () => {
+        await expect(page.getByRole('heading', { name: 'Kasvatus ja koulutus' })).toBeVisible();
+        await expect(page.getByRole('heading', { name: 'Kulttuuri ja vapaa-aika' })).toBeVisible();
+        await expect(page.getByRole('heading', { name: 'Asukasosallisuuden avustukset', exact: true })).toBeVisible();
+        await expect(page.getByRole('heading', { name: 'Muut Avustukset' })).toBeVisible();
+        await expect(page.getByRole('heading', { name: 'Ajankohtaista avustuksista' })).toBeVisible();
+    });
+        
+    test.afterAll(async () => {
+        await page.close();
+    });
+})
