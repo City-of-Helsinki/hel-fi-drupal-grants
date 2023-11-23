349333fff35d6478a151a3f9466933770104561d
Alexander Tallqvist <alexander.tallqvist@siili.com>
2023-11-23 16:01:31 +0200

    AU-1745: Removed debugging statement.

--- public/modules/custom/grants_metadata/src/TypedDataToDocumentContentWithWebform.php
+++ public/modules/custom/grants_metadata/src/TypedDataToDocumentContentWithWebform.php
@@ -243,7 +243,7 @@ class TypedDataToDocumentContentWithWebform {
     }
 
     // Optionally writ the data to a .json file. Used for testing.
-    self::writeJsonFile($documentStructure, $webform->id());
+    #self::writeJsonFile($documentStructure, $webform->id());
     return $documentStructure;
   }
 
