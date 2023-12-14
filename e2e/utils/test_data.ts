845f3132798dacb8f22857bb1cfcd81234dd5755
Roope Jansson <roope.jansson@vincit.fi>
2023-12-14 22:52:13 +0200

    change back to roger

--- e2e/utils/test_data.ts
+++ e2e/utils/test_data.ts
@@ -2,5 +2,5 @@
 export const TEST_IBAN = 'FI31 4737 2044 0000 48';
 
 // The user that is used in the tests
-export const TEST_SSN = '210281-9988';
-export const TEST_USER_UUID = '280f75c5-6a20-4091-b22d-dfcdce7fef60';
+export const TEST_SSN = '090797-999P';
+export const TEST_USER_UUID = '13cb60ae-269a-46da-9a43-da94b980c067';
