6107ca8c2380aeaf3e78bebdae873437143a7db7
Roope Jansson <roope.jansson@vincit.fi>
2023-12-14 23:10:24 +0200

    retry to 1

--- e2e/playwright.config.ts
+++ e2e/playwright.config.ts
@@ -17,7 +17,7 @@ export default defineConfig({
   forbidOnly: !!process.env.CI,
 
   // Retry on CI only.
-  retries: process.env.CI ? 2 : 0,
+  retries: process.env.CI ? 1 : 0,
 
   // The maximum number of concurrent worker processes to use for parallelizing tests
   workers: 1,
