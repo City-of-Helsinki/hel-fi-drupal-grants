05068662a6149a8314e80163a3ea410a292b0d0b
Suvi Kankare <suvi.kankare@vincit.fi>
2024-01-02 15:45:25 +0200

    AU-1971: init 56

--- e2e/utils/data/application/application_data_registered_community.ts
+++ e2e/utils/data/application/application_data_registered_community.ts
@@ -1,11 +1,13 @@
 
 import {registeredCommunityApplications_48} from "./application_data_48";
 import {registeredCommunityApplications_29} from "./application_data_29";
+import {registeredCommunityApplications_56} from "./application_data_56";
 import {registeredCommunityApplications_64} from "./application_data_64";
 
 const registeredCommunityApplications = {
     48: registeredCommunityApplications_48,
     29: registeredCommunityApplications_29,
+    56: registeredCommunityApplications_56,
     64: registeredCommunityApplications_64,
 }
 
