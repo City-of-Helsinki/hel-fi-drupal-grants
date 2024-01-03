9d2c1e8a92a23b9760e9fdafd3ff3187a7bfdf5d
Alexander Tallqvist <alexander.tallqvist@siili.com>
2024-01-03 17:29:35 +0200

    AU-1988: feature: AU-1988: Started implementing the bank account pending message.

---
+++ public/modules/custom/grants_attachments/js/pending-bank-account-confirmation.js
@@ -0,0 +1,50 @@
+(function ($, Drupal, drupalSettings) {
+
+  'use strict';
+
+  Drupal.behaviors.pendingBankAccountConfirmation = {
+
+    /**
+     * Attach the behavior.
+     *
+     * @param context
+     *   The context.
+     * @param settings
+     *   Drupal settings.
+     */
+    attach: function (context, settings) {
+      const selectedAccount = settings.grants_attachments.settings.selectedAccount;
+      if (typeof selectedAccount !== 'undefined') {
+        this.displayPendingBankAccountConfirmationMessage(selectedAccount);
+      }
+    },
+
+    /**
+     * The displayPendingBankAccountConfirmationMessage function.
+     *
+     * Desc...
+     *
+     * @param selectedAccount
+     *   The selected bank account.
+     */
+    displayPendingBankAccountConfirmationMessage: function(selectedAccount) {
+      if (!document.body.classList.contains('webform-submission-data-preview-page')) return;
+
+      // Create the list item.
+      const newListItem = document.createElement('li');
+      const newListItemDesc = Drupal.t("Confirmation file for @account", {'@account': selectedAccount});
+      const newListItemStatus = Drupal.t("Upload pending")
+      newListItem.innerHTML = newListItemDesc + "<br>" + newListItemStatus;
+
+      // Find or create the other attachments listing.
+      const otherAttachments = document.querySelector('.form-item-muu-liite');
+      const otherAttachmentsList = otherAttachments.querySelector('ul') || document.createElement('ul');
+
+      // Append the new item.
+      otherAttachmentsList.appendChild(newListItem);
+      otherAttachments.appendChild(otherAttachmentsList);
+    },
+
+  };
+
+})(jQuery, Drupal, drupalSettings);
