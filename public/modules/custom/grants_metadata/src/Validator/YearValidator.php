b5fdbd8111eab91b6eea3b6b1b1c68c820e55d2b
Oskari Nevalainen <oskari@mysteerio.fi>
2024-01-11 10:20:20 +0200

    Revert "feat: AU-1615: Add email type and pattern to profile forms"
    This reverts commit 7ecd8d2bc356e7fb1349f648c6bebc8d0db250c7.

--- public/modules/custom/grants_metadata/src/Validator/YearValidator.php
+++ public/modules/custom/grants_metadata/src/Validator/YearValidator.php
@@ -2,6 +2,8 @@
 
 namespace Drupal\grants_metadata\Validator;
 
+use Drupal\Core\Form\FormStateInterface;
+
 /**
  * The YearValidator class.
  *
@@ -11,6 +13,36 @@ namespace Drupal\grants_metadata\Validator;
  */
 class YearValidator {
 
-  static const PATTERN = "(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|\"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*\")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])"
+  /**
+   * Validate a year.
+   *
+   * Validates if the entered year is a number
+   * between 1900 - 2100.
+   *
+   * @param array $element
+   *   The form element to process.
+   * @param \Drupal\Core\Form\FormStateInterface $formState
+   *   The form state.
+   * @param array $form
+   *   The complete form structure.
+   */
+  public static function validate(array &$element, FormStateInterface $formState, array &$form): void {
+    $webformKey = $element['#webform_key'];
+    $value = $formState->getValue($webformKey);
+    $tOpts = ['context' => 'grants_metadata'];
+
+    if (!is_array($value) || !isset($value[0]['year'])) {
+      return;
+    }
+
+    $year = $value[0]['year'];
+    if ($year === '') {
+      return;
+    }
+
+    if (!preg_match("/^(19\d\d|20\d\d|2100)$/", $year)) {
+      $formState->setError($element, t('Enter a year between 1900 - 2100.', [], $tOpts));
+    }
+  }
 
 }
