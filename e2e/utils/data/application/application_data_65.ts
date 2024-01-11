c19433034209f97c7f8e1f5bc1a4c24f5104da92
Suvi Kankare <suvi.kankare@vincit.fi>
2024-01-11 09:54:15 +0200

    AU-946: start 65

---
+++ e2e/utils/data/application/application_data_65.ts
@@ -0,0 +1,316 @@
+import {FormData, FormDataWithRemoveOptionalProps} from "../test_data";
+import {fakerFI as faker} from "@faker-js/faker"
+import {PATH_TO_TEST_PDF, PATH_TO_TEST_EXCEL} from "../../helpers";
+import {createFormData} from "../../form_helpers";
+
+/**
+ * Basic form data for successful submit to Avus2
+ */
+const baseForm_65: FormData = {
+  title: 'Form submit',
+  formSelector: 'webform-submission-nuorlomaleir-form',
+  formPath: '/fi/form/nuorlomaleir',
+  formPages: {
+    "1_hakijan_tiedot": {
+      items: {
+        "edit-email": {
+          value: 'haloo@haloo.fi',
+        },
+        "edit-contact-person": {
+          value: faker.person.fullName(),
+        },
+        "edit-contact-person-phone-number": {
+          value: faker.phone.number(),
+        },
+        "edit-bank-account-account-number-select": {
+          role: 'select',
+          value: 'use-random-value',
+        },
+        "edit-community-address-community-address-select": {
+          value: '',
+        },
+        "edit-community-officials-items-0-item-community-officials-select": {
+          role: 'select',
+          selector: {
+            type: 'dom-id-first',
+            name: 'community-officials-selector',
+            value: '#edit-community-officials-items-0-item-community-officials-select',
+          },
+          value: '',
+        },
+        "nextbutton": {
+          role: 'button',
+          selector: {
+            type: 'form-topnavi-link',
+            name: 'data-drupal-selector',
+            value: '2_avustustiedot',
+          }
+        },
+      },
+    },
+    "2_avustustiedot": {
+      items: {
+        "edit-acting-year": {
+          role: 'select',
+          selector: {
+            type: 'dom-id-first',
+            name: 'bank-account-selector',
+            value: '#edit-acting-year',
+          },
+          value: '2025',
+        },
+        "edit-subventions-items-0-amount": {
+          value: '5709,98',
+        },
+        "nextbutton": {
+          role: 'button',
+          selector: {
+            type: 'form-topnavi-link',
+            name: 'data-drupal-selector',
+            value: '3_talousarvio',
+          }
+        },
+      },
+    },
+    "3_talousarvio": {
+      items: {
+        "edit-tulo-items-0-item-label": {
+          role: 'input',
+          value: faker.lorem.words(2),
+        },
+        "edit-tulo-items-0-item-value": {
+          role: 'input',
+          value: faker.number.int({min: 1, max: 5000}).toString(),
+        },
+        "edit-meno-items-0-item-label": {
+          role: 'input',
+          value: faker.lorem.words(2),
+        },
+        "edit-meno-items-0-item-value": {
+          role: 'input',
+          value: faker.number.int({min: 1, max: 5000}).toString(),
+        },
+        "nextbutton": {
+          role: 'button',
+          selector: {
+            type: 'form-topnavi-link',
+            name: 'data-drupal-selector',
+            value: 'lisatiedot_ja_liitteet',
+          }
+        },
+      },
+    },
+    "lisatiedot_ja_liitteet": {
+      items: {
+        "edit-additional-information": {
+          value: faker.lorem.sentences(3),
+        },
+        'edit-yhteison-saannot-attachment-upload': {
+          role: 'fileupload',
+          selector: {
+            type: 'locator',
+            name: 'data-drupal-selector',
+            value: '[name="files[yhteison_saannot_attachment]"]',
+            resultValue: '.form-item-yhteison-saannot-attachment a',
+          },
+          value: PATH_TO_TEST_PDF,
+        },
+        'edit-leiri-excel-attachment-upload': {
+          role: 'fileupload',
+          selector: {
+            type: 'locator',
+            name: 'data-drupal-selector',
+            value: '[name="files[leiri_excel_attachment]"]',
+            resultValue: '.form-item-leiri-excel-attachment a',
+          },
+          value: PATH_TO_TEST_EXCEL,
+        },
+        'edit-toimintasuunnitelma-attachment-upload': {
+          role: 'fileupload',
+          selector: {
+            type: 'locator',
+            name: 'data-drupal-selector',
+            value: '[name="files[toimintasuunnitelma_attachment]"]',
+            resultValue: '.form-item-toimintasuunnitelma-attachment a',
+          },
+          value: PATH_TO_TEST_PDF,
+        },
+        'edit-talousarvio-attachment-upload': {
+          role: 'fileupload',
+          selector: {
+            type: 'locator',
+            name: 'data-drupal-selector',
+            value: '[name="files[talousarvio_attachment]"]',
+            resultValue: '.form-item-talousarvio-attachment a',
+          },
+          value: PATH_TO_TEST_PDF,
+        },
+        'muu_liite_0': {
+          role: 'fileupload',
+          selector: {
+            type: 'locator',
+            name: 'data-drupal-selector',
+            value: '[name="files[muu_liite_items_0__item__attachment]"]',
+            resultValue: '.form-item-muu-liite-items-0--item--attachment a',
+          },
+          value: PATH_TO_TEST_PDF,
+        },
+        'muu_liite_0_kuvaus': {
+          role: 'input',
+          selector: {
+            type: 'data-drupal-selector',
+            name: 'data-drupal-selector',
+            value: 'edit-muu-liite-items-0-item-description',
+          },
+          value: faker.lorem.sentences(1),
+        },
+        "edit-extra-info": {
+          value: faker.lorem.sentences(2),
+        },
+        "nextbutton": {
+          role: 'button',
+          selector: {
+            type: 'form-topnavi-link',
+            name: 'data-drupal-selector',
+            value: 'webform_preview',
+          }
+        },
+      },
+    },
+    "webform_preview": {
+      items: {
+        "accept_terms_1": {
+          role: 'checkbox',
+          value: "1",
+        },
+        "sendbutton": {
+          role: 'button',
+          value: 'submit-form',
+          selector: {
+            type: 'data-drupal-selector',
+            name: 'data-drupal-selector',
+            value: 'edit-actions-submit',
+          }
+        },
+      },
+    },
+  },
+  expectedErrors: {},
+  expectedDestination: "/fi/hakemus/nuorlomaleir/",
+}
+
+/**
+ * Basic form data for successful submit to Avus2.
+ *
+ * Unregistered community.
+ */
+const baseFormUnRegisteredCommunity_65: FormData = createFormData(
+  baseForm_65,
+  {
+    formPages: {
+      "1_hakijan_tiedot": {
+        items: {
+          "edit-bank-account-account-number-select": {
+            role: 'select',
+            value: 'use-random-value',
+          },
+          "edit-community-officials-items-0-item-community-officials-select": {
+            role: 'select',
+            selector: {
+              type: 'dom-id-first',
+              name: 'community-officials-selector',
+              value: '#edit-community-officials-items-0-item-community-officials-select',
+            },
+            value: '',
+          },
+          "nextbutton": {
+            role: 'button',
+            selector: {
+              type: 'form-topnavi-link',
+              name: 'data-drupal-selector',
+              value: '2_avustustiedot',
+            }
+          },
+        },
+      },
+    },
+  }
+);
+
+const missingValues: FormDataWithRemoveOptionalProps = {
+  title: 'Missing values from 1st page',
+  formPages: {
+    '1_hakijan_tiedot': {
+      items: {},
+      // itemsToRemove: ['edit-bank-account-account-number-select'],
+    },
+    'webform_preview': {
+      items: {
+        "sendbutton": {
+          role: 'button',
+          value: 'save-draft',
+          selector: {
+            type: 'data-drupal-selector',
+            name: 'data-drupal-selector',
+            value: 'edit-actions-draft',
+          }
+        },
+      },
+      itemsToRemove: [],
+    },
+  },
+  expectedDestination: '',
+  expectedErrors: {
+    // 'edit-bank-account-account-number-select': 'Virhe sivulla 1. Hakijan tiedot: Valitse tilinumero kenttä on pakollinen.'
+  },
+};
+
+const saveDraft: FormDataWithRemoveOptionalProps = {
+  title: 'Safe to draft and verify data',
+  formPages: {
+    'webform_preview': {
+      items: {
+        "sendbutton": {
+          role: 'button',
+          value: 'save-draft',
+          selector: {
+            type: 'data-drupal-selector',
+            name: 'data-drupal-selector',
+            value: 'edit-actions-draft',
+          }
+        },
+      },
+      itemsToRemove: [],
+    },
+  },
+  expectedDestination: '',
+  expectedErrors: {},
+};
+
+/**
+ * All data for registered community, keyed with id. Those do not matter.
+ *
+ * Each keyed formdata in this object will result a new test run for this form.
+ *
+ */
+const registeredCommunityApplications_65 = {
+  success: baseForm_65,
+  draft: createFormData(baseForm_65, saveDraft),
+  // missing_values: createFormData(baseForm_65, missingValues),
+}
+
+/**
+ * All data for unregistered community applications.
+ *
+ * Each keyed formdata in this object will result a new test run for this form.
+ */
+const unRegisteredCommunityApplications_65 = {
+  success: baseFormUnRegisteredCommunity_65,
+  draft: createFormData(baseFormUnRegisteredCommunity_65, saveDraft),
+  // missing_values: createFormData(baseFormUnRegisteredCommunity_65, missingValues),
+}
+
+export {
+  registeredCommunityApplications_65,
+  unRegisteredCommunityApplications_65
+}
--- e2e/utils/data/application/application_data_unregistered_community.ts
+++ e2e/utils/data/application/application_data_unregistered_community.ts
@@ -1,8 +1,10 @@
 
 import {unRegisteredCommunityApplications_48} from "./application_data_48";
+import {unRegisteredCommunityApplications_65} from "./application_data_65";
 
 const unRegisteredCommunityApplications = {
-  48: unRegisteredCommunityApplications_48
+  48: unRegisteredCommunityApplications_48,
+  65: unRegisteredCommunityApplications_65
 }
 
 
