6bda25f45ecfefb7542c38e1e21296a637bab608
Suvi Kankare <suvi.kankare@vincit.fi>
2024-01-25 13:26:05 +0200

    AU-1321: start 58

---
+++ e2e/utils/data/application/application_data_58.ts
@@ -0,0 +1,239 @@
+import {FormData, FormDataWithRemoveOptionalProps} from "../test_data";
+import {fakerFI as faker} from "@faker-js/faker"
+import {PATH_MUU_LIITE} from "../../helpers";
+import {createFormData} from "../../form_helpers";
+
+/**
+ * Basic form data for successful submit to Avus2
+ */
+const baseFormRegisteredCommunity_58: FormData = {
+  title: 'Form submit',
+  formSelector: 'webform-submission-liikunta-suunnistuskartta-avustu-form',
+  formPath: '/fi/form/liikunta-suunnistuskartta-avustu',
+  formPages: {
+    "1_hakijan_tiedot": {
+      items: {
+        "edit-email": {
+          value: faker.internet.email(),
+        },
+        "edit-contact-person": {
+          value: faker.person.fullName(),
+        },
+        "edit-contact-person-phone-number": {
+          value: faker.phone.number(),
+        },
+        "edit-bank-account-account-number-select": {
+          role: 'select',
+          value: 'use-random-value',
+        },
+        "edit-community-address-community-address-select": {
+          value: '',
+        },
+        "edit-community-officials-items-0-item-community-officials-select": {
+          role: 'select',
+          selector: {
+            type: 'dom-id-first',
+            name: 'community-officials-selector',
+            value: '#edit-community-officials-items-0-item-community-officials-select',
+          },
+          value: '',
+        },
+        "nextbutton": {
+          role: 'button',
+          selector: {
+            type: 'form-topnavi-link',
+            name: 'data-drupal-selector',
+            value: '2_avustustiedot',
+          }
+        },
+      },
+    },
+    "2_avustustiedot": {
+      items: {
+        "edit-acting-year": {
+          role: 'select',
+          selector: {
+            type: 'dom-id-first',
+            name: 'bank-account-selector',
+            value: '#edit-acting-year',
+          },
+          value: '2024',
+        },
+        "edit-orienteering-maps-items-0-item-mapname": {
+          value: faker.lorem.sentences(4),
+        },
+        "edit-orienteering-maps-items-0-item-size": {
+          value: faker.number.int({min: 12, max: 5000}).toString(),
+        },
+        "edit-orienteering-maps-items-0-item-voluntaryhours": {
+          value: faker.number.int({min: 12, max: 5000}).toString(),
+        },
+        "edit-orienteering-maps-items-0-item-cost": {
+          value: faker.number.int({min: 12, max: 5000}).toString(),
+        },
+        "edit-orienteering-maps-items-0-item-othercompensations": {
+          value: faker.number.int({min: 12, max: 5000}).toString(),
+        },
+        "nextbutton": {
+          role: 'button',
+          selector: {
+            type: 'form-topnavi-link',
+            name: 'data-drupal-selector',
+            value: 'lisatiedot_ja_liitteet',
+          }
+        },
+      },
+    },
+    "lisatiedot_ja_liitteet": {
+      items: {
+        "edit-additional-information": {
+          role: 'input',
+          value: faker.lorem.sentences(3),
+        },
+        'edit-muu-liite-items-0-item-attachment-upload': {
+          role: 'fileupload',
+          selector: {
+            type: 'locator',
+            name: 'data-drupal-selector',
+            value: '[name="files[muu_liite_items_0__item__attachment]"]',
+            resultValue: '.form-item-muu-liite-items-0--item--attachment a',
+          },
+          value: PATH_MUU_LIITE,
+        },
+        'edit-muu-liite-items-0-item-description': {
+          role: 'input',
+          value: faker.lorem.sentences(1),
+        },
+        "edit-extra-info": {
+          value: faker.lorem.sentences(2),
+        },
+        "nextbutton": {
+          role: 'button',
+          selector: {
+            type: 'form-topnavi-link',
+            name: 'data-drupal-selector',
+            value: 'webform_preview',
+          }
+        },
+      },
+    },
+    "webform_preview": {
+      items: {
+        "accept_terms_1": {
+          role: 'checkbox',
+          value: "1",
+        },
+        "sendbutton": {
+          role: 'button',
+          value: 'save-draft',
+          selector: {
+            type: 'data-drupal-selector',
+            name: 'data-drupal-selector',
+            value: 'edit-actions-draft',
+          }
+        },
+      },
+    },
+  },
+  expectedErrors: {},
+  expectedDestination: "/fi/hakemus/liikunta_suunnistuskartta_avustu/",
+}
+
+const missingValues: FormDataWithRemoveOptionalProps = {
+  title: 'Missing values',
+  formPages: {
+    '1_hakijan_tiedot': {
+      items: {},
+      itemsToRemove: [
+        'edit-bank-account-account-number-select',
+        'edit-email',
+        'edit-contact-person',
+        'edit-contact-person-phone-number',
+        'edit-community-address-community-address-select',
+      ],
+    },
+    '2_avustustiedot': {
+      items: {},
+      itemsToRemove: [
+        'edit-acting-year',
+        'edit-orienteering-maps-items-0-item-mapname',
+      ],
+    },
+    'webform_preview': {
+      items: {},
+      itemsToRemove: [],
+    },
+  },
+  expectedDestination: '',
+  expectedErrors: {
+    'edit-bank-account-account-number-select': 'Virhe sivulla 1. Hakijan tiedot: Valitse tilinumero kenttä on pakollinen.',
+    'edit-email': 'Virhe sivulla 1. Hakijan tiedot: Sähköpostiosoite kenttä on pakollinen.',
+    'edit-contact-person': 'Virhe sivulla 1. Hakijan tiedot: Yhteyshenkilö kenttä on pakollinen.',
+    'edit-contact-person-phone-number': 'Virhe sivulla 1. Hakijan tiedot: Puhelinnumero kenttä on pakollinen.',
+    'edit-community-address': 'Virhe sivulla 1. Hakijan tiedot: Yhteisön osoite kenttä on pakollinen.',
+    'edit-community-address-community-address-select': 'Virhe sivulla 1. Hakijan tiedot: Valitse osoite kenttä on pakollinen.',
+    'edit-acting-year': 'Virhe sivulla 2. Avustustiedot: Vuosi, jolle haen avustusta kenttä on pakollinen.',
+    'edit-orienteering-maps-items-0-item-mapname': 'Virhe sivulla 2. Avustustiedot: Kartan nimi, sijainti ja karttatyyppi kenttä on pakollinen.',
+  },
+};
+
+const wrongValues: FormDataWithRemoveOptionalProps = {
+  title: 'Wrong values',
+  formPages: {
+    '1_hakijan_tiedot': {
+      items: {
+        "edit-email": {
+          role: 'input',
+          value: 'ääkkösiävaa',
+          selector: {
+            type: 'data-drupal-selector',
+            name: 'data-drupal-selector',
+            value: 'edit-email',
+          }
+        },
+      },
+      itemsToRemove: [],
+    },
+    'webform_preview': {
+      items: {},
+      itemsToRemove: [],
+    },
+  },
+  expectedDestination: '',
+  expectedErrors: {
+    'edit-email': 'Virhe sivulla 1. Hakijan tiedot: Sähköpostiosoite ääkkösiävaa ei kelpaa.',
+  },
+};
+
+const sendApplication: FormDataWithRemoveOptionalProps = {
+  title: 'Send to AVUS2',
+  formPages: {
+    'webform_preview': {
+      items: {
+        "sendbutton": {
+          role: 'button',
+          value: 'submit-form',
+          selector: {
+            type: 'data-drupal-selector',
+            name: 'data-drupal-selector',
+            value: 'edit-actions-submit',
+          }
+        },
+      },
+      itemsToRemove: [],
+    },
+  },
+  expectedDestination: '',
+  expectedErrors: {},
+};
+
+const registeredCommunityApplications_58 = {
+  draft: baseFormRegisteredCommunity_58,
+  missing_values: createFormData(baseFormRegisteredCommunity_58, missingValues),
+  wrong_values: createFormData(baseFormRegisteredCommunity_58, wrongValues),
+  // success: createFormData(baseFormRegisteredCommunity_58, sendApplication),
+}
+
+export {
+  registeredCommunityApplications_58
+}
