3e37b592c87e13900b1cdd7277f6ad8a48c57c68
suvikankare <kankare.suvi@gmail.com>
2024-01-25 14:40:23 +0200

    feat: AU-1314: tests for 60 (#1133)

--- e2e/utils/data/application/application_data_registered_community.ts
+++ e2e/utils/data/application/application_data_registered_community.ts
@@ -5,6 +5,7 @@ import {registeredCommunityApplications_51} from "./application_data_51";
 import {registeredCommunityApplications_53} from "./application_data_53";
 import {registeredCommunityApplications_56} from "./application_data_56";
 import {registeredCommunityApplications_57} from "./application_data_57";
+import {registeredCommunityApplications_60} from "./application_data_60";
 import {registeredCommunityApplications_63} from "./application_data_63";
 import {registeredCommunityApplications_64} from "./application_data_64";
 import {registeredCommunityApplications_65} from "./application_data_65";
@@ -17,6 +18,7 @@ const registeredCommunityApplications = {
     53: registeredCommunityApplications_53,
     56: registeredCommunityApplications_56,
     57: registeredCommunityApplications_57,
+    60: registeredCommunityApplications_60,
     63: registeredCommunityApplications_63,
     64: registeredCommunityApplications_64,
     65: registeredCommunityApplications_65,
