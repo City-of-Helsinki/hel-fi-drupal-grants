1da06bb7cc542083dd3cab3f38bb8e8cac95c9b9
Alexander Tallqvist <alexander.tallqvist@siili.com>
2024-02-06 17:07:53 +0200

    AU-2014: feature: AU-2014: Continued deleting implementation.

--- e2e/utils/deletion_helpers.ts
+++ e2e/utils/deletion_helpers.ts
@@ -2,17 +2,47 @@ import {Page} from "@playwright/test";
 import {FormData} from "./data/test_data";
 import {logger} from "./logger";
 
+/**
+ * The DeletionMethod enum.
+ *
+ * This enum represents the available deletion
+ * methods.
+ *
+ * SubmissionUrl = Delete an application by navigating
+ * to its submission URL and clicking "Delete" there.
+ *
+ * ApplicationId = Delete an application by finding the application
+ * from the "Oma asiointi" application listing and clicking "Delete".
+ */
+enum DeletionMethod {
+  SubmissionUrl,
+  ApplicationId
+}
+
+/**
+ * The deleteDraftApplication function.
+ *
+ * The main draft deletion function. This function
+ * randomly decides which deletion method to use. This
+ * is done so that we can test all available options.
+ *
+ * @param formKey
+ *   The form variant key.
+ * @param page
+ *   The browser page.
+ * @param formDetails
+ *   The form data.
+ * @param storedata
+ *   The env form data.
+ */
 const deleteDraftApplication = async (formKey: string, page: Page, formDetails: FormData, storedata: any) => {
   const thisStoreData = storedata[formKey];
-
   if (thisStoreData.status !== 'DRAFT') {
     return;
   }
 
-  // Randomly decide how we delete the application.
-  const method = Math.random() < 0.5 ? 1 : 2;
-
-  if (method === 1) {
+  const method = Math.random() < 0.5 ? DeletionMethod.SubmissionUrl : DeletionMethod.ApplicationId;
+  if (method === DeletionMethod.SubmissionUrl) {
     await deleteUsingSubmissionUrl(page, thisStoreData.submissionUrl);
   } else {
     await deleteUsingApplicationId(page, thisStoreData.applicationId);
@@ -22,46 +52,69 @@ const deleteDraftApplication = async (formKey: string, page: Page, formDetails: 
 /**
  * The deleteUsingSubmissionUrl function.
  *
+ * This function deletes a draft application by:
+ * 1. Navigating to the submission URL.
+ * 2. Clicking the "Delete draft" button.
+ * 3. Accepting the dialog popup.
+ * 4. Waiting for a redirect to "Oma asiointi".
+ * 5. Checking that a "Luonnos poistettu" message is displayed.
+ *
  * @param page
+ *   The browser page.
  * @param submissionUrl
+ *   The submission URL.
  */
 const deleteUsingSubmissionUrl = async (page: Page, submissionUrl: string) => {
   await page.goto(submissionUrl);
-  await page.locator('#webform-button--delete-draft').getByText('Poista luonnos').click();
+  await page.locator('#webform-button--delete-draft').click();
   page.once('dialog', async dialog => {
     logger(dialog.message());
     await dialog.accept();
   });
   await page.waitForURL("/fi/oma-asiointi");
-  await validateDeletionNotification(page, 'DELETED WITH SUBMISSION URL');
+  await validateDeletionNotification(page, 'Submission URL.');
 }
 
 /**
  * The deleteUsingApplicationId function.
  *
+ * This function deletes a draft application by:
+ * 1. Navigating to the "Oma asiointi" page.
+ * 2. Clicking the "Delete application" link for the desired application
+ *    in the application listing.
+ * 3. Waiting for the page to reload.
+ * 4. Checking that a "Luonnos poistettu" message is displayed.
+ *
  * @param page
+ *   The browser page.
  * @param applicationId
+ *   The application ID.
  */
 const deleteUsingApplicationId = async (page: Page, applicationId: string) => {
   await page.goto("/fi/oma-asiointi");
-  await page.locator(`#${applicationId}`).getByText('Poista hakemus').click();
+  await page.locator(`.application-delete-link-${applicationId}`).click();
   await page.waitForLoadState();
-  await validateDeletionNotification(page, 'DELETED WITH APPLICATION ID');
+  await validateDeletionNotification(page, 'Application ID.');
 }
 
 /**
  * The validateDeletionNotification function.
  *
+ * This function looks for a "Luonnos poistettu" message
+ * on the page. Throws an error if the message is not found.
+ *
  * @param page
+ *   The browser page.
  * @param message
+ *   Message indicating which deleting method was used.
  */
 const validateDeletionNotification = async (page: Page, message: string) => {
-  await page.waitForSelector('.hds-notification.hds-notification--info');
-  const notificationText = await page.innerText('.hds-notification.hds-notification--info');
-  if (notificationText.includes("Luonnos poistettu.")) {
-    logger("Draft application deletion confirmed:", message);
+  const notificationContainer = await page.locator('.hds-notification.hds-notification--info');
+  const notificationText = await notificationContainer.textContent({timeout: 1000});
+  if (notificationText && notificationText.includes("Luonnos poistettu.")) {
+    logger("Draft application deletion confirmed. Application deleted with:", message);
   } else {
-    throw new Error("Deletion notification not found or incorrect.");
+    throw new Error(`Failed to delete draft application. Tried deleting with: ${message}`);
   }
 }
 
