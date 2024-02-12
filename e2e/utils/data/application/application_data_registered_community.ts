667d9fcffb0be74760d3f6a2e0525d3054a977db
suvikankare <kankare.suvi@gmail.com>
2024-02-12 08:28:50 +0200

    feat: AU-975: tests for 62 (#1155)

--- e2e/utils/data/application/application_data_registered_community.ts
+++ e2e/utils/data/application/application_data_registered_community.ts
@@ -9,6 +9,7 @@ import {registeredCommunityApplications_57} from "./application_data_57";
 import {registeredCommunityApplications_58} from "./application_data_58";
 import {registeredCommunityApplications_60} from "./application_data_60";
 import {registeredCommunityApplications_61} from "./application_data_61";
+import {registeredCommunityApplications_62} from "./application_data_62";
 import {registeredCommunityApplications_63} from "./application_data_63";
 import {registeredCommunityApplications_64} from "./application_data_64";
 import {registeredCommunityApplications_65} from "./application_data_65";
@@ -27,6 +28,7 @@ const registeredCommunityApplications = {
     58: registeredCommunityApplications_58,
     60: registeredCommunityApplications_60,
     61: registeredCommunityApplications_61,
+    62: registeredCommunityApplications_62,
     63: registeredCommunityApplications_63,
     64: registeredCommunityApplications_64,
     65: registeredCommunityApplications_65,
