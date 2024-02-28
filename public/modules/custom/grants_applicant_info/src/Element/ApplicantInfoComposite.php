8deb1285e32a3dd8b413d8f55c8268e013418045
suvikankare <kankare.suvi@gmail.com>
2024-02-28 12:48:05 +0200

    fix: AU-2149: fix private person redirect message (#1194)

--- public/modules/custom/grants_applicant_info/src/Element/ApplicantInfoComposite.php
+++ public/modules/custom/grants_applicant_info/src/Element/ApplicantInfoComposite.php
@@ -39,7 +39,7 @@ class ApplicantInfoComposite extends WebformCompositeBase {
    * @throws \GuzzleHttp\Exception\GuzzleException
    */
   public static function getCompositeElements(array $element): array {
-    $tOpts = ['context' => 'grants_profile'];
+    $tOpts = ['context' => 'grants_handler'];
 
     if (isset($element['#webform'])) {
       $webform = Webform::load($element['#webform']);
@@ -71,14 +71,21 @@ class ApplicantInfoComposite extends WebformCompositeBase {
       '#type' => 'hidden',
       '#value' => $selectedRoleData["type"],
     ];
-
     if ($grantsProfile === NULL) {
+
       \Drupal::messenger()
         ->addWarning(t('You must have grants profile created.', [], $tOpts));
 
       $url = Url::fromRoute('grants_profile.edit');
-      $redirect = new RedirectResponse($url->toString());
-      $redirect->send();
+      $response = new RedirectResponse($url->toString());
+      $request = \Drupal::request();
+      // Save the session so things like messages get saved.
+      $request->getSession()->save();
+      $response->prepare($request);
+      // Make sure to trigger kernel events.
+      \Drupal::service('kernel')->terminate($request, $response);
+      $response->send();
+      return [];
     }
 
     switch ($selectedRoleData["type"]) {
