01b98612a11326431dc7565ad663cace1e1ca709
Alexander Tallqvist <alexander.tallqvist@siili.com>
2024-03-20 16:02:08 +0200

    AU-2148: feature: AU-2148: Implemented hard-coded data validation and changed test running logic.

--- e2e/tests/profiles/registered_community.ts
+++ e2e/tests/profiles/registered_community.ts
@@ -4,7 +4,7 @@ import {fillProfileForm} from '../../utils/form_helpers'
 import {isProfileCreated} from '../../utils/profile_helpers';
 import {deleteGrantsProfiles} from "../../utils/document_helpers";
 import {selectRole} from "../../utils/auth_helpers";
-import {validateProfileData} from "../../utils/validation_helpers";
+import {validateHardCodedProfileData, validateProfileData} from "../../utils/validation_helpers";
 import {
   profileDataRegisteredCommunity as profileData,
   FormData
@@ -14,26 +14,19 @@ const profileType = 'registered_community';
 
 test.describe('Registered Community - Grants Profile', () => {
   let page: Page;
-  let skip: boolean;
+  let profileExists: boolean;
+  let skipHardCodedDataTest: boolean = false;
   const testDataArray: [string, FormData][] = Object.entries(profileData);
 
   test.beforeAll(async ({browser}) => {
     page = await browser.newPage()
     await selectRole(page, 'REGISTERED_COMMUNITY');
-
-    skip = await isProfileCreated(profileType);
-    if (skip) {
-      process.env[`profile_created_${profileType}`] = 'TRUE';
-    }
-  });
-
-  test.beforeEach(async () => {
-    test.skip(skip);
+    profileExists = await isProfileCreated(profileType);
   });
 
   test.afterAll(() => {
-    if (process.env[`profile_created_${profileType}`] !== undefined &&
-        process.env[`profile_created_${profileType}`] === 'TRUE') {
+    if (process.env[`profile_exists_${profileType}`] !== undefined &&
+        process.env[`profile_exists_${profileType}`] === 'TRUE') {
       logger(`Profile exists for: ${profileType}`);
     } else {
       logger(`Profile does not exist for: ${profileType}`);
@@ -41,6 +34,12 @@ test.describe('Registered Community - Grants Profile', () => {
   });
 
   test('Profile creation', async () => {
+    if (profileExists) {
+      logger('Skipping profile creation test because profile already exists.');
+      test.skip(profileExists);
+    }
+
+    logger('Profile creation test.')
     let successTest: FormData | null = null;
 
     for (const [key, obj] of testDataArray) {
@@ -51,7 +50,7 @@ test.describe('Registered Community - Grants Profile', () => {
       await deleteGrantsProfiles(process.env.TEST_USER_UUID ?? '', profileType);
       await fillProfileForm(page, obj, obj.formPath, obj.formSelector);
     }
-    // Run the success test as the last test.
+    // Finally, fill the success test form.
     if (successTest) {
       await deleteGrantsProfiles(process.env.TEST_USER_UUID ?? '', profileType);
       await fillProfileForm(page, successTest, successTest.formPath, successTest.formSelector);
@@ -59,15 +58,29 @@ test.describe('Registered Community - Grants Profile', () => {
   });
 
   test('Test Grants profile data', async () => {
+    if (profileExists) {
+      logger('Skipping profile data validation test because profile already exists.');
+      test.skip(profileExists);
+    }
     for (const [key, obj] of testDataArray) {
       if (obj.viewPageSkipValidation) continue;
       await validateProfileData(
         page,
         obj,
-        key
+        key,
       );
     }
-    process.env[`profile_created_${profileType}`] = 'TRUE';
+    skipHardCodedDataTest = true;
+    process.env[`profile_exists_${profileType}`] = 'TRUE';
+  });
+
+  test('Test hard-coded Grants profile data', async () => {
+    if (skipHardCodedDataTest) {
+      logger('Skipping hard-coded profile data validation test because the data has already been validated.');
+      test.skip(skipHardCodedDataTest);
+    }
+    await validateHardCodedProfileData(page, profileType);
+    process.env[`profile_exists_${profileType}`] = 'TRUE';
   });
 
 });
