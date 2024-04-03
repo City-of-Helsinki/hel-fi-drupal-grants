8683dffcb557b2fb1528c5980686db61f41fcc91
Alexander Tallqvist <alexander.tallqvist@siili.com>
2024-04-03 11:38:27 +0300

    AU-2018: feature: AU-2018: Removed a space.

--- e2e/tests/auth.setup.ts
+++ e2e/tests/auth.setup.ts
@@ -1,7 +1,6 @@
 import {test as setup} from '@playwright/test';
 import { checkLoginStateAndLogin } from "../utils/auth_helpers";
 
-
 setup.setTimeout(60000)
 
 setup('authenticate', async ({page}) => {
