be03b312c807ac5f1c7db43d9eec95ca9f3e1c8e
Alexander Tallqvist <alexander.tallqvist@siili.com>
2024-04-08 13:26:30 +0300

    AU-2347: feature: AU-2347: Cleaned up slowLocator.

--- e2e/utils/helpers.ts
+++ e2e/utils/helpers.ts
@@ -2,34 +2,36 @@ import {Locator, Page} from "@playwright/test";
 import {logger} from "./logger";
 
 /**
- * Return a "slow" page locator that waits before 'click' and 'fill' requests.
+ * The slowLocator function.
+ *
+ * This function returns a "slow" page locator that
+ * waits before 'click' and 'fill' requests.
  *
  * @param page
+ *  Playwright page object
  * @param waitInMs
+ *  The wait time in ms for the locator.
  */
-function slowLocator(
-    page: Page,
-    waitInMs: number
-): (...args: any[]) => Locator {
-    // Grab original
-    const l = page.locator.bind(page);
+function slowLocator(page: Page, waitInMs: number): (...args: any[]) => Locator {
+  // Grab original.
+  const l = page.locator.bind(page);
 
-    // Return a new function that uses the original locator but remaps certain functions
-    return (locatorArgs) => {
-        const locator = l(locatorArgs);
+  // Return a new function that uses the original locator but remaps certain functions.
+  return (locatorArgs) => {
+    const locator = l(locatorArgs);
 
-        locator.click = async (args) => {
-            await new Promise((r) => setTimeout(r, waitInMs));
-            return l(locatorArgs).click(args);
-        };
-
-        locator.fill = async (args) => {
-            await new Promise((r) => setTimeout(r, waitInMs));
-            return l(locatorArgs).fill(args);
-        };
-
-        return locator;
+    locator.click = async (args) => {
+      await new Promise((r) => setTimeout(r, waitInMs));
+      return l(locatorArgs).click(args);
     };
+
+    locator.fill = async (args) => {
+      await new Promise((r) => setTimeout(r, waitInMs));
+      return l(locatorArgs).fill(args);
+    };
+
+    return locator;
+  };
 }
 
 const extractUrl = async (page: Page) => {
