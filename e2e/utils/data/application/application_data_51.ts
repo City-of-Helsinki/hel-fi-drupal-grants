2d6fd6624ade332373d31cdf0f8db5b2d14fbcd5
Alexander Tallqvist <alexander.tallqvist@siili.com>
2024-04-10 14:27:46 +0300

    AU-2354: feature: AU-2354: Fixed imports in form data.

--- e2e/utils/data/application/application_data_51.ts
+++ e2e/utils/data/application/application_data_51.ts
@@ -2,7 +2,7 @@ import {FormData, FormDataWithRemoveOptionalProps} from "../test_data";
 import {fakerFI as faker} from "@faker-js/faker";
 import {PROFILE_INPUT_DATA} from "../profile_input_data";
 import {ATTACHMENTS} from "../attachment_data";
-import {createFormData} from "../../form_helpers";
+import {createFormData} from "../../form_data_helpers";
 import {
   viewPageFormatAddress,
   viewPageFormatBoolean,
