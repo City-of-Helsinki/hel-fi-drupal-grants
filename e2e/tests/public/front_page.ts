8ff147e85b92dfb29ed94030696f8dff30b1b0c5
Alexander Tallqvist <alexander.tallqvist@siili.com>
2024-05-14 16:37:36 +0300

    AU-2390: feature: AU-2390: Refactored code.

--- e2e/tests/public/front_page.ts
+++ e2e/tests/public/front_page.ts
@@ -1,6 +1,7 @@
-import {Page, test} from '@playwright/test';
+import {expect, Page, test} from '@playwright/test';
 import {pageCollection} from "../../utils/data/public_page_data";
 import {validateComponent, validatePageTitle} from "../../utils/public_helpers";
+import {logger} from "../../utils/logger";
 
 const scenario = pageCollection['front_page'];
 
@@ -25,6 +26,27 @@ test.describe(`Testing page: ${scenario.url}`, () => {
     for (const component of scenario.components) {
       await validateComponent(page, component);
     }
+  });
+
+  test('Can change language', async () => {
+    logger('Validating language changing...');
+    let languageCode: string | null;
+
+    await page.getByRole('link', {name: 'Svenska'}).click();
+    languageCode = await page.locator('html').getAttribute('lang');
+    await expect(page.getByRole('heading', {name: 'Understöd', exact: true})).toBeVisible();
+    await expect(languageCode).toBe('sv');
+
+    await page.getByRole('link', {name: 'English'}).click();
+    languageCode = await page.locator('html').getAttribute('lang');
+    await expect(page.getByRole('heading', {name: 'Grants', exact: true})).toBeVisible();
+    await expect(languageCode).toBe('en');
+
+    await page.getByRole('link', {name: 'Suomi'}).click();
+    languageCode = await page.locator('html').getAttribute('lang');
+    await expect(page.getByRole('heading', {name: 'Avustukset'})).toBeVisible();
+    await expect(languageCode).toBe('fi');
+    logger('Language changing validated.');
   });
 
 });
--- e2e/utils/public_helpers.ts
+++ e2e/utils/public_helpers.ts
@@ -1,7 +1,6 @@
 import {expect, Page} from "@playwright/test";
 import {ComponentDetails} from "./data/test_data";
 import {logger} from "./logger";
-import {text} from "stream/consumers";
 
 /**
  * The validateComponent function.
@@ -16,6 +15,9 @@ import {text} from "stream/consumers";
  * 2. Making sure that each component has the required
  * elements.
  *
+ * 3. If requested, making sure that an element has certain
+ * text content.
+ *
  * @param page
  *  Playwright page object.
  * @param component
@@ -26,48 +28,107 @@ const validateComponent = async (page: Page, component: ComponentDetails) => {
 
   // Extract data.
   const { containerClass, occurrences = 1, elements } = component;
-  logger(`Expecting ${occurrences} occurrences of "${containerClass}" `);
 
   // Check the count for each component.
+  logger(`Expecting ${occurrences} occurrences of "${containerClass}".`);
   const componentCount = await page.locator(containerClass).count();
   await expect(componentCount, `Expected ${occurrences} occurrences of "${containerClass}" but found ${componentCount}.`).toBe(occurrences);
 
-  // For each component, check the count of the required elements.
-  for (let i = 0; i < occurrences; i++) {
+  // Validate the elements inside each component.
+  for (let index = 0; index < occurrences; index++) {
     for (const element of elements) {
       const { selector, countExact, countAtLeast, expectedText } = element;
-      let errorMessage = '';
 
-      // Check for "exact" count of elements.
-      if (countExact) {
-        logger(`Expecting ${countExact} occurrences of "${selector}" in instance "${i + 1}" of "${containerClass}"`);
-        const elementCount = await page.locator(containerClass).nth(i).locator(selector).count();
-        errorMessage = `Expected ${countExact} of "${selector}" in occurrence ${i + 1} of "${containerClass}" but found ${elementCount}.`;
-        await expect(elementCount, errorMessage).toBe(countExact);
+      if (countExact !== undefined) {
+        await validateExactCount(page, containerClass, selector, countExact, index);
       }
 
-      // Check for "at least" count of elements.
-      if (countAtLeast) {
-        logger(`Expecting at least ${countAtLeast} occurrences of "${selector}" in instance "${i + 1}" of "${containerClass}"`);
-        const elementCount = await page.locator(containerClass).nth(i).locator(selector).count();
-        errorMessage =`Expected at least ${countAtLeast} of "${selector}" in occurrence ${i + 1} of "${containerClass}" but found ${elementCount}.`
-        await expect(elementCount, errorMessage).toBeGreaterThanOrEqual(countAtLeast);
+      if (countAtLeast !== undefined) {
+        await validateAtLeastCount(page, containerClass, selector, countAtLeast, index);
       }
 
-      // Check for expected text inside of elements.
       if (expectedText && expectedText.length) {
-        for (let j = 0; j < expectedText.length; j++) {
-          let textString = expectedText[j];
-          logger(`Expecting to find text "${textString}" in "${selector}" of "${containerClass}".`);
-          let elementWithText = await page.locator(containerClass).nth(i).locator(selector).getByText(textString).count();
-          errorMessage = `Expected text "${textString}" for "${selector}" in occurrence ${i + 1} of "${containerClass}" but found nothing".`;
-          await expect(elementWithText, errorMessage).toBeTruthy();
-        }
+        await validateTextContent(page, containerClass, selector, expectedText, index);
       }
     }
   }
   logger('Component validated! \n');
 };
+
+/**
+ * The validateExactCount function.
+ *
+ * This function validates that a containerClass contains
+ * exactly countExact of selector.
+ *
+ * @param page
+ *   Playwright page object.
+ * @param containerClass
+ *   The container class.
+ * @param selector
+ *   The element selector.
+ * @param countExact
+ *   The "exact" element count.
+ * @param index
+ *   The :nth index of containerClass.
+ */
+const validateExactCount = async (page: Page, containerClass: string, selector: string, countExact: number, index: number) => {
+  logger(`Expecting ${countExact} occurrences of "${selector}" in instance "${index + 1}" of "${containerClass}"`);
+  const elementCount = await page.locator(containerClass).nth(index).locator(selector).count();
+  const errorMessage = `Expected exactly ${countExact} of "${selector}" in occurrence ${index + 1} of "${containerClass}" but found ${elementCount}.`;
+  await expect(elementCount, errorMessage).toBe(countExact);
+}
+
+/**
+ * The validateAtLeastCount function.
+ *
+ * This function validates that a containerClass contains
+ * at least countAtLeast of selector.
+ *
+ * @param page
+ *   Playwright page object.
+ * @param containerClass
+ *   The container class.
+ * @param selector
+ *   The element selector.
+ * @param countAtLeast
+ *   The "at least" element count.
+ * @param index
+ *   The :nth index of containerClass.
+ */
+const validateAtLeastCount = async (page: Page, containerClass: string, selector: string, countAtLeast: number, index: number) => {
+  logger(`Expecting at least ${countAtLeast} occurrences of "${selector}" in instance "${index + 1}" of "${containerClass}"`);
+  const elementCount = await page.locator(containerClass).nth(index).locator(selector).count();
+  const errorMessage = `Expected at least ${countAtLeast} of "${selector}" in occurrence ${index + 1} of "${containerClass}" but found ${elementCount}.`;
+  await expect(elementCount, errorMessage).toBeGreaterThanOrEqual(countAtLeast);
+}
+
+/**
+ * The validateTextContent function.
+ *
+ * This function validates that a selector
+ * has a given text.
+ *
+ * @param page
+ *   Playwright page object.
+ * @param containerClass
+ *   The container class.
+ * @param selector
+ *   The element selector.
+ * @param expectedText
+ *   The expected text inside selector.
+ * @param index
+ *   The :nth index of containerClass.
+ */
+const validateTextContent = async (page: Page, containerClass: string, selector: string, expectedText: string[], index: number) => {
+  for (const text of expectedText) {
+    logger(`Expecting to find text "${text}" in "${selector}" in instance "${index + 1}" of "${containerClass}".`);
+    const elementsWithText = await page.locator(containerClass).nth(index).locator(selector).filter({ hasText: text }).count();
+    const errorMessage = `Expected text "${text}" for "${selector}" in occurrence ${index + 1} of "${containerClass}" but found ${elementsWithText} instances.`;
+    await expect(elementsWithText, errorMessage).toBeGreaterThan(0);
+  }
+}
+
 
 /**
  * The validatePageTitle function.
@@ -76,7 +137,7 @@ const validateComponent = async (page: Page, component: ComponentDetails) => {
  * is set to "*** | Helsingin kaupunki".
  *
  * @param page
- *  Playwright page object.
+ *   Playwright page object.
  */
 const validatePageTitle = async (page: Page) => {
   logger(`Validating page title...`);
@@ -106,7 +167,6 @@ const getReceivedApplicationDateValues = async (page: Page): Promise<Date[]> => 
     const trimmedText = innerText.trim();
     return new Date(trimmedText)
   })
-
   return await Promise.all(datePromises)
 }
 
