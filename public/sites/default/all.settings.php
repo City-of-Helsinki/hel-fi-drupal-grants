f86899464efcf80f4cd7a7f8aa09376f40aeb57c
Janne Suominen <janne.suominen@siili.com>
2024-05-14 08:13:59 +0300

    Remove hard coded environment urls.

--- public/sites/default/all.settings.php
+++ public/sites/default/all.settings.php
@@ -7,9 +7,7 @@
 
 if (getenv('APP_ENV') == 'production') {
   $config['openid_connect.client.tunnistamo']['settings']['is_production'] = TRUE;
-  $config['openid_connect.client.tunnistamo']['settings']['environment_url'] = 'https://api.hel.fi/sso';
   $config['openid_connect.client.tunnistamoadmin']['settings']['is_production'] = TRUE;
-  $config['openid_connect.client.tunnistamoadmin']['settings']['environment_url'] = 'https://api.hel.fi/sso';
 }
 
 $config['openid_connect.client.tunnistamo']['settings']['client_id'] = getenv('TUNNISTAMO_CLIENT_ID');
