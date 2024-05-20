c693486bff9bb9c330cf6d1314d3020db8d96f4b
Alexander Tallqvist <117807015+Alexander-Tallqvist-Siili@users.noreply.github.com>
2024-05-20 15:56:55 +0300

    feat: AU-2390, E2E, Smoke tests (#1373)
    * AU-2390: feature: AU-2390: Reformated code.
    * AU-2390: feature: AU-2390: Continued fixing smoke tests.
    * AU-2390: feature: AU-2390: Continued on smoke tests.
    * AU-2390: feature: AU-2390: Removed old tests.
    * AU-2390: feature: AU-2390: Continued implementing smoke tests.
    * AU-2390: feature: AU-2390: Continued implementing smoke tests.
    * AU-2390: feature: AU-2390: Continued smoketest implementation.
    * AU-2390: feature: AU-2390: Continued implementation.
    * AU-2390: feature: AU.2390: Continued smoke test implementation.
    * AU-2390: feature: AU-2390: Continued implementation.
    * AU-2390: feature: AU-2390: Continued smoke test implementation.
    * AU-2390: featrue: AU-2390: Continued implementation.
    * AU-2390: feature: AU-2390: Continued smoke test implementation.
    * AU-2390: feature: AU-2390: Continued smoke test implementation.
    * AU-2390: feature: AU-2390: Increased the timeout in playwright.config.ts.
    * AU-2390: feature: AU-2390: Added text validation to components.
    * AU-2390: feature: AU-2390: Cleaned up code.
    * AU-2390: feature: AU-2390: Refactored code.
    * AU-2390: feature: AU-2390: Minor changes.
    * AU-2390: feature: AU-2390: Cleaned up code.
    * AU-2390: feature: AU-2390: Updated readme.
    * AU-2390: feature: AU-2390: Fixed readme.

---
+++ e2e/utils/data/public_page_data.ts
@@ -0,0 +1,253 @@
+import {PageCollection} from "./test_data";
+
+const pageCollection: PageCollection = {
+  "front_page": {
+    url: "/fi/avustukset",
+    validatePageTitle: true,
+    components: [
+      {
+        containerClass: ".hero",
+        elements: [
+          { selector: ".hero__title", countExact: 1 },
+          { selector: "a", countExact: 1 },
+        ],
+      },
+      {
+        containerClass: ".component--list-of-links",
+        elements: [
+          { selector: ".list-of-links__item", countAtLeast: 3 }
+        ],
+        occurrences: 2,
+      },
+      {
+        containerClass: ".component--banner",
+        elements: [
+          { selector: ".banner__title", countExact: 1 },
+          { selector: ".banner__desc", countExact: 1 },
+          { selector: "#edit-openid-connect-client-tunnistamo-login", countExact: 1 },
+        ],
+      },
+      {
+        containerClass: ".component--liftup-with-image",
+        elements: [
+          { selector: ".liftup-with-image__title", countExact: 1 },
+          { selector: ".liftup-with-image__desc", countExact: 1 },
+          { selector: "figure", countExact: 1 },
+        ],
+      },
+      {
+        containerClass: ".component--news-list",
+        elements: [
+          { selector: ".component__title", countExact: 1 },
+          { selector: ".component__content", countExact: 1 },
+        ],
+      },
+      {
+        containerClass: "#header-branding",
+        elements: [
+          { selector: ".profile__login-link", countExact: 1, expectedText: ['Kirjaudu']},
+        ],
+      },
+      {
+        containerClass: "#block-mainnavigation",
+        elements: [
+          { selector: ".menu--level-0", countExact: 1},
+          { selector: ".menu--level-0 > .menu__item", countAtLeast: 3},
+        ],
+      },
+      {
+        containerClass: ".footer-top",
+        elements: [
+          { selector: ".menu__item", countAtLeast: 5},
+        ],
+      },
+      {
+        containerClass: ".footer-bottom",
+        elements: [
+          { selector: ".menu__item",
+            countAtLeast: 5,
+            expectedText: ['Saavutettavuusseloste', 'Tietopyynnöt', 'Tietoa hel.fistä', 'Evästeasetukset'],
+          },
+        ],
+      },
+      {
+        containerClass: "#sliding-popup",
+        elements: [
+          { selector: ".eu-cookie-compliance-message h2",
+            countExact: 1,
+            expectedText: ['Hel.fi käyttää evästeitä'],
+          },
+          { selector: ".eu-cookie-compliance-message p",
+            countExact: 1,
+            expectedText: ['Tämä sivusto käyttää välttämättömiä evästeitä suorituskyvyn varmistamiseksi sekä yleisen käytön seurantaan. Lisäksi käytämme kohdennusevästeitä käyttäjäkokemuksen parantamiseksi, analytiikkaan ja kohdistetun sisällön näyttämiseen.'],
+          },
+          { selector: ".eu-cookie-compliance-more-info",
+            countExact: 1,
+            expectedText: ['Näytä evästeet'],
+          },
+          { selector: ".eu-cookie-compliance-default-button",
+            countExact: 1,
+            expectedText: ['Hyväksy kaikki evästeet'],
+          },
+          { selector: ".eu-cookie-compliance-save-preferences-button",
+            countExact: 1,
+            expectedText: ['Hyväksy vain välttämättömät evästeet'],
+          },
+        ],
+      },
+    ]
+  },
+  "about_grants": {
+    url: "/fi/tietoa-avustuksista",
+    validatePageTitle: true,
+    components: [
+      {
+        containerClass: ".hero",
+        elements: [
+          { selector: ".hero__title", countExact: 1 },
+        ],
+      },
+      {
+        containerClass: ".component--list-of-links",
+        elements: [
+          { selector: ".list-of-links__item", countAtLeast: 1 }
+        ],
+        occurrences: 5,
+      },
+      {
+        containerClass: ".component--news-list",
+        elements: [
+          { selector: ".component__title", countExact: 1 },
+          { selector: ".component__content", countExact: 1 },
+        ],
+      },
+    ]
+  },
+  "applicant_instructions": {
+    url: "/fi/ohjeita-hakijalle",
+    validatePageTitle: true,
+    components: [
+      {
+        containerClass: ".page-title",
+        elements: [
+          { selector: "h1", countExact: 1 },
+        ],
+      },
+      {
+        containerClass: ".component--lead-in",
+        elements: [
+          { selector: ".component__content", countExact: 1 },
+        ],
+      },
+      {
+        containerClass: ".component--list-of-links",
+        elements: [
+          { selector: ".list-of-links__item", countAtLeast: 1 },
+        ],
+      },
+      {
+        containerClass: ".component--content-liftup",
+        elements: [
+          { selector: ".content-liftup__image", countExact: 1 },
+          { selector: ".content-liftup__text", countExact: 1 },
+        ],
+      },
+    ]
+  },
+  "my_services": {
+    url: "/fi/oma-asiointi",
+    validatePageTitle: true,
+    components: [
+      {
+        containerClass: ".hero",
+        elements: [
+          { selector: ".hero__title", countExact: 1 },
+        ],
+      },
+      {
+        containerClass: ".oma-asiointi-infoboxes-container",
+        elements: [
+          { selector: "h2", countExact: 1 },
+          { selector: ".oma-asiointi-infobox", countExact: 2 },
+        ],
+      },
+      {
+        containerClass: ".oma-asiointi-infobox",
+        elements: [
+          { selector: "h3", countExact: 1 },
+          { selector: "p", countExact: 1 },
+          { selector: "a", countExact: 1 },
+        ],
+        occurrences: 2,
+      },
+      {
+        containerClass: "#oma-asiointi__drafts",
+        elements: [
+          { selector: "h2", countExact: 1, expectedText: ['Keskeneräiset hakemukset'] },
+          { selector: ".application-list", countExact: 1 },
+        ],
+      },
+      {
+        containerClass: "#oma-asiointi__sent",
+        elements: [
+          { selector: "h2", countExact: 1, expectedText: ['Lähetetyt hakemukset'] },
+          { selector: ".application-list", countExact: 1 },
+          { selector: ".application-list-filter", countExact: 1, expectedText: ['Etsi hakemusta'] },
+          { selector: "label[for='checkbox-processed']", countExact: 1, expectedText: ['Näytä vain käsittelyssä olevat hakemukset'] },
+          { selector: ".application-list__search-row .hds-button", countExact: 1, expectedText: ['Etsi hakemusta'] },
+        ],
+      },
+    ]
+  },
+  "search_page": {
+    url: "/fi/etsi-avustusta",
+    validatePageTitle: true,
+    components: [
+      {
+        containerClass: ".hero",
+        elements: [
+          { selector: ".hero__title", countExact: 1 },
+        ],
+      },
+      {
+        containerClass: ".application_search--filters",
+        elements: [
+          { selector: ".form-item-target-group", countExact: 1, expectedText: ['Valitse kohderyhmä'] },
+          { selector: ".form-item-activity", countExact: 1, expectedText: ['Millaiseen toimintaan haet avustusta?'] },
+          { selector: ".form-item-applicant", countExact: 1, expectedText: ['Avustuksen hakija'] },
+          { selector: ".form-item-search", countExact: 1, expectedText: ['Tai etsi hakusanalla'] },
+          { selector: ".form-item-application-open", countExact: 1, expectedText: ['Näytä vain haettavissa olevat avustukset'] },
+          { selector: ".form-submit", countExact: 1, expectedText: ['Etsi'] },
+        ],
+      },
+      {
+        containerClass: ".application_search--rows",
+        elements: [
+          { selector: ".application_search--row", countAtLeast: 3 },
+        ],
+      },
+    ]
+  },
+  "service_instructions": {
+    url: "/fi/ohjeita-hakijalle/palvelun-kayttoohjeet",
+    validatePageTitle: true,
+    components: [
+      {
+        containerClass: ".page-title",
+        elements: [
+          { selector: "h1", countExact: 1 },
+        ],
+      },
+      {
+        containerClass: ".component--list-of-links",
+        elements: [
+          { selector: ".list-of-links__item", countAtLeast: 4 }
+        ],
+      },
+    ]
+  },
+};
+
+export {
+  pageCollection,
+}
