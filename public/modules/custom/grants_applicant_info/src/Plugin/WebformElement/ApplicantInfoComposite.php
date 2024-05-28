e358c83d35e6e05e58cb5e2f4d24c72b23cfd4d5
Petri Leinonen <petri.leinonen@siili.com>
2024-05-28 12:35:03 +0300

    AU-2283: wip

--- public/modules/custom/grants_applicant_info/src/Plugin/WebformElement/ApplicantInfoComposite.php
+++ public/modules/custom/grants_applicant_info/src/Plugin/WebformElement/ApplicantInfoComposite.php
@@ -90,15 +90,17 @@ class ApplicantInfoComposite extends WebformCompositeBase {
   protected function formatTextItemValue(array $element, WebformSubmissionInterface $webform_submission, array $options = []): array {
     $value = $this->getValue($element, $webform_submission, $options);
     $lines = [];
+    $lines[] = '<dl>';
     foreach ($value as $fieldName => $fieldValue) {
       if (isset($element["#webform_composite_elements"][$fieldName])) {
         $webformElement = $element["#webform_composite_elements"][$fieldName];
         if ($webformElement && isset($webformElement['#title'])) {
-          $lines[] = '<strong>' . $webformElement['#title']->render() . '</strong>';
-          $lines[] = $fieldValue;
+          $lines[] = '<dt>' . $webformElement['#title']->render() . '</dt>';
+          $lines[] = '<dd>' . $fieldValue . '</dd>';
         }
       }
     }
+    $lines[] = '</dl>';
 
     return $lines;
   }
