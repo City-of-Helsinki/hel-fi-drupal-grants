0b96a494af1996523dc4fbd0fc2a454f32d4f11c
Oskari Nevalainen <oskari@mysteerio.fi>
2024-06-17 09:04:50 +0300

    AU-448: Change correct status to production check

--- public/modules/custom/grants_handler/src/ApplicationHandler.php
+++ public/modules/custom/grants_handler/src/ApplicationHandler.php
@@ -1060,7 +1060,7 @@ class ApplicationHandler {
     $isProd = self::isProduction($appEnv);
 
     if (
-      ($isProd && $status !== 'production') ||
+      ($isProd && $status !== 'released') ||
       $status === 'archived'
     ) {
       return FALSE;
