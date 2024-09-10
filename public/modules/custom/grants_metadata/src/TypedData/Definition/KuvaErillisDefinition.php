3957bcc6f90cab6519fe29e72c4863ce83f984e6
Janne Suominen <janne.suominen@siili.com>
2024-09-10 09:10:09 +0300

    UHF-10537: Add files as well.

---
+++ public/modules/custom/grants_metadata/src/TypedData/Definition/KuvaErillisDefinition.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Drupal\grants_metadata\TypedData\Definition;
+
+use Drupal\Core\TypedData\ComplexDataDefinitionBase;
+use Drupal\Core\TypedData\ListDataDefinition;
+use Drupal\grants_budget_components\TypedData\Definition\GrantsBudgetInfoDefinition;
+
+/**
+ * Define KuvaErillisDefinition data.
+ */
+class KuvaErillisDefinition extends ComplexDataDefinitionBase {
+
+  /**
+   * @inheritDoc
+   */
+  public function getPropertyDefinitions() {
+    // TODO: Implement getPropertyDefinitions() method.
+  }
+
+}
