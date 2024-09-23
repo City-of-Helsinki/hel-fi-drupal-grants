d24d8f6a98162ee47db630821a125f1f19a391c9
rpnykanen <risto.nykanen@druid.fi>
2024-09-23 14:00:37 +0300

    UHF-10537: added most of the field definitions

--- public/modules/custom/grants_metadata/src/TypedData/Definition/KuvaErillisDefinition.php
+++ public/modules/custom/grants_metadata/src/TypedData/Definition/KuvaErillisDefinition.php
@@ -3,8 +3,7 @@
 namespace Drupal\grants_metadata\TypedData\Definition;
 
 use Drupal\Core\TypedData\ComplexDataDefinitionBase;
-use Drupal\Core\TypedData\ListDataDefinition;
-use Drupal\grants_budget_components\TypedData\Definition\GrantsBudgetInfoDefinition;
+use Drupal\Core\TypedData\DataDefinition;
 
 /**
  * Define KuvaErillisDefinition data.
@@ -25,6 +24,251 @@ class KuvaErillisDefinition extends ComplexDataDefinitionBase {
         $info[$key] = $property;
       }
     }
+
+    $info['hankesuunnitelma_jatkohakemus'] = DataDefinition::create('string')
+      ->setSetting('defaultValue', '')
+      ->setSetting('jsonPath', [
+        'compensation',
+        'activityBasisInfo',
+        'activityBasisArray',
+        'hankesuunnitelma_jatkohakemus',
+      ]);
+
+    $info['hankkeen_tarkoitus_tavoitteet'] = DataDefinition::create('string')
+      ->setSetting('defaultValue', '')
+      ->setSetting('jsonPath', [
+        'compensation',
+        'activityBasisInfo',
+        'activityBasisArray',
+        'hankkeen_tarkoitus_tavoitteet',
+      ]);
+
+    $info['hankkeen_toimenpiteet_aikataulu'] = DataDefinition::create('string')
+      ->setSetting('defaultValue', '')
+      ->setSetting('jsonPath', [
+        'compensation',
+        'activityBasisInfo',
+        'activityBasisArray',
+        'hankkeen_toimenpiteet_aikataulu',
+      ]);
+
+    $info['hankkeen_toimenpiteet_alkupvm'] = DataDefinition::create('string')
+      ->setSetting('defaultValue', '')
+      ->setSetting('jsonPath', [
+        'compensation',
+        'activityBasisInfo',
+        'activityBasisArray',
+        'hankkeen_toimenpiteet_alkupvm',
+      ])
+      ->setSetting('typeOverride', [
+        'dataType' => 'string',
+        'jsonType' => 'datetime',
+      ])
+      ->setSetting('valueCallback', [
+        'service' => 'grants_metadata.converter',
+        'method' => 'convertDates',
+        'arguments' => [
+          'dateFormat' => 'c',
+        ],
+      ]);
+
+    $info['hankkeen_toimenpiteet_loppupvm'] = DataDefinition::create('string')
+      ->setSetting('defaultValue', '')
+      ->setSetting('jsonPath', [
+        'compensation',
+        'activityBasisInfo',
+        'activityBasisArray',
+        'hankkeen_toimenpiteet_loppupvm',
+      ])
+      ->setSetting('typeOverride', [
+        'dataType' => 'string',
+        'jsonType' => 'datetime',
+      ])
+      ->setSetting('valueCallback', [
+        'service' => 'grants_metadata.converter',
+        'method' => 'convertDates',
+        'arguments' => [
+          'dateFormat' => 'c',
+        ],
+      ]);
+
+    $info['hankkeen_keskeisimmat_kumppanit'] = DataDefinition::create('string')
+      ->setSetting('defaultValue', '')
+      ->setSetting('jsonPath', [
+        'compensation',
+        'activityBasisInfo',
+        'activityBasisArray',
+        'hankkeen_keskeisimmat_kumppanit',
+      ]);
+
+    $info['haun_painopisteet_liikkumis_kehitys'] = DataDefinition::create('string')
+      ->setSetting('defaultValue', '')
+      ->setSetting('jsonPath', [
+        'compensation',
+        'activityBasisInfo',
+        'activityBasisArray',
+        'haun_painopisteet_liikkumis_kehitys',
+      ]);
+
+    $info['haun_painopisteet_digi_kehitys'] = DataDefinition::create('string')
+      ->setSetting('defaultValue', '')
+      ->setSetting('jsonPath', [
+        'compensation',
+        'activityBasisInfo',
+        'activityBasisArray',
+        'haun_painopisteet_digi_kehitys',
+      ]);
+
+    $info['haun_painopisteet_vertais_kehitys'] = DataDefinition::create('string')
+      ->setSetting('defaultValue', '')
+      ->setSetting('jsonPath', [
+        'compensation',
+        'activityBasisInfo',
+        'activityBasisArray',
+        'haun_painopisteet_vertais_kehitys',
+      ]);
+
+    $info['haun_painopisteet_kulttuuri_kehitys'] = DataDefinition::create('string')
+      ->setSetting('defaultValue', '')
+      ->setSetting('jsonPath', [
+        'compensation',
+        'activityBasisInfo',
+        'activityBasisArray',
+        'haun_painopisteet_kulttuuri_kehitys',
+      ]);
+
+    $info['hankkeen_kohderyhmat_kenelle'] = DataDefinition::create('string')
+      ->setSetting('defaultValue', '')
+      ->setSetting('jsonPath', [
+        'compensation',
+        'activityBasisInfo',
+        'activityBasisArray',
+        'hankkeen_kohderyhmat_kenelle',
+      ]);
+
+    $info['hankkeen_kohderyhmat_erityisryhmat'] = DataDefinition::create('string')
+      ->setSetting('defaultValue', '')
+      ->setSetting('jsonPath', [
+        'compensation',
+        'activityBasisInfo',
+        'activityBasisArray',
+        'hankkeen_kohderyhmat_erityisryhmat',
+      ]);
+
+    $info['hankkeen_kohderyhmat_tavoitus'] = DataDefinition::create('string')
+      ->setSetting('defaultValue', '')
+      ->setSetting('jsonPath', [
+        'compensation',
+        'activityBasisInfo',
+        'activityBasisArray',
+        'hankkeen_kohderyhmat_tavoitus',
+      ]);
+
+    $info['hankkeen_kohderyhmat_konkretia'] = DataDefinition::create('string')
+      ->setSetting('defaultValue', '')
+      ->setSetting('jsonPath', [
+        'compensation',
+        'activityBasisInfo',
+        'activityBasisArray',
+        'hankkeen_kohderyhmat_konkretia',
+      ]);
+
+    $info['hankkeen_kohderyhmat_osallisuus'] = DataDefinition::create('string')
+      ->setSetting('defaultValue', '')
+      ->setSetting('jsonPath', [
+        'compensation',
+        'activityBasisInfo',
+        'activityBasisArray',
+        'hankkeen_kohderyhmat_osallisuus',
+      ]);
+
+    $info['hankkeen_kohderyhmat_osaaminen'] = DataDefinition::create('string')
+      ->setSetting('defaultValue', '')
+      ->setSetting('jsonPath', [
+        'compensation',
+        'activityBasisInfo',
+        'activityBasisArray',
+        'hankkeen_kohderyhmat_osaaminen',
+      ]);
+
+    $info['hankkeen_kohderyhmat_postinrot'] = DataDefinition::create('string')
+      ->setSetting('defaultValue', '')
+      ->setSetting('jsonPath', [
+        'compensation',
+        'activityBasisInfo',
+        'activityBasisArray',
+        'hankkeen_kohderyhmat_postinrot',
+      ]);
+
+    $info['hankkeen_kohderyhmat_miksi_alue'] = DataDefinition::create('string')
+      ->setSetting('defaultValue', '')
+      ->setSetting('jsonPath', [
+        'compensation',
+        'activityBasisInfo',
+        'activityBasisArray',
+        'hankkeen_kohderyhmat_miksi_alue',
+      ]);
+
+    $info['hankkeen_riskit_keskeisimmat'] = DataDefinition::create('string')
+      ->setSetting('defaultValue', '')
+      ->setSetting('jsonPath', [
+        'compensation',
+        'activityBasisInfo',
+        'activityBasisArray',
+        'hankkeen_riskit_keskeisimmat',
+      ]);
+
+    $info['hankkeen_riskit_seuranta'] = DataDefinition::create('string')
+      ->setSetting('defaultValue', '')
+      ->setSetting('jsonPath', [
+        'compensation',
+        'activityBasisInfo',
+        'activityBasisArray',
+        'hankkeen_riskit_seuranta',
+      ]);
+
+    $info['hankkeen_riskit_vakiinnuttaminen'] = DataDefinition::create('string')
+      ->setSetting('defaultValue', '')
+      ->setSetting('jsonPath', [
+        'compensation',
+        'activityBasisInfo',
+        'activityBasisArray',
+        'hankkeen_riskit_vakiinnuttaminen',
+      ]);
+
+    // Osio 4
+
+    /*
+    // staattinen tulokenttä
+    // tulon tyyppi
+    $info['tulot'] = DataDefinition::create();
+
+    $info['talous_tulon_tyyppi'] = DataDefinition::create();
+    // menon tyyppi
+    $info['talous_menon_tyyppi'] = DataDefinition::create();
+    */
+
+    // Osio 5
+
+    $info['additional_information'] = DataDefinition::create('string')
+      ->setSetting('jsonPath', ['compensation', 'additionalInformation'])
+      ->setSetting('defaultValue', "");
+
+    $info['attachments_info'] = DataDefinition::create('string')
+      ->setSetting('defaultValue', '')
+      ->setSetting('jsonPath', [
+        'compensation',
+        'activityBasisInfo',
+        'activityBasisArray',
+        'hankkeen_riskit_vakiinnuttaminen',
+      ]);
+
+    $info['extra_info'] = DataDefinition::create('string')
+      ->setSetting('jsonPath', ['attachmentsInfo', 'generalInfoArray', 'extraInfo'])
+      ->setSetting('defaultValue', "");
+
+    // $info['muu_liite'] = DataDefinition::create('string')
+
     return $this->propertyDefinitions;
   }
 
