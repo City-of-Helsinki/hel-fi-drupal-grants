037d01765751374c31cbb0de65ae36d87268db1e
Janne Suominen <janne.suominen@siili.com>
2024-10-23 15:56:47 +0300

    Pubsub comments

--- docker/openshift/crons/pubsub.sh
+++ docker/openshift/crons/pubsub.sh
@@ -2,9 +2,9 @@
 
 #echo "Running PubSub daemon: $(date +'%Y-%m-%dT%H:%M:%S%:z')"
 
-while true
-do
+#while true
+#do
   # PubSub process exists with success return code after
   # certain number of messages and should then be restarted.
 #  drush helfi:azure:pubsub-listen || exit 1
-done
+#done
