279c894e2f113c40e97878f4ca3d3bed8ba19246
rpnykanen <48206082+rpnykanen@users.noreply.github.com>
2024-10-23 11:01:38 +0300

    must be completely commented out (#1519)

--- docker/openshift/crons/pubsub.sh
+++ docker/openshift/crons/pubsub.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-#echo "Running PubSub daemon: $(date +'%Y-%m-%dT%H:%M:%S%:z')"
+# echo "Running PubSub daemon: $(date +'%Y-%m-%dT%H:%M:%S%:z')"
 
-while true
-do
+# while true
+# do
   # PubSub process exists with success return code after
   # certain number of messages and should then be restarted.
-#  drush helfi:azure:pubsub-listen || exit 1
-done
+  # drush helfi:azure:pubsub-listen || exit 1
+#done
