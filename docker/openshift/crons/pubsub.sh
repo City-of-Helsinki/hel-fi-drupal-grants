0b0793b767dc9b04df22ac1a31edee36e866ba74
hel-platta-automation <95360595+hel-platta-automation@users.noreply.github.com>
2024-10-25 05:16:08 +0000

    Update configuration

--- docker/openshift/crons/pubsub.sh
+++ docker/openshift/crons/pubsub.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 
-#echo "Running PubSub daemon: $(date +'%Y-%m-%dT%H:%M:%S%:z')"
+if [ -z "$DRUPAL_PUBSUB_VAULT" ]; then
+  echo "PubSub is not configured, exiting."
+  exit 0
+fi
 
-#while true
-#do
+echo "Running PubSub daemon: $(date +'%Y-%m-%dT%H:%M:%S%:z')"
+
+while true
+do
   # PubSub process exists with success return code after
   # certain number of messages and should then be restarted.
-#  drush helfi:azure:pubsub-listen || exit 1
-#done
+  drush helfi:azure:pubsub-listen || exit 1
+done
