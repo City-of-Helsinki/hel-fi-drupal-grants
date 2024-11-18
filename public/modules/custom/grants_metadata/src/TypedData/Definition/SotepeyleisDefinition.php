532423e90dc43205ae14aba5e6499287af05503b
Jere Ljungberg <jere@mysteerio.fi>
2024-11-18 11:00:09 +0200

    UHF-10456: Add configs

---
+++ public/modules/custom/grants_metadata/src/TypedData/Definition/SotepeyleisDefinition.php
@@ -0,0 +1,163 @@
+<?php
+
+namespace Drupal\grants_metadata\TypedData\Definition;
+
+use Drupal\Core\TypedData\ComplexDataDefinitionBase;
+use Drupal\Core\TypedData\DataDefinition;
+use Drupal\Core\TypedData\ListDataDefinition;
+
+/**
+ * Define SotepeyleisDefinition data.
+ */
+class SotepeyleisDefinition extends ComplexDataDefinitionBase {
+
+  use ApplicationDefinitionTrait;
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getPropertyDefinitions() {
+    if (!isset($this->propertyDefinitions)) {
+
+      $info = &$this->propertyDefinitions;
+
+      foreach ($this->getBaseProperties() as $key => $property) {
+        $info[$key] = $property;
+      }
+    }
+
+    $info['members_applicant_person_local'] = DataDefinition::create('integer')
+      ->setSetting('defaultValue', "")
+      ->setSetting('valueCallback', [
+        '\Drupal\grants_handler\Plugin\WebformHandler\GrantsHandler',
+        'convertToInt',
+      ])
+      ->setSetting('typeOverride', [
+        'dataType' => 'integer',
+        'jsonType' => 'int',
+      ])
+      ->setSetting('jsonPath', [
+        'compensation',
+        'activitiesInfoArray',
+        'membersApplicantPersonLocal',
+      ]);
+
+    $info['members_applicant_person_global'] = DataDefinition::create('integer')
+      ->setSetting('defaultValue', "")
+      ->setSetting('valueCallback', [
+        '\Drupal\grants_handler\Plugin\WebformHandler\GrantsHandler',
+        'convertToInt',
+      ])
+      ->setSetting('jsonPath', [
+        'compensation',
+        'activitiesInfoArray',
+        'membersApplicantPersonGlobal',
+      ]);
+
+    $info['members_applicant_community_local'] = DataDefinition::create('integer')
+      ->setSetting('defaultValue', "")
+      ->setSetting('valueCallback', [
+        '\Drupal\grants_handler\Plugin\WebformHandler\GrantsHandler',
+        'convertToInt',
+      ])
+      ->setSetting('jsonPath', [
+        'compensation',
+        'activitiesInfoArray',
+        'membersApplicantCommunityLocal',
+      ]);
+
+    $info['members_applicant_community_global'] = DataDefinition::create('integer')
+      ->setSetting('valueCallback', [
+        '\Drupal\grants_handler\Plugin\WebformHandler\GrantsHandler',
+        'convertToInt',
+      ])
+      ->setSetting('jsonPath', [
+        'compensation',
+        'activitiesInfoArray',
+        'membersApplicantCommunityGlobal',
+      ]);
+
+    $info['subventions'] = ListDataDefinition::create('grants_metadata_compensation_type')
+      ->setSetting('jsonPath', [
+        'compensation',
+        'compensationInfo',
+        'compensationArray',
+      ]);
+
+    $info['purpose'] = DataDefinition::create('string')
+      ->setSetting('jsonPath', [
+        'compensation',
+        'compensationInfo',
+        'generalInfoArray',
+        'purpose',
+      ]);
+
+    $info['compensation_boolean'] = DataDefinition::create('boolean')
+      ->setRequired(TRUE)
+      ->setSetting('typeOverride', [
+        'dataType' => 'string',
+        'jsonType' => 'bool',
+      ])
+      ->setSetting('jsonPath', [
+        'compensation',
+        'compensationInfo',
+        'generalInfoArray',
+        'compensationPreviousYear',
+      ]);
+
+    $info['compensation_explanation'] = DataDefinition::create('string')
+      ->setSetting('defaultValue', "")
+      ->setSetting('jsonPath', [
+        'compensation',
+        'compensationInfo',
+        'generalInfoArray',
+        'explanation',
+      ]);
+
+    $info['fee_person'] = DataDefinition::create('float')
+      ->setSetting('jsonPath', [
+        'compensation',
+        'activitiesInfoArray',
+        'feePerson',
+      ])
+      ->setSetting('valueCallback', [
+        '\Drupal\grants_handler\Plugin\WebformHandler\GrantsHandler',
+        'convertToFloat',
+      ])
+      ->setSetting('typeOverride', [
+        'dataType' => 'string',
+        'jsonType' => 'float',
+      ]);
+
+    $info['fee_community'] = DataDefinition::create('float')
+      ->setSetting('jsonPath', [
+        'compensation',
+        'activitiesInfoArray',
+        'feeCommunity',
+      ])
+      ->setSetting('valueCallback', [
+        '\Drupal\grants_handler\Plugin\WebformHandler\GrantsHandler',
+        'convertToFloat',
+      ])
+      ->setSetting('typeOverride', [
+        'dataType' => 'string',
+        'jsonType' => 'float',
+      ]);
+
+    return $this->propertyDefinitions;
+  }
+
+  /**
+   * Override property definition.
+   *
+   * @param string $name
+   *   Property name.
+   *
+   * @return \Drupal\Core\TypedData\DataDefinitionInterface|void|null
+   *   Property definition.
+   */
+  public function getPropertyDefinition($name) {
+    $retval = parent::getPropertyDefinition($name);
+    return $retval;
+  }
+}
