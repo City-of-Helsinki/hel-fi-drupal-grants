d3a3146f20f82fab43366f4147b2edc72ad27126
rpnykanen <48206082+rpnykanen@users.noreply.github.com>
2025-01-23 13:27:00 +0200

    updated error message and translations (#1629)

--- public/modules/custom/grants_handler/src/Plugin/WebformHandler/GrantsHandler.php
+++ public/modules/custom/grants_handler/src/Plugin/WebformHandler/GrantsHandler.php
@@ -793,7 +793,7 @@ moment and reload the page.',
         default:
           // We show integrity error earlier, so suppress error here.
           if ($dataIntegrityStatus == 'OK') {
-            $errorMsg = $this->t('Application period is closed. You can edit the draft, but not submit it.');
+            $errorMsg = $this->t('The application is being processed. The application cannot be edited or submitted.');
           }
 
           $form['actions']['submit']['#disabled'] = TRUE;
