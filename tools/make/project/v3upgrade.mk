dda953f5971daa1f5ca876941fbf15caa6b279db
hel-platta-automation <95360595+hel-platta-automation@users.noreply.github.com>
2025-01-24 15:03:43 +0000

    Update configuration

--- tools/make/project/v3upgrade.mk
+++
@@ -1,7 +0,0 @@
-PHONY += drush-major-update-config
-drush-major-update-config: ## Update translations.
-	$(call drush,helfi:platform-config:update-config)
-
-PHONY += drush-major-update-db
-drush-major-update-db: ## Update translations.
-	$(call drush,helfi:platform-config:update-database)
