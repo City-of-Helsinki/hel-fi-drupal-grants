71fa9dc8f7058dfe30e2da82af26933be65c9615
Markus Kalijärvi <markus.kalijarvi@druid.fi>
2025-03-17 08:56:51 +0200

    Wait less when running actions.

--- e2e/playwright.config.ts
+++ e2e/playwright.config.ts
@@ -22,9 +22,9 @@ export default defineConfig({
     : 'html',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
-    /* Wait for maximum of 180 seconds. Drop the timeout to 60s when */
+    /* Wait for maximum of 120 seconds. Drop the timeout to 60s when */
     /* development server cpu and memory issues have been fixed. */
-    actionTimeout: 180 * 1000,
+    actionTimeout: 120 * 1000,
     /* Base URL to use in actions like `await page.goto('/')`. */
     baseURL: process.env.TEST_BASEURL ?? "https://hel-fi-drupal-grant-applications.docker.so",
     ignoreHTTPSErrors: true,
