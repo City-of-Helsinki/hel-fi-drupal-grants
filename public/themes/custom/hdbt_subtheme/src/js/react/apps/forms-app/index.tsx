8eb70de115bda90515a045d95c9bd1a07fa4c4e9
Jere Ljungberg <jere@mysteerio.fi>
2025-03-25 16:31:36 +0200

    Test vite for bundling

--- public/themes/custom/hdbt_subtheme/src/js/react/apps/forms-app/index.tsx
+++ public/themes/custom/hdbt_subtheme/src/js/react/apps/forms-app/index.tsx
@@ -2,13 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { ErrorBoundary } from '@sentry/react';
 
-import initSentry from '@/react/common/helpers/Sentry';
+// import initSentry from '@/react/common/helpers/Sentry';
 import { FormNotFoundError } from './components/FormNotFoundError';
 import FormWrapper from './containers/FormWrapper';
 import { GeneralError } from './components/GeneralError';
 import { ToastStack } from './components/ToastStack';
 
-initSentry();
+// initSentry();
 
 const rootSelector: string = 'grants-react-form';
 const rootElement: HTMLElement | null = document.getElementById(rootSelector);
---
+++ public/themes/custom/hdbt_subtheme/vite.config.js
@@ -0,0 +1,55 @@
+import { defineConfig, loadEnv } from 'vite';
+import path from 'path';
+import { globSync } from 'glob';
+import react from '@vitejs/plugin-react';
+import analyzer from 'vite-bundle-analyzer';
+
+export default defineConfig(({ command, mode }) => {
+  const { VITE_HOST, VITE_ORIGIN, VITE_PORT, } = loadEnv(mode, process.cwd(), '');
+
+  return {
+    base: '',
+    css: {
+      postcss: {
+        to: 'css/styles.min.css',
+      },
+      preprocessorOptions: {
+        scss: {
+          additionalData: `$debug_mode: false;`,
+        },
+      },
+    },
+    build: {
+      cssCodeSplit: false,
+      manifest: false,
+      outDir: path.resolve(__dirname, 'dist'),
+      manualChunks: false,
+      rollupOptions: {
+        input: {
+          'forms-app': './src/js/react/apps/forms-app/index.tsx',
+          styles: './src/scss/styles.scss',
+          ...Object.fromEntries(
+            globSync('src/**/*.js').map(file => [
+              path.parse(file).name,
+              `./${file}`,
+            ])
+          ),
+        },
+        output: {
+          entryFileNames: 'js/[name].min.js',
+        },
+      },
+    },
+    plugins: [analyzer(), react()],
+    publicDir: false,
+    resolve: {
+      alias: [
+        {
+          '@/react/common': path.resolve(__dirname, '../../contrib/hdbt/src/js/react/common/'),
+          find: /^~(.*)$/,
+          replacement: '$1',
+        },
+      ],
+    },
+  }
+})
