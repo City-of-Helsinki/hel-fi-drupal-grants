0940c97b359618d6cc12fbf1db71874c3960c3f8
Jere Ljungberg <jere@mysteerio.fi>
2025-06-03 10:24:58 +0300

    UHF-11314: Fix schema issue

--- public/themes/custom/hdbt_subtheme/src/js/react/apps/forms-app/components/TextParagraph.tsx
+++ public/themes/custom/hdbt_subtheme/src/js/react/apps/forms-app/components/TextParagraph.tsx
@@ -1,6 +1,7 @@
 import { FieldProps, UiSchema } from '@rjsf/utils';
 import { Notification } from 'hds-react';
 import { useAtomValue } from 'jotai';
+import { JSONSchema7Definition } from 'json-schema';
 
 import { shouldRenderPreviewAtom } from '../store';
 
@@ -16,14 +17,26 @@ export const TextParagraph = ({ schema, uiSchema }: FieldProps) => {
     return null;
   }
 
+  const getTitle = (p: JSONSchema7Definition) => {
+    if (p === false) {
+      return '';
+    }
+
+    if (typeof p === 'object') {
+      return p?.title;
+    }
+
+    return p;
+  };
+
   return variant === 'infobox' ? (
     <Notification label={title}>
-      {Array.isArray(items) && items?.length && items.map((p, index: number) => <p key={index}>{p}</p>)}
+      {Array.isArray(items) && items?.length && items.map((p, index: number) => <p key={index}>{getTitle(p)}</p>)}
     </Notification>
   ) : (
     <div>
       {title && <h4>{title}</h4>}
-      {Array.isArray(items) && items?.length && items.map((p, index: number) => <p key={index}>{p}</p>)}
+      {Array.isArray(items) && items?.length && items.map((p, index: number) => <p key={index}>{getTitle(p)}</p>)}
     </div>
   )
 }
