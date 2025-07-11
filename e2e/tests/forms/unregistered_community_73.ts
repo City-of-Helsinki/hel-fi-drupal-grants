130bfc8336069e5fa2c56ab54a0b5f8edf9df5c6
Tero Elonen <tero.elonen@druid.fi>
2025-07-11 13:58:40 +0300

    UHF-12009: Add e2e tests for the form ID73

---
+++ e2e/tests/forms/unregistered_community_73.ts
@@ -0,0 +1,120 @@
+import {Page, test} from '@playwright/test';
+import {FormData, PageHandlers, FormPage} from "../../utils/data/test_data";
+import {fillHakijanTiedotUnregisteredCommunity} from "../../utils/form_helpers";
+import {fillFormField, uploadFile} from "../../utils/input_helpers";
+import {generateTests} from "../../utils/test_generator_helpers";
+import {Role, selectRole} from "../../utils/auth_helpers";
+import {unRegisteredCommunityApplications as applicationData} from '../../utils/data/application_data';
+
+const formPages: PageHandlers = {
+  '1_hakijan_tiedot': async (page: Page, {items}: FormPage) => {
+    await fillHakijanTiedotUnregisteredCommunity(items, page);
+  },
+  '2_avustustiedot': async (page: Page, {items}: FormPage) => {
+
+    if (items['edit-acting-year']) {
+      await fillFormField(page, items['edit-acting-year'], 'edit-acting-year');
+    }
+
+    if (items['edit-subventions-items-0-amount']) {
+      await page.locator('#edit-subventions-items-0-amount')
+        .fill(items['edit-subventions-items-0-amount'].value ?? '');
+    }
+
+  },
+  '3_talousarvio': async (page: Page, {items}: FormPage) => {
+
+    if (items['edit-tulo']) {
+      await fillFormField(page, items['edit-tulo'], 'edit-tulo')
+    }
+
+    if (items['edit-meno']) {
+      await fillFormField(page, items['edit-meno'], 'edit-meno')
+    }
+
+  },
+  'lisatiedot_ja_liitteet': async (page: Page, {items}: FormPage) => {
+
+    if (items['edit-additional-information']) {
+      await page.getByRole('textbox', {name: 'Lisätiedot'})
+        .fill(items['edit-additional-information'].value ?? '');
+    }
+
+    if (items['edit-yhteison-saannot-attachment-upload']) {
+      await uploadFile(
+        page,
+        items['edit-yhteison-saannot-attachment-upload'].selector?.value ?? '',
+        items['edit-yhteison-saannot-attachment-upload'].selector?.resultValue ?? '',
+        items['edit-yhteison-saannot-attachment-upload'].value
+      )
+    }
+
+    if (items['edit-leiri-excel-attachment-upload']) {
+      await uploadFile(
+        page,
+        items['edit-leiri-excel-attachment-upload'].selector?.value ?? '',
+        items['edit-leiri-excel-attachment-upload'].selector?.resultValue ?? '',
+        items['edit-leiri-excel-attachment-upload'].value
+      )
+    }
+
+    if (items['edit-toimintasuunnitelma-attachment-upload']) {
+      await uploadFile(
+        page,
+        items['edit-toimintasuunnitelma-attachment-upload'].selector?.value ?? '',
+        items['edit-toimintasuunnitelma-attachment-upload'].selector?.resultValue ?? '',
+        items['edit-toimintasuunnitelma-attachment-upload'].value
+      )
+    }
+
+    if (items['edit-talousarvio-attachment-upload']) {
+      await uploadFile(
+        page,
+        items['edit-talousarvio-attachment-upload'].selector?.value ?? '',
+        items['edit-talousarvio-attachment-upload'].selector?.resultValue ?? '',
+        items['edit-talousarvio-attachment-upload'].value
+      )
+    }
+
+    if (items['edit-muu-liite']) {
+      await fillFormField(page, items['edit-muu-liite'], 'edit-muu-liite')
+    }
+
+    if (items['edit-extra-info']) {
+      await page.getByLabel('Lisäselvitys liitteistä')
+        .fill(items['edit-extra-info'].value ?? '');
+    }
+
+  },
+  'webform_preview': async (page: Page, {items}: FormPage) => {
+    if (items['accept_terms_1']) {
+      // Check data on confirmation page
+      await page.getByLabel('Vakuutamme, että hakemuksessa ja sen liitteissä antamamme tiedot ovat oikeita, ja hyväksymme avustusehdot').check();
+    }
+  },
+};
+
+test.describe('NUORLEIRI(73)', () => {
+  let page: Page;
+
+  const profileType = 'unregistered_community';
+  const formId = '73';
+
+  test.beforeAll(async ({browser}) => {
+    page = await browser.newPage();
+    await selectRole(page, profileType.toUpperCase() as Role);
+  });
+
+  test.afterAll(async() => {
+    await page.close();
+  });
+
+  const testDataArray: [string, FormData][] = Object.entries(applicationData[formId]);
+  const tests = generateTests(profileType, formId, formPages, testDataArray);
+
+  for (const { testName, testFunction } of tests) {
+    test(testName, async ({browser}) => {
+      await testFunction(page, browser);
+    });
+  }
+});
--- e2e/utils/data/application/application_data_registered_community.ts
+++ e2e/utils/data/application/application_data_registered_community.ts
@@ -20,6 +20,7 @@ import {registeredCommunityApplications_67} from "./application_data_67";
 import {registeredCommunityApplications_68} from "./application_data_68";
 import {registeredCommunityApplications_69} from "./application_data_69";
 import {registeredCommunityApplications_70} from "./application_data_70";
+import {registeredCommunityApplications_73} from "./application_data_73";
 
 const registeredCommunityApplications = {
   29: registeredCommunityApplications_29,
@@ -44,6 +45,7 @@ const registeredCommunityApplications = {
   68: registeredCommunityApplications_68,
   69: registeredCommunityApplications_69,
   70: registeredCommunityApplications_70,
+  73: registeredCommunityApplications_73,
 }
 
 export {
