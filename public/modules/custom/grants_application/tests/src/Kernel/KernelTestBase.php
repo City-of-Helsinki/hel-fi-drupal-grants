0de3202c94f809ab12d1980740a0a2d7492b06de
Markus Kalijärvi <markus.kalijarvi@druid.fi>
2025-09-11 11:22:58 +0300

    Added kernel tests for the application metadata entity.

---
+++ public/modules/custom/grants_application/tests/src/Kernel/KernelTestBase.php
@@ -0,0 +1,62 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Drupal\Tests\grants_application\Kernel;
+
+use Drupal\KernelTests\KernelTestBase as CoreKernelTestBase;
+
+/**
+ * Kernel test base for grants application tests.
+ */
+class KernelTestBase extends CoreKernelTestBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  protected static $modules = [
+    'datetime',
+    'entity',
+    'externalauth',
+    'field',
+    'file',
+    'grants_application',
+    'grants_attachments',
+    'grants_handler',
+    'grants_events',
+    'grants_metadata',
+    'grants_mandate',
+    'grants_profile',
+    'helfi_atv',
+    'helfi_audit_log',
+    'helfi_api_base',
+    'helfi_helsinki_profiili',
+    'helfi_yjdh',
+    'language',
+    'locale',
+    'openid_connect',
+    'openid_connect_logout_redirect',
+    'options',
+    'rest',
+    'serialization',
+    'system',
+    'taxonomy',
+    'text',
+    'user',
+    'webform',
+    'views',
+  ];
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function setUp(): void {
+    parent::setUp();
+    $this->installEntitySchema('user');
+    $this->installEntitySchema('taxonomy_term');
+    $this->installEntitySchema('application_metadata');
+    $this->installSchema('system', ['sequences']);
+    $this->installConfig(['grants_application']);
+  }
+
+}
