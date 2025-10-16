6af5ddbb3ad8a783a547c11bd1666ee9e85e3ae8
Risto Nykänen <risto.nykanen@druid.fi>
2025-10-16 11:24:51 +0300

    UHF-11384: less duplication

--- public/modules/custom/grants_handler/src/Controller/E2eTestController.php
+++ public/modules/custom/grants_handler/src/Controller/E2eTestController.php
@@ -32,29 +32,17 @@ class E2eTestController extends ControllerBase {
       return new JsonResponse('', 403);
     }
 
-    $lookFor = [
-      'appenv' => $this->getAppEnvForAtv(getenv('APP_ENV')),
-      'profile_type' => $profileType,
-    ];
-
-    $parameters = [
-      'lookfor' => $lookFor,
-      'user_id' => $id,
-      'type' => 'grants_profile',
-      'sort' => 'updated_at',
-    ];
-
     try {
-      $documents = $this->atvService->searchDocuments($parameters, TRUE);
+      $documents = $this->queryDocuments($id, $profileType);
     }
     catch (\Throwable $e) {
-      $this->logger->error('Exception while searching for documents: ' . $e->getMessage());
+      $this->logger->error('Exception while searching for test documents: ' . $e->getMessage());
       return new JsonResponse(['Exception while searching for test documents'], 500);
     }
 
     if (empty($documents)) {
       $this->logger->error(
-        'E2E-test requested for latest profile but cannot find.'
+        'E2E-test requested for latest profile but cannot find any'
       );
       return new JsonResponse('Document not found.', 404);
     }
@@ -79,24 +67,12 @@ class E2eTestController extends ControllerBase {
       return new JsonResponse('', 403);
     }
 
-    $lookFor = [
-      'appenv' => $this->getAppEnvForAtv(getenv('APP_ENV')),
-      'profile_type' => $profileType,
-    ];
-
-    $parameters = [
-      'lookfor' => $lookFor,
-      'user_id' => $id,
-      'type' => 'grants_profile',
-      'service_name' => 'AvustushakemusIntegraatio',
-    ];
-
     try {
-      $documents = $this->atvService->searchDocuments($parameters, TRUE);
+      $documents = $this->queryDocuments($id, $profileType);
     }
     catch (\Throwable $e) {
-      $this->logger->error('Exception while searching for documents: ' . $e->getMessage());
-      return new JsonResponse(['Exception while searching for test documents'], 500);
+      $this->logger->error('Exception while removing for documents: ' . $e->getMessage());
+      return new JsonResponse(['Exception while removing test documents'], 500);
     }
 
     // Don't know if requesting for already deleted documents is 500 or 200.
@@ -120,6 +96,33 @@ class E2eTestController extends ControllerBase {
     }
 
     return new JsonResponse('Documents deleted', 200);
+  }
+
+  /**
+   * Query atv for documents.
+   *
+   * @param string $userid
+   *   The user uuid.
+   * @param string $profileType
+   *   The profile type.
+   *
+   * @return array
+   *   The documents.
+   */
+  private function queryDocuments(string $userid, string $profileType) {
+    $lookFor = [
+      'appenv' => $this->getAppEnvForAtv(getenv('APP_ENV')),
+      'profile_type' => $profileType,
+    ];
+
+    $parameters = [
+      'lookfor' => $lookFor,
+      'user_id' => $userid,
+      'type' => 'grants_profile',
+      'sort' => 'updated_at',
+    ];
+
+    return $this->atvService->searchDocuments($parameters, TRUE);
   }
 
   /**
