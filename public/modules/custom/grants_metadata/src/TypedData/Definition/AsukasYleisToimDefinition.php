39431e25e59abdc03fd27c6f99bbacb23ef38974
Markus Kalijärvi <markus.kalijarvi@druid.fi>
2025-10-22 15:43:01 +0300

    Reverted AsukasYleisToimDefinition.php

--- public/modules/custom/grants_metadata/src/TypedData/Definition/AsukasYleisToimDefinition.php
+++ public/modules/custom/grants_metadata/src/TypedData/Definition/AsukasYleisToimDefinition.php
@@ -2,12 +2,16 @@
 
 namespace Drupal\grants_metadata\TypedData\Definition;
 
+use Drupal\Core\TypedData\ComplexDataDefinitionBase;
 use Drupal\Core\TypedData\DataDefinition;
+use Drupal\Core\TypedData\ListDataDefinition;
 
 /**
  * Define Asukasosallisuus, yleis- ja toiminta-avustushakemus data.
  */
-class AsukasYleisToimDefinition extends YleisDefinitionBase {
+class AsukasYleisToimDefinition extends ComplexDataDefinitionBase {
+
+  use ApplicationDefinitionTrait;
 
   /**
    * Base data definitions for all.
@@ -16,13 +20,72 @@ class AsukasYleisToimDefinition extends YleisDefinitionBase {
    *   Property definitions.
    */
   public function getPropertyDefinitions(): array {
-    $this->propertyDefinitions = parent::getPropertyDefinitions();
-    $info = &$this->propertyDefinitions;
+    if (!isset($this->propertyDefinitions)) {
+      $info = &$this->propertyDefinitions;
 
-    // Remove the compensation_purpose from base class.
-    unset($info['compensation_purpose']);
+      foreach ($this->getBaseProperties() as $key => $property) {
+        $info[$key] = $property;
+      }
+    }
 
-    // Add purpose with the same JSON path as compensation_purpose.
+    $info['members_applicant_person_local'] = DataDefinition::create('integer')
+      ->setSetting('defaultValue', "")
+      ->setSetting('valueCallback', [
+        '\Drupal\grants_handler\Plugin\WebformHandler\GrantsHandler',
+        'convertToInt',
+      ])
+      ->setSetting('typeOverride', [
+        'dataType' => 'integer',
+        'jsonType' => 'int',
+      ])
+      ->setSetting('jsonPath', [
+        'compensation',
+        'activitiesInfoArray',
+        'membersApplicantPersonLocal',
+      ]);
+
+    $info['members_applicant_person_global'] = DataDefinition::create('integer')
+      ->setSetting('defaultValue', "")
+      ->setSetting('valueCallback', [
+        '\Drupal\grants_handler\Plugin\WebformHandler\GrantsHandler',
+        'convertToInt',
+      ])
+      ->setSetting('jsonPath', [
+        'compensation',
+        'activitiesInfoArray',
+        'membersApplicantPersonGlobal',
+      ]);
+
+    $info['members_applicant_community_local'] = DataDefinition::create('integer')
+      ->setSetting('defaultValue', "")
+      ->setSetting('valueCallback', [
+        '\Drupal\grants_handler\Plugin\WebformHandler\GrantsHandler',
+        'convertToInt',
+      ])
+      ->setSetting('jsonPath', [
+        'compensation',
+        'activitiesInfoArray',
+        'membersApplicantCommunityLocal',
+      ]);
+
+    $info['members_applicant_community_global'] = DataDefinition::create('integer')
+      ->setSetting('valueCallback', [
+        '\Drupal\grants_handler\Plugin\WebformHandler\GrantsHandler',
+        'convertToInt',
+      ])
+      ->setSetting('jsonPath', [
+        'compensation',
+        'activitiesInfoArray',
+        'membersApplicantCommunityGlobal',
+      ]);
+
+    $info['subventions'] = ListDataDefinition::create('grants_metadata_compensation_type')
+      ->setSetting('jsonPath', [
+        'compensation',
+        'compensationInfo',
+        'compensationArray',
+      ]);
+
     $info['purpose'] = DataDefinition::create('string')
       ->setSetting('jsonPath', [
         'compensation',
@@ -31,7 +94,97 @@ class AsukasYleisToimDefinition extends YleisDefinitionBase {
         'purpose',
       ]);
 
+    $info['compensation_boolean'] = DataDefinition::create('boolean')
+      ->setRequired(TRUE)
+      ->setSetting('typeOverride', [
+        'dataType' => 'string',
+        'jsonType' => 'bool',
+      ])
+      ->setSetting('jsonPath', [
+        'compensation',
+        'compensationInfo',
+        'generalInfoArray',
+        'compensationPreviousYear',
+      ]);
+
+    $info['compensation_explanation'] = DataDefinition::create('string')
+      ->setSetting('defaultValue', "")
+      ->setSetting('jsonPath', [
+        'compensation',
+        'compensationInfo',
+        'generalInfoArray',
+        'explanation',
+      ]);
+
+    $info['who_benefits'] = DataDefinition::create('string')
+      ->setSetting('jsonPath', [
+        'compensation',
+        'compensationInfo',
+        'generalInfoArray',
+        'whoBenefits',
+      ]);
+
+    $info['changes_on_success'] = DataDefinition::create('string')
+      ->setSetting('jsonPath', [
+        'compensation',
+        'compensationInfo',
+        'generalInfoArray',
+        'changesOnSuccess',
+      ]);
+
+    $info['results_of_activities'] = DataDefinition::create('string')
+      ->setSetting('jsonPath', [
+        'compensation',
+        'compensationInfo',
+        'generalInfoArray',
+        'resultsOfActivities',
+      ]);
+
+    $info['fee_person'] = DataDefinition::create('float')
+      ->setSetting('jsonPath', [
+        'compensation',
+        'activitiesInfoArray',
+        'feePerson',
+      ])
+      ->setSetting('valueCallback', [
+        '\Drupal\grants_handler\Plugin\WebformHandler\GrantsHandler',
+        'convertToFloat',
+      ])
+      ->setSetting('typeOverride', [
+        'dataType' => 'string',
+        'jsonType' => 'float',
+      ]);
+
+    $info['fee_community'] = DataDefinition::create('float')
+      ->setSetting('jsonPath', [
+        'compensation',
+        'activitiesInfoArray',
+        'feeCommunity',
+      ])
+      ->setSetting('valueCallback', [
+        '\Drupal\grants_handler\Plugin\WebformHandler\GrantsHandler',
+        'convertToFloat',
+      ])
+      ->setSetting('typeOverride', [
+        'dataType' => 'string',
+        'jsonType' => 'float',
+      ]);
+
     return $this->propertyDefinitions;
+  }
+
+  /**
+   * Override property definition.
+   *
+   * @param string $name
+   *   Property name.
+   *
+   * @return \Drupal\Core\TypedData\DataDefinitionInterface|void|null
+   *   Property definition.
+   */
+  public function getPropertyDefinition($name) {
+    $retval = parent::getPropertyDefinition($name);
+    return $retval;
   }
 
 }
