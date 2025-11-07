4465ed75e911578b9c93c0d2868048fddb1e2663
Jere Ljungberg <jere@mysteerio.fi>
2025-11-07 11:41:29 +0200

    UHF-12301: Include formDescription in the file array

--- public/themes/custom/hdbt_subtheme/src/js/react/apps/forms-app/components/FileInput.tsx
+++ public/themes/custom/hdbt_subtheme/src/js/react/apps/forms-app/components/FileInput.tsx
@@ -1,14 +1,17 @@
-import React, { useCallback } from "react";
+import { Checkbox, FileInput as HDSFileInput, TextInput } from "hds-react";
 import { FieldProps, UiSchema } from "@rjsf/utils";
-import { Checkbox, FileInput as HDSFileInput } from "hds-react";
 import { useAtomValue } from "jotai";
+import { useCallback } from "react";
+import { useTranslation } from 'react-i18next';
+
 import { formConfigAtom, getApplicationNumberAtom, shouldRenderPreviewAtom } from "../store";
 import { formatErrors } from '../utils';
 
 type ATVFile = {
-  fileType: string;
-  fileName: string;
+  fileDescription?: string;
   fileId: number;
+  fileName: string;
+  fileType: string;
   href: string;
   size: number;
 }
@@ -63,6 +66,7 @@ export const FileInput = ({
   required,
   uiSchema,
 }: FieldProps) => {
+  const { t } = useTranslation();
   const shouldRenderPreview = useAtomValue(shouldRenderPreviewAtom);
   const applicationNumber = useAtomValue(getApplicationNumberAtom);
   const { token } = useAtomValue(formConfigAtom)!;
@@ -80,11 +84,11 @@ export const FileInput = ({
     )
   }
 
-  const handleChange = useCallback(async (files: File[]) => {
+  const handleChange = useCallback(async (files: File[], existingData: any) => {
     if (!files.length) {
       onChange(undefined);
       return;
-    } 
+    }
 
     const result = await uploadFiles(name, applicationNumber, token, files, fileType);
 
@@ -94,8 +98,11 @@ export const FileInput = ({
 
     const { href: integrationID, ...rest } = result;
 
+    const fileDescription = existingData?.fileDescription || '';
+
     onChange({
       integrationID,
+      fileDescription,
       isDeliveredLater: false,
       isIncludedInOtherFile: false,
       isNewAttachment: true,
@@ -116,13 +123,31 @@ export const FileInput = ({
     language={drupalSettings.path.currentLanguage}
     // 20mb in bytes
     maxSize={20 * 1024 * 1024}
-    onChange={handleChange}
+    onChange={(files: File[]) => {
+      handleChange(files, formData);
+    }}
     required={required}
   />;
 
+  const descriptionElement = <TextInput
+    id={`${name}-description`}
+    label={t('file_description.title')}
+    onChange={(e) => {
+      onChange({
+        ...formData,
+        fileDescription: e.target.value,
+      });
+    }}
+    value={formData?.fileDescription || ''}
+  />;
+
   if (uiSchema?.['misc:variant'] === 'simple') {
-    inputElement.props.className = 'hdbt-form--fileinput';
-    return inputElement;
+    return (
+      <div className="hdbt-form--fileinput">
+        {inputElement}
+        {descriptionElement}
+      </div>
+    );
   }
 
   return (
