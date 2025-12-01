f4c4556c257c5a96668d736cf974eef994119376
Risto Nykänen <risto.nykanen@druid.fi>
2025-12-01 08:35:39 +0200

    UHF-12306: added asserts

--- public/modules/custom/grants_application/tests/src/Kernel/JsonSchemaValidatorTest.php
+++ public/modules/custom/grants_application/tests/src/Kernel/JsonSchemaValidatorTest.php
@@ -20,7 +20,20 @@ final class JsonSchemaValidatorTest extends KernelTestBase {
   public function testValidator(): void {
     /** @var JsonSchemaValidator $validator */
     $validator = $this->container->get(JsonSchemaValidator::class);
-    $schema = '{"$id": "https://example.com/address.schema.json","$schema": "https://json-schema.org/draft/2020-12/schema","description": "An address similar to http://microformats.org/wiki/h-card","type": "object","properties": {"test": {"type": "string"}}}';
-    $result = $validator->validate(json_decode('{"test": "value"}'), json_decode($schema));
+    $schema = '{"$id": "https://example.com/address.schema.json","$schema": "https://json-schema.org/draft/2020-12/schema","description": "description","type": "object","properties": {"test": {"type": "string"}}}';
+
+    $value1 = '{}';
+    $result = $validator->validate(json_decode($value1), json_decode($schema));
+    $this->assertTrue($result);
+
+    $value2 = '{"test": "value"}';
+    $result2 = $validator->validate(json_decode($value2), json_decode($schema));
+    $this->assertIsNotArray($result2);
+    $this->assertTrue($result2);
+
+    $value3= '{"test": 12345}';
+    $result3 = $validator->validate(json_decode($value3), json_decode($schema));
+    $this->assertIsArray($result3);
+    $this->assertTrue(isset($result3[0]['message']));
   }
 }
