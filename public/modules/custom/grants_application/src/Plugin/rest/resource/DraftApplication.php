dfe13c7016e7900edf6abc4fb56d5026cf24a4ea
Jere Ljungberg <jere@mysteerio.fi>
2025-12-01 14:34:29 +0200

    UHF-12310: Enable copying react applications

--- public/modules/custom/grants_application/src/Plugin/rest/resource/DraftApplication.php
+++ public/modules/custom/grants_application/src/Plugin/rest/resource/DraftApplication.php
@@ -37,7 +37,7 @@ use Symfony\Component\Routing\RouteCollection;
   label: new TranslatableMarkup("Application"),
   uri_paths: [
     "canonical" => "/applications/{application_type_id}/{application_number}",
-    "create" => "/applications/{application_type_id}/{application_number}",
+    "create" => "/applications/{application_type_id}/{application_number}/{copy_from?}",
     "edit" => "/applications/{application_type_id}/{application_number}",
   ]
 )]
@@ -229,17 +229,32 @@ final class DraftApplication extends ResourceBase {
    *
    * @param int $application_type_id
    *   The application type id.
+   * @param string|null $copy_from
+   *   The application number to copy from.
    *
    * @return \Symfony\Component\HttpFoundation\JsonResponse
    *   The response.
    */
-  public function post(int $application_type_id): JsonResponse {
+  public function post(int $application_type_id, string|null $copy_from = NULL): JsonResponse {
     try {
       $settings = $this->formSettingsService->getFormSettings($application_type_id);
     }
     catch (\Exception $e) {
       // Cannot find form by application type id.
       return new JsonResponse([], 404);
+    }
+
+    $form_data = [];
+    if ($copy_from) {
+      try {
+        $copy_document = $this->atvService->getDocument($copy_from);
+        $copy_content = $copy_document->getContent();
+        $form_data = $copy_content['form_data'] ?? $copy_content['compensation']['form_data'];
+      }
+      catch (\Throwable $e) {
+        // Unable to fetch the document to copy from.
+        return new JsonResponse([], 500);
+      }
     }
 
     try {
@@ -284,7 +299,7 @@ final class DraftApplication extends ResourceBase {
     // Grants_events requires the events-array to exist.
     // And compensation must be json-object.
     $document->setContent([
-      'form_data' => [],
+      'form_data' => $form_data,
       'compensation' => [
         'applicantInfoArray' => []
       ],
