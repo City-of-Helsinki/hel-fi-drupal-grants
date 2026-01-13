c8a810b6cbbb8bfd2b722b78bfb56a6f33a33e64
Santeri Hurnanen <santeri.hurnanen@fame.fi>
2026-01-13 14:39:48 +0200

    Run linter and phpstan

--- public/modules/custom/helfi_audit_log/src/ResilientLogger.php
+++ public/modules/custom/helfi_audit_log/src/ResilientLogger.php
@@ -6,17 +6,21 @@ namespace Drupal\helfi_audit_log;
 
 use ResilientLogger\ResilientLogger as ResilientLoggerBase;
 use Drupal\Core\Site\Settings;
-use ResilientLogger\Types;
 
 /**
- * @phpstan-import-type ResilientLoggerOptions from Types
+ * Implements resilient logger.
+ *
+ * @phpstan-import-type ResilientLoggerOptions from \ResilientLogger\Types
  */
 class ResilientLogger extends ResilientLoggerBase {
+
+  /**
+   * Create from settings.
+   */
   public static function createFromSettings(Settings $settings): self {
     /** @var ResilientLoggerOptions $options */
     $options = $settings->get('resilient_logger', []);
     return self::create($options);
   }
-}
 
-?>
+}
--- public/modules/custom/helfi_audit_log/src/ResilientLoggerTasks.php
+++ public/modules/custom/helfi_audit_log/src/ResilientLoggerTasks.php
@@ -10,6 +10,19 @@ use Drupal\Core\State\StateInterface;
 use Drupal\Core\Logger\LoggerChannelFactoryInterface;
 use Drupal\Core\Site\Settings;
 
+/**
+ * Drupal-specific helper for scheduling ResilientLogger tasks.
+ *
+ * Tasks are not executed immediately but are evaluated during Drupal's cron
+ * runs. Each task runs conditionally based on time offsets and the last
+ * execution time, not on every cron trigger.
+ *
+ * @see https://www.php.net/manual/en/function.strtotime.php
+ *
+ * Parameter block "resilient_logger.tasks" is used to read values for
+ * "offset_submit" and "offset_clear". If these are not found, defaults
+ * will be used instead.
+ */
 class ResilientLoggerTasks {
   private const SETTINGS_NAME = "resilient_logger";
   private const LOGGER_CHANNEL = "resilient_logger.tasks";
@@ -23,48 +36,54 @@ class ResilientLoggerTasks {
   private const STATE_KEY_PREV_SUBMIT = "resilient_logger.prev_submit_unsent";
   private const STATE_KEY_PREV_CLEAR = "resilient_logger.prev_clear_sent";
 
+  /**
+   * The logger.
+   */
   private LoggerInterface $logger;
 
   /**
-   * Drupal-specific helper for scheduling ResilientLogger tasks.
-   * Tasks are not executed immediately but are evaluated during Drupal's cron runs.
-   * Each task runs conditionally based on time offsets and the last execution time,
-   * not on every cron trigger.
-   * @see https://www.php.net/manual/en/function.strtotime.php
-   * 
-   * Parameter block "resilient_logger.tasks" is used to read values for
-   * "offset_submit" and "offset_clear". If these are not found, defaults
-   * will be used instead.
+   * String representation of the next time submit unsent entries should run.
+   *
+   * Defaults to +15min from previous one.
    */
+  private string $submitDateOffset;
+
+  /**
+   * String representation of the next time clear old entries should run.
+   *
+   * Defaults to first day of the next month at 00:00.
+   */
+  private string $clearDateOffset;
+
   public function __construct(
     private StateInterface $state,
     private ResilientLogger $service,
     LoggerChannelFactoryInterface $loggerFactory,
-    Settings $settings
+    Settings $settings,
   ) {
     // Retrieve your resilient_logger settings.
     $config = $settings->get(self::SETTINGS_NAME, []);
 
     $this->logger = $loggerFactory->get(self::LOGGER_CHANNEL);
 
-    /**
-     * String representation of the next time submit unsent entries task should run.
-     * Defaults to +15min from previous one.
-     */ 
     $this->submitDateOffset = $config[self::PARAM_KEY_OFFSET_SUBMIT] ?? self::DEFAULT_OFFSET_SUBMIT;
 
-    /**
-     * String representation of the next time clear old entries task should run.
-     * Defaults to first day of the next month at 00:00.
-     */
     $this->clearDateOffset = $config[self::PARAM_KEY_OFFSET_CLEAR] ?? self::DEFAULT_OFFSET_CLEAR;
   }
 
+  /**
+   * Handle cron task.
+   *
+   * @see \helfi_audit_log_cron
+   */
   public function handleTasks(int $currentTime) {
     $this->handleSubmitUnsentEntries($currentTime);
     $this->handleClearSentEntries($currentTime);
   }
 
+  /**
+   * Submits audit log events from the database.
+   */
   public function handleSubmitUnsentEntries(int $currentTime) {
     $shouldSubmitUnsent = $this->isTaskDue(
       self::STATE_KEY_PREV_SUBMIT,
@@ -79,6 +98,9 @@ class ResilientLoggerTasks {
     }
   }
 
+  /**
+   * Clears sent audit log events from the database.
+   */
   public function handleClearSentEntries(int $currentTime) {
     $shouldClearSent = $this->isTaskDue(
       self::STATE_KEY_PREV_CLEAR,
@@ -92,25 +114,27 @@ class ResilientLoggerTasks {
       $this->state->set(self::STATE_KEY_PREV_CLEAR, $currentTime);
     }
   }
-  
+
+  /**
+   * Checks if the task should run.
+   */
   public function isTaskDue(
     string $stateKey,
     string $dateOffset,
-    int $currentTime
+    int $currentTime,
   ): bool {
-    if ($dateOffset == null) {
-      return false;
+    if ($dateOffset == NULL) {
+      return FALSE;
     }
 
     $prevTriggerAt = $this->state->get($stateKey, 0);
     $nextTriggerAt = strtotime($dateOffset, $prevTriggerAt);
 
-    if ($nextTriggerAt === false) {
-      return false;
+    if ($nextTriggerAt === FALSE) {
+      return FALSE;
     }
 
     return $nextTriggerAt < $currentTime;
   }
-}
 
-?>
+}
