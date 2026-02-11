bcad645b4046ed768bc3f5b3a6265fce60b25450
Markus Kalijärvi <markus.kalijarvi@druid.fi>
2026-02-11 21:48:58 +0200

    Reduced code duplication.

--- public/modules/custom/grants_application_search/src/Plugin/views/filter/CanonicalSubventionType.php
+++ public/modules/custom/grants_application_search/src/Plugin/views/filter/CanonicalSubventionType.php
@@ -4,12 +4,8 @@ declare(strict_types=1);
 
 namespace Drupal\grants_application_search\Plugin\views\filter;
 
-use Drupal\Core\Entity\EntityTypeManagerInterface;
-use Drupal\Core\Language\LanguageInterface;
-use Drupal\Core\Language\LanguageManagerInterface;
 use Drupal\grants_application_search\Plugin\search_api\processor\CanonicalFields;
 use Drupal\views\Attribute\ViewsFilter;
-use Symfony\Component\DependencyInjection\ContainerInterface;
 
 /**
  * Canonical subvention type filter.
@@ -17,9 +13,7 @@ use Symfony\Component\DependencyInjection\ContainerInterface;
  * @todo UHF-12853: Review this class when removing the webform functionality.
  */
 #[ViewsFilter('canonical_subvention_type_select')]
-final class CanonicalSubventionType extends CanonicalSelectFilterBase {
-
-  private const string VOCABULARY = 'avustuslaji';
+final class CanonicalSubventionType extends CanonicalTaxonomySelectFilterBase {
 
   /**
    * Cached reverse map: canonical ID -> term ID.
@@ -27,34 +21,6 @@ final class CanonicalSubventionType extends CanonicalSelectFilterBase {
    * @var array|null
    */
   private static ?array $reverseMap = NULL;
-
-  public function __construct(
-    array $configuration,
-    $plugin_id,
-    $plugin_definition,
-    private readonly EntityTypeManagerInterface $entityTypeManager,
-    private readonly LanguageManagerInterface $languageManager,
-  ) {
-    parent::__construct($configuration, $plugin_id, $plugin_definition);
-  }
-
-  /**
-   * {@inheritdoc}
-   */
-  public static function create(
-    ContainerInterface $container,
-    array $configuration,
-    $plugin_id,
-    $plugin_definition,
-  ): self {
-    return new self(
-      $configuration,
-      $plugin_id,
-      $plugin_definition,
-      $container->get('entity_type.manager'),
-      $container->get('language_manager'),
-    );
-  }
 
   /**
    * {@inheritdoc}
@@ -66,38 +32,16 @@ final class CanonicalSubventionType extends CanonicalSelectFilterBase {
   /**
    * {@inheritdoc}
    */
-  protected function buildSelectOptions(): array {
-    $langcode = $this->languageManager
-      ->getCurrentLanguage(LanguageInterface::TYPE_INTERFACE)
-      ->getId();
+  protected function getVocabulary(): string {
+    return 'avustuslaji';
+  }
 
+  /**
+   * {@inheritdoc}
+   */
+  protected function getOptionValue(string $term_id): ?string {
     $reverse = $this->getReverseMap();
-
-    $terms = $this->entityTypeManager
-      ->getStorage('taxonomy_term')
-      ->loadTree(self::VOCABULARY, 0, NULL, TRUE);
-
-    $options = [];
-    foreach ($terms as $term) {
-      if (!$term->isPublished()) {
-        continue;
-      }
-      $term_id = (string) $term->id();
-
-      // Only terms that exist in the mapping are exposed.
-      $canonical = $reverse[$term_id] ?? NULL;
-      if ($canonical === NULL) {
-        continue;
-      }
-
-      $translated = $term->hasTranslation($langcode)
-        ? $term->getTranslation($langcode)
-        : $term;
-
-      $options[$canonical] = $translated->label();
-    }
-
-    return $options;
+    return $reverse[$term_id] ?? NULL;
   }
 
   /**
--- public/modules/custom/grants_application_search/src/Plugin/views/filter/CanonicalTargetGroup.php
+++ public/modules/custom/grants_application_search/src/Plugin/views/filter/CanonicalTargetGroup.php
@@ -4,11 +4,7 @@ declare(strict_types=1);
 
 namespace Drupal\grants_application_search\Plugin\views\filter;
 
-use Drupal\Core\Entity\EntityTypeManagerInterface;
-use Drupal\Core\Language\LanguageInterface;
-use Drupal\Core\Language\LanguageManagerInterface;
 use Drupal\views\Attribute\ViewsFilter;
-use Symfony\Component\DependencyInjection\ContainerInterface;
 
 /**
  * Canonical target group filter.
@@ -16,37 +12,7 @@ use Symfony\Component\DependencyInjection\ContainerInterface;
  * @todo UHF-12853: Review this class when removing the webform functionality.
  */
 #[ViewsFilter('canonical_target_group_select')]
-final class CanonicalTargetGroup extends CanonicalSelectFilterBase {
-
-  private const string VOCABULARY = 'target_group';
-
-  public function __construct(
-    array $configuration,
-    $plugin_id,
-    $plugin_definition,
-    private readonly EntityTypeManagerInterface $entityTypeManager,
-    private readonly LanguageManagerInterface $languageManager,
-  ) {
-    parent::__construct($configuration, $plugin_id, $plugin_definition);
-  }
-
-  /**
-   * {@inheritdoc}
-   */
-  public static function create(
-    ContainerInterface $container,
-    array $configuration,
-    $plugin_id,
-    $plugin_definition,
-  ): self {
-    return new self(
-      $configuration,
-      $plugin_id,
-      $plugin_definition,
-      $container->get('entity_type.manager'),
-      $container->get('language_manager'),
-    );
-  }
+final class CanonicalTargetGroup extends CanonicalTaxonomySelectFilterBase {
 
   /**
    * {@inheritdoc}
@@ -58,28 +24,8 @@ final class CanonicalTargetGroup extends CanonicalSelectFilterBase {
   /**
    * {@inheritdoc}
    */
-  protected function buildSelectOptions(): array {
-    $langcode = $this->languageManager
-      ->getCurrentLanguage(LanguageInterface::TYPE_INTERFACE)
-      ->getId();
-
-    $terms = $this->entityTypeManager
-      ->getStorage('taxonomy_term')
-      ->loadTree(self::VOCABULARY, 0, NULL, TRUE);
-
-    $options = [];
-    foreach ($terms as $term) {
-      if (!$term->isPublished()) {
-        continue;
-      }
-      $translated = $term->hasTranslation($langcode)
-        ? $term->getTranslation($langcode)
-        : $term;
-
-      $options[(string) $term->id()] = $translated->label();
-    }
-
-    return $options;
+  protected function getVocabulary(): string {
+    return 'target_group';
   }
 
 }
