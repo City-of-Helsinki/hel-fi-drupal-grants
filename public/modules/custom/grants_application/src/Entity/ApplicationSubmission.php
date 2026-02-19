88eb1a54ee0347b368be133c493b131a31913490
Risto Nykänen <risto.nykanen@druid.fi>
2026-02-19 15:24:04 +0200

    the parameter must match the function

--- public/modules/custom/grants_application/src/Entity/ApplicationSubmission.php
+++ public/modules/custom/grants_application/src/Entity/ApplicationSubmission.php
@@ -255,7 +255,7 @@ class ApplicationSubmission extends ContentEntityBase implements ContentEntityIn
    *   The url.
    */
   public function getPrintApplicationUrl(): Url {
-    $parameters = ['form_identifier' => $this->get('form_identifier')->value];
+    $parameters = ['application_number' => $this->get('application_number')->value];
     $attributes = [
       'attributes' => [
         'data-drupal-selector' => 'application-print-link',
