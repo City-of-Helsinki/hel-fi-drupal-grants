146a1e211ab97576ffcca7ee698735632fa0dfac
Risto Nykänen <risto.nykanen@druid.fi>
2026-07-06 16:02:36 +0300

    UHF-13428: phpstan

--- public/modules/custom/grants_application/tests/src/Kernel/Plugin/rest/resource/DraftTest.php
+++ public/modules/custom/grants_application/tests/src/Kernel/Plugin/rest/resource/DraftTest.php
@@ -430,7 +430,8 @@ final class DraftTest extends KernelTestBase {
     $http_kernel = $this->container->get('http_kernel');
     $response = $http_kernel->handle($request);
 
-    $data = json_decode($response->getContent() ?? '', TRUE);
+    $responseContent = $response->getContent() ?: '';
+    $data = json_decode($responseContent, TRUE);
 
     $this->assertTrue($response instanceof JsonResponse && $response->isSuccessful());
     $expected = $number + 1;
