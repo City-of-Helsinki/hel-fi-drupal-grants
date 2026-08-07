2b347cb5068416e928873d8c11be985af0002569
Tero Elonen <tero.elonen@druid.fi>
2026-08-07 16:25:16 +0300

    UHF-13339: Fix PHPCS

--- public/modules/custom/grants_application/src/Controller/ApplicationController.php
+++ public/modules/custom/grants_application/src/Controller/ApplicationController.php
@@ -136,7 +136,7 @@ final class ApplicationController extends ControllerBase {
   ): array|RedirectResponse {
     // Grant terms are stored in block.
     $blockStorage = $this->entityTypeManager()->getStorage('block_content');
-    $langcode = \Drupal::languageManager()->getCurrentLanguage()->getId();
+    $langcode = $this->languageManager()->getCurrentLanguage()->getId();
     $terms_block = $blockStorage->load(1);
     if ($terms_block && $terms_block->hasTranslation($langcode)) {
       $terms_block = $terms_block->getTranslation($langcode);
